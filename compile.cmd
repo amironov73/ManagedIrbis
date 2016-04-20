d525ab58b42c9986a1b10c276544da604e4e6301
Alexey Mironov <amironov73@gmail.com>
2016-04-20 14:00:41 +0900

    Fix compile.cmd and appveyor.yml

--- compile.cmd
+++ compile.cmd
@@ -31,29 +31,29 @@ SET OUTPUT=..\Binaries
 %MSBUILD% MoonIrbis\MoonIrbis.csproj         /t:Clean   /p:Configuration=Release /p:TargetFrameworkVersion=v4.5   /p:OutputPath=%OUTPUT%\Release45
 
 
-%MSBUILD% AM.Core\AM.Core.csproj             /p:Configuration=Debug   /p:TargetFrameworkVersion=v3.5   /p:OutputPath=%OUTPUT%\Debug35    /p:DefineConstants="FW461;FW46;FW452;FW451;FW45;FW40;FW35"
-%MSBUILD% AM.Core\AM.Core.csproj             /p:Configuration=Release /p:TargetFrameworkVersion=v3.5   /p:OutputPath=%OUTPUT%\Release35  /p:DefineConstants="FW461;FW46;FW452;FW451;FW45;FW40;FW35"
-%MSBUILD% ManagedClient\ManagedClient.csproj /p:Configuration=Debug   /p:TargetFrameworkVersion=v3.5   /p:OutputPath=%OUTPUT%\Debug35    /p:DefineConstants="FW461;FW46;FW452;FW451;FW45;FW40;FW35"
-%MSBUILD% ManagedClient\ManagedClient.csproj /p:Configuration=Release /p:TargetFrameworkVersion=v3.5   /p:OutputPath=%OUTPUT%\Release35  /p:DefineConstants="FW461;FW46;FW452;FW451;FW45;FW40;FW35"
-%MSBUILD% IrbisUI\IrbisUI.csproj             /p:Configuration=Debug   /p:TargetFrameworkVersion=v3.5   /p:OutputPath=%OUTPUT%\Debug35    /p:DefineConstants="FW461;FW46;FW452;FW451;FW45;FW40;FW35"
-%MSBUILD% IrbisUI\IrbisUI.csproj             /p:Configuration=Release /p:TargetFrameworkVersion=v3.5   /p:OutputPath=%OUTPUT%\Release35  /p:DefineConstants="FW461;FW46;FW452;FW451;FW45;FW40;FW35"
-%MSBUILD% MoonIrbis\MoonIrbis.csproj         /p:Configuration=Debug   /p:TargetFrameworkVersion=v3.5   /p:OutputPath=%OUTPUT%\Debug35    /p:DefineConstants="FW461;FW46;FW452;FW451;FW45;FW40;FW35"
-%MSBUILD% MoonIrbis\MoonIrbis.csproj         /p:Configuration=Release /p:TargetFrameworkVersion=v3.5   /p:OutputPath=%OUTPUT%\Release35  /p:DefineConstants="FW461;FW46;FW452;FW451;FW45;FW40;FW35"
+%MSBUILD% AM.Core\AM.Core.csproj             /p:Configuration=Debug   /p:TargetFrameworkVersion=v3.5   /p:OutputPath=%OUTPUT%\Debug35    /p:DefineConstants="FW461;FW46;FW452;FW451;FW45;FW40;FW35" /consoleloggerparameters:ErrorsOnly
+%MSBUILD% AM.Core\AM.Core.csproj             /p:Configuration=Release /p:TargetFrameworkVersion=v3.5   /p:OutputPath=%OUTPUT%\Release35  /p:DefineConstants="FW461;FW46;FW452;FW451;FW45;FW40;FW35" /consoleloggerparameters:ErrorsOnly
+%MSBUILD% ManagedClient\ManagedClient.csproj /p:Configuration=Debug   /p:TargetFrameworkVersion=v3.5   /p:OutputPath=%OUTPUT%\Debug35    /p:DefineConstants="FW461;FW46;FW452;FW451;FW45;FW40;FW35" /consoleloggerparameters:ErrorsOnly
+%MSBUILD% ManagedClient\ManagedClient.csproj /p:Configuration=Release /p:TargetFrameworkVersion=v3.5   /p:OutputPath=%OUTPUT%\Release35  /p:DefineConstants="FW461;FW46;FW452;FW451;FW45;FW40;FW35" /consoleloggerparameters:ErrorsOnly
+%MSBUILD% IrbisUI\IrbisUI.csproj             /p:Configuration=Debug   /p:TargetFrameworkVersion=v3.5   /p:OutputPath=%OUTPUT%\Debug35    /p:DefineConstants="FW461;FW46;FW452;FW451;FW45;FW40;FW35" /consoleloggerparameters:ErrorsOnly
+%MSBUILD% IrbisUI\IrbisUI.csproj             /p:Configuration=Release /p:TargetFrameworkVersion=v3.5   /p:OutputPath=%OUTPUT%\Release35  /p:DefineConstants="FW461;FW46;FW452;FW451;FW45;FW40;FW35" /consoleloggerparameters:ErrorsOnly
+%MSBUILD% MoonIrbis\MoonIrbis.csproj         /p:Configuration=Debug   /p:TargetFrameworkVersion=v3.5   /p:OutputPath=%OUTPUT%\Debug35    /p:DefineConstants="FW461;FW46;FW452;FW451;FW45;FW40;FW35" /consoleloggerparameters:ErrorsOnly
+%MSBUILD% MoonIrbis\MoonIrbis.csproj         /p:Configuration=Release /p:TargetFrameworkVersion=v3.5   /p:OutputPath=%OUTPUT%\Release35  /p:DefineConstants="FW461;FW46;FW452;FW451;FW45;FW40;FW35" /consoleloggerparameters:ErrorsOnly
 
-%MSBUILD% AM.Core\AM.Core.csproj             /p:Configuration=Debug   /p:TargetFrameworkVersion=v4.0   /p:OutputPath=%OUTPUT%\Debug40    /p:DefineConstants="FW461;FW46;FW452;FW451;FW45;FW40"
-%MSBUILD% AM.Core\AM.Core.csproj             /p:Configuration=Release /p:TargetFrameworkVersion=v4.0   /p:OutputPath=%OUTPUT%\Release40  /p:DefineConstants="FW461;FW46;FW452;FW451;FW45;FW40"
-%MSBUILD% ManagedClient\ManagedClient.csproj /p:Configuration=Debug   /p:TargetFrameworkVersion=v4.0   /p:OutputPath=%OUTPUT%\Debug40    /p:DefineConstants="FW461;FW46;FW452;FW451;FW45;FW40"
-%MSBUILD% ManagedClient\ManagedClient.csproj /p:Configuration=Release /p:TargetFrameworkVersion=v4.0   /p:OutputPath=%OUTPUT%\Release40  /p:DefineConstants="FW461;FW46;FW452;FW451;FW45;FW40"
-%MSBUILD% IrbisUI\IrbisUI.csproj             /p:Configuration=Debug   /p:TargetFrameworkVersion=v4.0   /p:OutputPath=%OUTPUT%\Debug40    /p:DefineConstants="FW461;FW46;FW452;FW451;FW45;FW40"
-%MSBUILD% IrbisUI\IrbisUI.csproj             /p:Configuration=Release /p:TargetFrameworkVersion=v4.0   /p:OutputPath=%OUTPUT%\Release40  /p:DefineConstants="FW461;FW46;FW452;FW451;FW45;FW40"
-%MSBUILD% MoonIrbis\MoonIrbis.csproj         /p:Configuration=Debug   /p:TargetFrameworkVersion=v4.0   /p:OutputPath=%OUTPUT%\Debug40    /p:DefineConstants="FW461;FW46;FW452;FW451;FW45;FW40"
-%MSBUILD% MoonIrbis\MoonIrbis.csproj         /p:Configuration=Release /p:TargetFrameworkVersion=v4.0   /p:OutputPath=%OUTPUT%\Release40  /p:DefineConstants="FW461;FW46;FW452;FW451;FW45;FW40"
+%MSBUILD% AM.Core\AM.Core.csproj             /p:Configuration=Debug   /p:TargetFrameworkVersion=v4.0   /p:OutputPath=%OUTPUT%\Debug40    /p:DefineConstants="FW461;FW46;FW452;FW451;FW45;FW40"      /consoleloggerparameters:ErrorsOnly
+%MSBUILD% AM.Core\AM.Core.csproj             /p:Configuration=Release /p:TargetFrameworkVersion=v4.0   /p:OutputPath=%OUTPUT%\Release40  /p:DefineConstants="FW461;FW46;FW452;FW451;FW45;FW40"      /consoleloggerparameters:ErrorsOnly
+%MSBUILD% ManagedClient\ManagedClient.csproj /p:Configuration=Debug   /p:TargetFrameworkVersion=v4.0   /p:OutputPath=%OUTPUT%\Debug40    /p:DefineConstants="FW461;FW46;FW452;FW451;FW45;FW40"      /consoleloggerparameters:ErrorsOnly
+%MSBUILD% ManagedClient\ManagedClient.csproj /p:Configuration=Release /p:TargetFrameworkVersion=v4.0   /p:OutputPath=%OUTPUT%\Release40  /p:DefineConstants="FW461;FW46;FW452;FW451;FW45;FW40"      /consoleloggerparameters:ErrorsOnly
+%MSBUILD% IrbisUI\IrbisUI.csproj             /p:Configuration=Debug   /p:TargetFrameworkVersion=v4.0   /p:OutputPath=%OUTPUT%\Debug40    /p:DefineConstants="FW461;FW46;FW452;FW451;FW45;FW40"      /consoleloggerparameters:ErrorsOnly
+%MSBUILD% IrbisUI\IrbisUI.csproj             /p:Configuration=Release /p:TargetFrameworkVersion=v4.0   /p:OutputPath=%OUTPUT%\Release40  /p:DefineConstants="FW461;FW46;FW452;FW451;FW45;FW40"      /consoleloggerparameters:ErrorsOnly
+%MSBUILD% MoonIrbis\MoonIrbis.csproj         /p:Configuration=Debug   /p:TargetFrameworkVersion=v4.0   /p:OutputPath=%OUTPUT%\Debug40    /p:DefineConstants="FW461;FW46;FW452;FW451;FW45;FW40"      /consoleloggerparameters:ErrorsOnly
+%MSBUILD% MoonIrbis\MoonIrbis.csproj         /p:Configuration=Release /p:TargetFrameworkVersion=v4.0   /p:OutputPath=%OUTPUT%\Release40  /p:DefineConstants="FW461;FW46;FW452;FW451;FW45;FW40"      /consoleloggerparameters:ErrorsOnly
 
-%MSBUILD% AM.Core\AM.Core.csproj             /p:Configuration=Debug   /p:TargetFrameworkVersion=v4.5   /p:OutputPath=%OUTPUT%\Debug45    /p:DefineConstants="FW461;FW46;FW452;FW451;FW45"
-%MSBUILD% AM.Core\AM.Core.csproj             /p:Configuration=Release /p:TargetFrameworkVersion=v4.5   /p:OutputPath=%OUTPUT%\Release45  /p:DefineConstants="FW461;FW46;FW452;FW451;FW45"
-%MSBUILD% ManagedClient\ManagedClient.csproj /p:Configuration=Debug   /p:TargetFrameworkVersion=v4.5   /p:OutputPath=%OUTPUT%\Debug45    /p:DefineConstants="FW461;FW46;FW452;FW451;FW45"
-%MSBUILD% ManagedClient\ManagedClient.csproj /p:Configuration=Release /p:TargetFrameworkVersion=v4.5   /p:OutputPath=%OUTPUT%\Release45  /p:DefineConstants="FW461;FW46;FW452;FW451;FW45"
-%MSBUILD% IrbisUI\IrbisUI.csproj             /p:Configuration=Debug   /p:TargetFrameworkVersion=v4.5   /p:OutputPath=%OUTPUT%\Debug45    /p:DefineConstants="FW461;FW46;FW452;FW451;FW45"
-%MSBUILD% IrbisUI\IrbisUI.csproj             /p:Configuration=Release /p:TargetFrameworkVersion=v4.5   /p:OutputPath=%OUTPUT%\Release45  /p:DefineConstants="FW461;FW46;FW452;FW451;FW45"
-%MSBUILD% MoonIrbis\MoonIrbis.csproj         /p:Configuration=Debug   /p:TargetFrameworkVersion=v4.5   /p:OutputPath=%OUTPUT%\Debug45    /p:DefineConstants="FW461;FW46;FW452;FW451;FW45"
-%MSBUILD% MoonIrbis\MoonIrbis.csproj         /p:Configuration=Release /p:TargetFrameworkVersion=v4.5   /p:OutputPath=%OUTPUT%\Release45  /p:DefineConstants="FW461;FW46;FW452;FW451;FW45"
+%MSBUILD% AM.Core\AM.Core.csproj             /p:Configuration=Debug   /p:TargetFrameworkVersion=v4.5   /p:OutputPath=%OUTPUT%\Debug45    /p:DefineConstants="FW461;FW46;FW452;FW451;FW45"           /consoleloggerparameters:ErrorsOnly
+%MSBUILD% AM.Core\AM.Core.csproj             /p:Configuration=Release /p:TargetFrameworkVersion=v4.5   /p:OutputPath=%OUTPUT%\Release45  /p:DefineConstants="FW461;FW46;FW452;FW451;FW45"           /consoleloggerparameters:ErrorsOnly
+%MSBUILD% ManagedClient\ManagedClient.csproj /p:Configuration=Debug   /p:TargetFrameworkVersion=v4.5   /p:OutputPath=%OUTPUT%\Debug45    /p:DefineConstants="FW461;FW46;FW452;FW451;FW45"           /consoleloggerparameters:ErrorsOnly
+%MSBUILD% ManagedClient\ManagedClient.csproj /p:Configuration=Release /p:TargetFrameworkVersion=v4.5   /p:OutputPath=%OUTPUT%\Release45  /p:DefineConstants="FW461;FW46;FW452;FW451;FW45"           /consoleloggerparameters:ErrorsOnly
+%MSBUILD% IrbisUI\IrbisUI.csproj             /p:Configuration=Debug   /p:TargetFrameworkVersion=v4.5   /p:OutputPath=%OUTPUT%\Debug45    /p:DefineConstants="FW461;FW46;FW452;FW451;FW45"           /consoleloggerparameters:ErrorsOnly
+%MSBUILD% IrbisUI\IrbisUI.csproj             /p:Configuration=Release /p:TargetFrameworkVersion=v4.5   /p:OutputPath=%OUTPUT%\Release45  /p:DefineConstants="FW461;FW46;FW452;FW451;FW45"           /consoleloggerparameters:ErrorsOnly
+%MSBUILD% MoonIrbis\MoonIrbis.csproj         /p:Configuration=Debug   /p:TargetFrameworkVersion=v4.5   /p:OutputPath=%OUTPUT%\Debug45    /p:DefineConstants="FW461;FW46;FW452;FW451;FW45"           /consoleloggerparameters:ErrorsOnly
+%MSBUILD% MoonIrbis\MoonIrbis.csproj         /p:Configuration=Release /p:TargetFrameworkVersion=v4.5   /p:OutputPath=%OUTPUT%\Release45  /p:DefineConstants="FW461;FW46;FW452;FW451;FW45"           /consoleloggerparameters:ErrorsOnly
