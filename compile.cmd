413f21e54a9aecff1b244a5f0b313b400ad949f9
amironov73 <amironov73@gmail.com>
2016-05-02 14:48:57 +0800

    Fix compile.cmd

--- compile.cmd
+++ compile.cmd
@@ -5,32 +5,18 @@ SET OUTPUT=..\Binaries
 SET PARAMS=/consoleloggerparameters:ErrorsOnly /m
 
 FOR %%P IN (AM.Core,ManagedClient,IrbisUI,MoonIrbis) DO (
-  FOR %%T IN (v3.5,v4.0,v4.5) DO (
-     FOR %%B IN (Debug,Release) DO (
-       CALL :CLEAN %%P %%B %%T
-     )
-  )
-)
-
-FOR %%P IN (AM.Core,ManagedClient,IrbisUI,MoonIrbis) DO (
   FOR %%B IN (Debug,Release) DO (
-    CALL :BUILD %%P %%B v3.5 "FW461;FW46;FW452;FW451;FW45;FW40;FW35"
-    CALL :BUILD %%P %%B v4.0 "FW461;FW46;FW452;FW451;FW45;FW40"
-    CALL :BUILD %%P %%B v4.5 "FW461;FW46;FW452;FW451;FW45"
+    CALL :BUILD %%P %%B 35 v3.5 "FW35"
+    CALL :BUILD %%P %%B 40 v4.0 "FW40"
+    CALL :BUILD %%P %%B 45 v4.5 "FW40;FW45"
   )
 )
 
 EXIT
 
-:CLEAN
-
-ECHO CLEAN %1 %2 %3
-%MSBUILD% %1\%1.csproj /t:Clean /p:Configuration=%2 /p:TargetFrameworkVersion=%3 /p:OutputPath=%OUTPUT%\%2 %PARAMS%
-GOTO :END
-
 :BUILD
-ECHO BUILD %1 %2 %3
-%MSBUILD% %1\%1.csproj /p:Configuration=%2 /p:TargetFrameworkVersion=%3  /p:OutputPath=%OUTPUT%\%2 %PARAMS% /p:DefineConstants=%4
+ECHO BUILD %1 %2 %4
+%MSBUILD% %1\%1.csproj /p:Configuration=%2 /p:TargetFrameworkVersion=%4  /p:OutputPath=%OUTPUT%\%2%3 %PARAMS% /p:DefineConstants=%5
 GOTO :END
 
 :END
