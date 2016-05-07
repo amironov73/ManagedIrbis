d65443ec9deaf964a00cadfba65ecea3c67f04aa
amironov73 <amironov73@gmail.com>
2016-05-07 15:51:12 +0800

    Add SerializationUtility

--- AM.Core/AM/Diagnostics/ProcessUtility.cs
+++ AM.Core/AM/Diagnostics/ProcessUtility.cs
@@ -33,6 +33,278 @@ namespace AM.Diagnostics
 
         #region Public methods
 
+        /// <summary>
+        /// Запуск процесса с перехватом всего консольного вывода
+        /// в строковую переменную.
+        /// </summary>
+        /// <param name="info"></param>
+        /// <param name="standardInput">Если не null, задает
+        /// содержимое стандартного ввода.</param>
+        /// <returns></returns>
+        public static string RunAndReadStandardOutput
+            (
+                ProcessStartInfo info,
+                string standardInput
+            )
+        {
+            info.UseShellExecute = false;
+            info.RedirectStandardOutput = true;
+            if (standardInput != null)
+            {
+                info.RedirectStandardInput = true;
+            }
+            using (Process process = new Process())
+            {
+                process.StartInfo = info;
+                if (standardInput != null)
+                {
+                    process.StandardInput.Write(standardInput);
+                }
+                process.Start();
+                process.WaitForExit();
+                return process.StandardOutput.ReadToEnd();
+            }
+        }
+
+        /// <summary>
+        /// Запуск процесса с перехватом всего консольного вывода
+        /// в строковую переменную.
+        /// </summary>
+        /// <param name="info"></param>
+        /// <returns></returns>
+        public static string RunAndReadStandardOutput
+            (
+                ProcessStartInfo info
+            )
+        {
+            return RunAndReadStandardOutput
+                (
+                    info,
+                    null
+                );
+        }
+
+        /// <summary>
+        /// Запуск процесса с перехватом всего консольного вывода
+        /// в строковую переменную.
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <param name="arguments"></param>
+        /// <param name="standardInput"></param>
+        /// <returns></returns>
+        public static string RunAndReadStandardOutput
+            (
+                string fileName,
+                string arguments,
+                string standardInput
+            )
+        {
+            ProcessStartInfo psi = new ProcessStartInfo
+                (
+                    fileName,
+                    arguments
+                  );
+            return RunAndReadStandardOutput(psi, standardInput);
+        }
+
+        /// <summary>
+        /// Запуск процесса с перехватом всего консольного вывода
+        /// в строковую переменную.
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <param name="arguments"></param>
+        /// <returns></returns>
+        public static string RunAndReadStandardOutput
+            (
+            string fileName,
+            string arguments)
+        {
+            return RunAndReadStandardOutput
+                (
+                    fileName,
+                    arguments,
+                    null
+                  );
+        }
+
+        /// <summary>
+        /// Запуск процесса с перехватом всего консольного вывода
+        /// в строковую переменную.
+        /// </summary>
+        /// <param name="info"></param>
+        /// <param name="standardInput">Если не null, задает
+        /// содержимое стандартного ввода.</param>
+        /// <returns></returns>
+        public static string[] RunAndReadStandardOutputAndError
+            (
+                ProcessStartInfo info,
+                string standardInput
+            )
+        {
+            info.UseShellExecute = false;
+            info.RedirectStandardOutput = true;
+            info.RedirectStandardError = true;
+            if (standardInput != null)
+            {
+                info.RedirectStandardInput = true;
+            }
+            using (Process process = new Process())
+            {
+                process.StartInfo = info;
+                if (standardInput != null)
+                {
+                    process.StandardInput.Write(standardInput);
+                }
+                process.Start();
+                process.WaitForExit();
+                string stdout = process.StandardOutput.ReadToEnd();
+                string stderr = process.StandardError.ReadToEnd();
+                return new string[]
+                    {
+                        stdout, stderr
+                    };
+            }
+        }
+
+        /// <summary>
+        /// Запуск процесса с перехватом всего консольного вывода
+        /// в строковую переменную.
+        /// </summary>
+        /// <param name="info"></param>
+        /// <returns></returns>
+        public static string[] RunAndReadStandardOutputAndError
+            (
+                ProcessStartInfo info
+            )
+        {
+            return RunAndReadStandardOutputAndError(info, null);
+        }
+
+        /// <summary>
+        /// Запуск процесса с перехватом всего консольного вывода
+        /// в строковую переменную.
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <param name="arguments"></param>
+        /// <param name="standardInfo"></param>
+        /// <returns></returns>
+        public static string[] RunAndReadStandardOutputAndError
+            (
+                string fileName,
+                string arguments,
+                string standardInfo
+            )
+        {
+            ProcessStartInfo psi = new ProcessStartInfo
+                (fileName,
+                  arguments);
+            return RunAndReadStandardOutputAndError(psi, standardInfo);
+        }
+
+        /// <summary>
+        /// Запуск процесса с перехватом всего консольного вывода
+        /// в строковую переменную.
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <param name="arguments"></param>
+        /// <returns></returns>
+        public static string[] RunAndReadStandardOutputAndError
+            (
+                string fileName,
+                string arguments
+            )
+        {
+            return RunAndReadStandardOutputAndError(fileName, arguments, null);
+        }
+
+        /// <summary>
+        /// Запускает процесс и ожидает его завершения.
+        /// </summary>
+        /// <param name="info"></param>
+        /// <param name="milliseconds">Сколько ожидать.
+        /// Неположительные значения = бесконечно.</param>
+        /// <returns>Код, вовращенный процессом.
+        /// Если с процессом не сложилось, возвращается -1.</returns>
+        public static int RunAndWait
+            (
+                ProcessStartInfo info,
+                int milliseconds
+            )
+        {
+            using (Process process = new Process())
+            {
+                process.StartInfo = info;
+                process.Start();
+                if (milliseconds > 0)
+                {
+                    if (!process.WaitForExit(milliseconds))
+                    {
+                        return -1;
+                    }
+                }
+                else
+                {
+                    process.WaitForExit();
+                }
+                return process.ExitCode;
+            }
+        }
+
+        /// <summary>
+        /// Запускает процесс и ожидает его завершения.
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <param name="arguments"></param>
+        /// <param name="milliseconds">Сколько ожидать.
+        /// Неположительные значения = бесконечно.</param>
+        /// <returns>Код, возвращенный процессом. 
+        /// Если с процессом как-то не сложилось, возвращается -1.</returns>
+        public static int RunAndWait
+            (
+                string fileName,
+                string arguments,
+                int milliseconds
+            )
+        {
+            ProcessStartInfo psi = new ProcessStartInfo
+                (
+                    fileName,
+                    arguments
+                )
+            {
+                UseShellExecute = false
+            };
+            return RunAndWait(psi, milliseconds);
+        }
+
+        /// <summary>
+        /// Запускает процесс и ожидает его завершения.
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <param name="arguments"></param>
+        /// <returns></returns>
+        public static int RunAndWait
+            (
+                string fileName,
+                string arguments
+            )
+        {
+            return RunAndWait(fileName, arguments, -1);
+        }
+
+        /// <summary>
+        /// Запускает процесс и ожидает его завершения.
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        public static int RunAndWait
+            (
+                string fileName
+            )
+        {
+            ProcessStartInfo psi = new ProcessStartInfo(fileName);
+            return RunAndWait(psi, -1);
+        }
 
         #endregion
     }
---
+++ AM.Core/AM/Runtime/SerializationUtility.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AM.Runtime
+{
+    static class SerializationUtility
+    {
+        #region Private members
+        #endregion
+
+        #region Public methods
+
+        /// <summary>
+        /// Deserialize object from byte array.
+        /// </summary>
+        /// <typeparam name="T">Type of object.</typeparam>
+        /// <param name="bytes">Array of bytes.</param>
+        /// <returns>Deserialized object.</returns>
+        [CLSCompliant(false)]
+        public static T BinDeserialize<T>(byte[] bytes)
+        {
+            MemoryStream stream = new MemoryStream(bytes, false);
+            return BinDeserialize<T>(stream);
+        }
+
+        /// <summary>
+        /// Deserialize object from disk file.
+        /// </summary>
+        /// <typeparam name="T">Type of object.</typeparam>
+        /// <param name="fileName">File name.</param>
+        /// <returns>Deseriazlized object.</returns>
+        [CLSCompliant(false)]
+        public static T BinDeserialize<T>(string fileName)
+        {
+            using (Stream stream = File.OpenRead(fileName))
+            {
+                return BinDeserialize<T>(stream);
+            }
+        }
+
+        /// <summary>
+        /// Deserialize object from stream.
+        /// </summary>
+        /// <typeparam name="T">Type of object.</typeparam>
+        /// <param name="stream">Stream.</param>
+        /// <returns>Deserialized object.</returns>
+        [CLSCompliant(false)]
+        public static T BinDeserialize<T>(Stream stream)
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            return (T)formatter.Deserialize(stream);
+        }
+
+        /// <summary>
+        /// Serialize object to stream.
+        /// </summary>
+        /// <param name="stream">Stream.</param>
+        /// <param name="obj">Object.</param>
+        public static void BinSerialize(Stream stream, object obj)
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(stream, obj);
+        }
+
+        /// <summary>
+        /// Serialize object to disk file.
+        /// </summary>
+        /// <param name="fileName">File name.</param>
+        /// <param name="obj">Object.</param>
+        public static void BinSerialize(string fileName, object obj)
+        {
+            using (Stream stream = File.Create(fileName))
+            {
+                BinSerialize(stream, obj);
+            }
+        }
+
+        /// <summary>
+        /// Serialize object to byte array.
+        /// </summary>
+        /// <param name="obj">Object.</param>
+        /// <returns>Array of bytes.</returns>
+        public static byte[] BinSerialize(object obj)
+        {
+            MemoryStream stream = new MemoryStream();
+            BinSerialize(stream, obj);
+            return stream.ToArray();
+        }
+
+        #endregion
+    }
+}
