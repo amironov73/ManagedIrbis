aa9b13bfefae783b6bbe511f8c780a12d11a4260
Alexey Mironov <amironov73@gmail.com>
2016-05-13 08:51:47 +0900

    Add AM.Rfid

---
+++ AM.Rfid/AM/Rfid/RfidException.cs
@@ -0,0 +1,78 @@
+﻿/* RfidException.cs -- RFID-specific exception
+ * Ars Magna project, http://arsmagna.ru 
+ */
+
+#region Using directives
+
+using System;
+using System.Runtime.Serialization;
+
+using JetBrains.Annotations;
+
+#endregion
+
+namespace AM.Rfid
+{
+    /// <summary>
+    /// RFID-specific exception
+    /// </summary>
+    [PublicAPI]
+    [Serializable]
+    public sealed class RfidException
+        : ApplicationException
+    {
+        #region Construciton
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public RfidException()
+        {
+        }
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public RfidException
+            (
+                string message
+            )
+            : base(message)
+        {
+        }
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public RfidException
+            (
+                string message,
+                Exception innerException
+            )
+            : base
+            (
+                message,
+                innerException
+            )
+        {
+        }
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public RfidException
+            (
+                SerializationInfo info,
+                StreamingContext context
+            )
+            : base
+            (
+                info,
+                context
+            )
+        {
+        }
+
+        #endregion
+    }
+}
