991723627771f0a3cc7a65b215d9aa467214bc34
Alexey Mironov <amironov73@gmail.com>
2016-05-15 12:14:08 +0900

    Add TaskFactoryUtility

---
+++ AM.Core/AM/Threading/Tasks/TaskFactoryUtility.cs
@@ -0,0 +1,79 @@
+﻿/* TaskFactoryUtility.cs --
+ * Ars Magna project, http://arsmagna.ru
+ */
+
+#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+using CodeJam;
+
+using JetBrains.Annotations;
+
+using MoonSharp.Interpreter;
+
+#endregion
+
+namespace AM.Threading.Tasks
+{
+    /// <summary>
+    /// Extensions for <see cref="TaskFactory"/> class.
+    /// </summary>
+    [PublicAPI]
+    [MoonSharpUserData]
+    public static class TaskFactoryUtility
+    {
+        #region Public methods
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [NotNull]
+        public static Task Run
+            (
+                [NotNull] this TaskFactory factory,
+                [NotNull] Action action
+            )
+        {
+            Task result = factory.StartNew
+                (
+                    action,
+                    factory.CancellationToken,
+                    factory.CreationOptions
+                    | TaskCreationOptions.DenyChildAttach,
+                    factory.Scheduler
+                );
+
+            return result;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [NotNull]
+        public static Task<TResult> Run<TResult>
+            (
+                [NotNull] this TaskFactory factory,
+                [NotNull] Func<TResult> action
+            )
+        {
+            Task<TResult> result = factory.StartNew
+                (
+                    action,
+                    factory.CancellationToken,
+                    factory.CreationOptions
+                    | TaskCreationOptions.DenyChildAttach,
+                    factory.Scheduler
+                );
+
+            return result;
+        }
+
+        #endregion
+    }
+}
---
+++ AM.Core/AM/Threading/Tasks/TasksUtility.cs
@@ -0,0 +1,88 @@
+﻿/* TasksUtility.cs -- 
+ * Ars Magna project, http://arsmagna.ru
+ */
+
+#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+using CodeJam;
+
+using JetBrains.Annotations;
+
+using MoonSharp.Interpreter;
+
+#endregion
+
+namespace AM.Threading.Tasks
+{
+    /// <summary>
+    /// Extensions for <see cref="Task"/> class.
+    /// </summary>
+    [PublicAPI]
+    [MoonSharpUserData]
+    public static class TasksUtility
+    {
+        #region Public methods
+
+        /// <summary>
+        /// Waits for the task to complete, unwrapping any exceptions.
+        /// </summary>
+        /// <param name="task">The task. May not be <c>null</c>.</param>
+        public static void WaitAndUnwrapException(this Task task)
+        {
+            task.GetAwaiter().GetResult();
+        }
+
+        /// <summary>
+        /// Waits for the task to complete, unwrapping any exceptions.
+        /// </summary>
+        /// <typeparam name="TResult">The type of the result of the task.</typeparam>
+        /// <param name="task">The task. May not be <c>null</c>.</param>
+        /// <returns>The result of the task.</returns>
+        public static TResult WaitAndUnwrapException<TResult>(this Task<TResult> task)
+        {
+            return task.GetAwaiter().GetResult();
+        }
+
+        /// <summary>
+        /// Waits for the task to complete, but does not raise task exceptions. The task exception (if any) is unobserved.
+        /// </summary>
+        /// <param name="task">The task. May not be <c>null</c>.</param>
+        public static void WaitWithoutException(this Task task)
+        {
+            try
+            {
+                task.Wait();
+            }
+            catch (AggregateException)
+            {
+            }
+        }
+
+        /// <summary>
+        /// Waits for the task to complete, but does not raise task exceptions. The task exception (if any) is unobserved.
+        /// </summary>
+        /// <param name="task">The task. May not be <c>null</c>.</param>
+        /// <param name="cancellationToken">A cancellation token to observe while waiting for the task to complete.</param>
+        /// <exception cref="OperationCanceledException">The <paramref name="cancellationToken"/> was cancelled before the <paramref name="task"/> completed.</exception>
+        public static void WaitWithoutException(this Task task, CancellationToken cancellationToken)
+        {
+            try
+            {
+                task.Wait(cancellationToken);
+            }
+            catch (AggregateException)
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+            }
+        }
+
+        #endregion
+    }
+}
