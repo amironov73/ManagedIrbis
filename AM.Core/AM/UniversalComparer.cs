7589ca5ebb3187f3928cac026dd248324954376e
Alexey Mironov <amironov73@gmail.com>
2016-06-10 08:31:18 +0900

    Add UniversalComparer

---
+++ AM.Core/AM/UniversalComparer.cs
@@ -0,0 +1,73 @@
+﻿/* UniversalComparer.cs -- универсальный компаратор
+ * Ars Magna project, http://arsmagna.ru
+ */
+
+#region Using directives
+
+using System;
+using System.Collections.Generic;
+
+using CodeJam;
+
+using JetBrains.Annotations;
+
+using MoonSharp.Interpreter;
+
+#endregion
+
+namespace AM
+{
+    /// <summary>
+    /// Универсальный компаратор.
+    /// </summary>
+    [PublicAPI]
+    [MoonSharpUserData]
+    public sealed class UniversalComparer<T>
+        : IComparer<T>
+    {
+        #region Properties
+
+        /// <summary>
+        /// Используемый для сравнения делегат.
+        /// </summary>
+        [NotNull]
+        public Func<T, T, int> Function { get { return _function; } }
+
+        #endregion
+
+        #region Construction
+
+        /// <summary>
+        /// Конструктор.
+        /// </summary>
+        public UniversalComparer
+            (
+                [NotNull] Func<T, T, int> function
+            )
+        {
+            Code.NotNull(function, "function");
+
+            _function = function;
+        }
+
+        #endregion
+
+        #region Private members
+
+        private Func<T, T, int> _function;
+
+        #endregion
+
+        #region IComparer<T> members
+
+        /// <summary>
+        /// Compares the specified values.
+        /// </summary>
+        public int Compare(T left, T right)
+        {
+            return _function(left, right);
+        }
+
+        #endregion
+    }
+}
---
+++ UnitTests/AM/UniversalComparerTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using AM;
+
+namespace UnitTests.AM
+{
+    [TestClass]
+    public class UniversalComparerTest
+    {
+        [TestMethod]
+        public void TestUniversalComparer()
+        {
+            UniversalComparer<int> comparer
+                = new UniversalComparer<int>
+                    (
+                        (left, right) => left - right
+                    );
+
+            Assert.IsTrue
+                (
+                    0 ==
+                    comparer.Compare(1, 1)
+                );
+            Assert.IsTrue
+                (
+                    0 <
+                    comparer.Compare(2,1)
+                );
+            Assert.IsTrue
+                (
+                    0 > 
+                    comparer.Compare(1,2)
+                );
+        }
+    }
+}
