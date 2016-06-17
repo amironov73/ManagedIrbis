4723fc184c1d4f4f3fb1cf6ab51b731bf7589653
amironov73 <amironov73@gmail.com>
2016-06-17 20:56:04 +0800

    Add tests

---
+++ UnitTests/AM/Runtime/InteropUtilityTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using AM.Runtime;
+
+namespace UnitTests.AM.Runtime
+{
+    [TestClass]
+    public class InteropUtilityTest
+    {
+        [StructLayout(LayoutKind.Explicit)]
+        struct DummyStruct
+        {
+            [FieldOffset(0)]
+            public int FirstField;
+
+            [FieldOffset(4)]
+            public short SecondField;
+
+            [FieldOffset(6)]
+            public int ThirdField;
+        }
+
+        [TestMethod]
+        public void TestInteropUtilityByteArrayToStructure()
+        {
+            byte[] bytes =  {1, 0, 0, 0, 2, 0, 3, 0, 0, 0};
+            DummyStruct dummy = InteropUtility
+                    .ByteArrayToStructure<DummyStruct>
+                    (
+                        bytes
+                    );
+            Assert.AreEqual(1,dummy.FirstField);
+            Assert.AreEqual(2,dummy.SecondField);
+            Assert.AreEqual(3,dummy.ThirdField);
+        }
+
+        [Ignore]
+        [TestMethod]
+        public void TestInteropUtilityByteArrayToStructure2()
+        {
+            byte[] bytes = { 1, 0, 0, 0, 2, 0, 3, 0, 0, 0 };
+            DummyStruct dummy = InteropUtility
+                    .ByteArrayToStructure2<DummyStruct>
+                    (
+                        bytes
+                    );
+            Assert.AreEqual(1, dummy.FirstField);
+            Assert.AreEqual(2, dummy.SecondField);
+            Assert.AreEqual(3, dummy.ThirdField);
+        }
+    }
+}
