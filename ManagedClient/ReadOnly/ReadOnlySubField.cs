35d505b7a54210f5c6f876991a595862b2e08720
amironov73 <amironov73@gmail.com>
2016-06-18 12:08:09 +0800

    Add ReadOnlyIrbisRecord.cs

---
+++ ManagedClient/ReadOnly/ReadOnlySubField.cs
@@ -0,0 +1,57 @@
+﻿/* ReadOnlySubField.cs -- MARC record subfield.
+ * Ars Magna project, http://arsmagna.ru
+ */
+
+#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AM;
+using AM.IO;
+using AM.Runtime;
+
+using CodeJam;
+
+using JetBrains.Annotations;
+
+using MoonSharp.Interpreter;
+
+using Newtonsoft.Json;
+
+#endregion
+
+
+namespace ManagedClient.ReadOnly
+{
+    /// <summary>
+    /// MARC record subfield
+    /// </summary>
+    [PublicAPI]
+    [MoonSharpUserData]
+    public sealed class ReadOnlySubField
+    {
+        #region Propertites
+
+        #endregion
+
+        #region Construction
+
+        #endregion
+
+        #region Private members
+
+        #endregion
+
+        #region Public methods
+
+        #endregion
+
+        #region Object members
+
+        #endregion
+    }
+}
---
+++ ManagedClient/Transform/RecordTransformContext.cs
@@ -0,0 +1,84 @@
+﻿/* RecordTransformContext.cs --
+ * Ars Magna project, http://arsmagna.ru
+ */
+
+#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AM;
+using AM.IO;
+using AM.Runtime;
+
+using CodeJam;
+
+using JetBrains.Annotations;
+
+using ManagedClient.ReadOnly;
+
+using MoonSharp.Interpreter;
+
+using Newtonsoft.Json;
+
+#endregion
+
+namespace ManagedClient.Transform
+{
+    /// <summary>
+    /// Context of <see cref="IrbisRecord"/> transformation.
+    /// </summary>
+    [PublicAPI]
+    [Serializable]
+    [MoonSharpUserData]
+    public sealed class RecordTransformContext
+    {
+        #region Properties
+
+        /// <summary>
+        /// Source record.
+        /// </summary>
+        [NotNull]
+        public ReadOnlyIrbisRecord Source { get { return _source; } }
+
+        /// <summary>
+        /// Target record.
+        /// </summary>
+        public IrbisRecord Target { get { return _target; } }
+
+        #endregion
+
+        #region Construction
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public RecordTransformContext
+            (
+                [NotNull] ReadOnlyIrbisRecord source
+            )
+        {
+            Code.NotNull(source, "source");
+
+            _source = source;
+            _target = new IrbisRecord();
+        }
+
+        #endregion
+
+        #region Private member
+
+        private ReadOnlyIrbisRecord _source;
+
+        private IrbisRecord _target;
+
+        #endregion
+
+        #region Public methods
+
+        #endregion
+    }
+}
