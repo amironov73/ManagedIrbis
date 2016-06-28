feb375435e4c76d36c9dc860d1ae636c2ad63af2
Alexey Mironov <amironov73@gmail.com>
2016-06-28 13:28:48 +0900

    Add Examples folder

---
+++ Examples/Template.cs
@@ -0,0 +1,22 @@
+﻿/* Ars Magna Project, http://arsmagna.ru
+ */
+
+#region Using directives
+
+using System;
+
+using AM;
+
+#endregion
+
+public class Sample
+{
+    /// <summary>
+    /// This code illustrates well known
+    /// Hello World approach in applied programming.
+    /// </summary>
+    public static void Main()
+    {
+        Console.WriteLine("Hello, Ars Magna!");
+    }
+}
