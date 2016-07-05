d536ed9dba198f9279adb7279b1292b7d1eac79b
Alexey Mironov <amironov73@gmail.com>
2016-07-05 11:23:44 +0900

    Add IrbisFileName

---
+++ ManagedClient/Network/Commands/ReadFileCommand.cs
@@ -0,0 +1,37 @@
+﻿/* ReadFileCommand.cs -- 
+ * Ars Magna project, http://arsmagna.ru
+ */
+
+#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AM;
+
+using CodeJam;
+
+using JetBrains.Annotations;
+
+using MoonSharp.Interpreter;
+
+using Newtonsoft.Json;
+
+#endregion
+
+namespace ManagedClient.Network.Commands
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [PublicAPI]
+    [MoonSharpUserData]
+    public sealed class ReadFileCommand
+    {
+    }
+}
---
+++ ManagedClient/Network/IrbisFileName.cs
@@ -0,0 +1,109 @@
+﻿/* IrbisFileName.cs -- 
+ * Ars Magna project, http://arsmagna.ru
+ */
+
+#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AM;
+using AM.Runtime;
+using CodeJam;
+
+using JetBrains.Annotations;
+
+using MoonSharp.Interpreter;
+
+using Newtonsoft.Json;
+
+#endregion
+
+namespace ManagedClient.Network
+{
+    /// <summary>
+    /// Irbis file name.
+    /// </summary>
+    [PublicAPI]
+    [MoonSharpUserData]
+    [DebuggerDisplay("Path={Path} Database={Database} FileName={FileName}")]
+    public sealed class IrbisFileName
+        : IHandmadeSerializable,
+        IVerifiable
+    {
+        #region Properties
+
+        /// <summary>
+        /// Path.
+        /// </summary>
+        public IrbisPath Path { get; set; }
+
+        /// <summary>
+        /// Database name.
+        /// </summary>
+        [CanBeNull]
+        public string Database { get; set; }
+
+        /// <summary>
+        /// File name.
+        /// </summary>
+        [CanBeNull]
+        public string FileName { get; set; }
+
+        #endregion
+
+        #region Public methods
+        
+        #endregion
+
+        #region IHandmadeSerializable members
+
+        /// <summary>
+        /// Restore object stat from the specified stream.
+        /// </summary>
+        public void RestoreFromStream
+            (
+                BinaryReader reader
+            )
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Save object state to the specified stream.
+        /// </summary>
+        public void SaveToStream
+            (
+                BinaryWriter writer
+            )
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #region IVerifiable members
+
+        /// <summary>
+        /// Verify object state.
+        /// </summary>
+        public bool Verify
+            (
+                bool throwOnError
+            )
+        {
+            return true;
+        }
+
+        #endregion
+
+        #region Object members
+
+        #endregion
+    }
+}
