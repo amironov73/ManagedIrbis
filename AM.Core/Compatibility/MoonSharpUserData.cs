72132a14d2701ea99f470d694cca59a305637099
Alexey Mironov <amironov73@gmail.com>
2016-07-08 08:49:10 +0900

    Prepare to .NET Core

---
+++ AM.Core/Compatibility/MoonSharpUserData.cs
@@ -0,0 +1,17 @@
+﻿/* MoonSharpUserData.cs -- temporary solution.
+ * Ars Magna project, http://arsmagna.ru
+ */
+
+#region Using directives
+
+using System;
+
+#endregion
+
+namespace MoonSharp.Interpreter
+{
+    class MoonSharpUserDataAttribute
+        : Attribute
+    {
+    }
+}
