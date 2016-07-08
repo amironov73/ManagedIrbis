756def683f6c545656229131abff95bdaeef06fc
Alexey Mironov <amironov73@gmail.com>
2016-07-08 13:04:41 +0900

    Add UniversalCommand

---
+++ Docs/defines.md
@@ -0,0 +1,18 @@
+Ars Magna project uses those preprocessor symbols:
+
+| Symbol | Meaning |
+| ------ | ------- |
+| FW35 | Desktop .NET 3.5 SP1 |
+| FW4 | Desktop .NET 4.0, 4.5.x, 4.6.x |
+| FW40 | Desktop .NET 4.0 only (not 4.5 or 4.6) |
+| FW45 | Desktop .NET 4.5 |
+| FW46 | Desktop .NET 4.6 |
+| PCL | Portable Class Library |
+| PROFILE328 | PCL profile 328 |
+| NETCORE | .NET Core 5.0 |
+| NETPLATFORM | .NET Platform 5.0 |
+| MONO | Mono 4+ |
+| UNITY | Unity 5+ |
+| SL | Silverlight 5.0 |
+| XAMARIN | Xamarin Android 4+ |
+| UAP | Universal Windows Platform |
---
+++ ManagedClient/Network/Commands/UniversalCommand.cs
@@ -0,0 +1,119 @@
+﻿/* UniversalCommand.cs -- 
+ * Ars Magna project, http://arsmagna.ru
+ */
+
+#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AM;
+
+using CodeJam;
+
+using JetBrains.Annotations;
+
+using MoonSharp.Interpreter;
+
+using Newtonsoft.Json;
+
+#endregion
+
+namespace ManagedClient.Network.Commands
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [PublicAPI]
+    [MoonSharpUserData]
+    public sealed class UniversalCommand
+        : AbstractCommand
+    {
+        #region Properties
+
+        /// <summary>
+        /// Allow any server response.
+        /// </summary>
+        public bool AllowAnyResponse { get; set; }
+
+        /// <summary>
+        /// Arguments.
+        /// </summary>
+        [CanBeNull]
+        public object[] Arguments { get; private set; }
+
+        /// <summary>
+        /// Command code
+        /// </summary>
+        [NotNull]
+        public string CommandCode { get; private set; }
+
+        #endregion
+
+        #region Construction
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public UniversalCommand
+            (
+                [NotNull] IrbisConnection connection,
+                [NotNull] string commandCode
+            )
+            : base(connection)
+        {
+            Code.NotNullNorEmpty(commandCode, "commandCode");
+
+            CommandCode = commandCode;
+        }
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public UniversalCommand
+            (
+                [NotNull] IrbisConnection connection,
+                [NotNull] string commandCode,
+                params object[] arguments
+            )
+            : this (connection, commandCode)
+        {
+            Arguments = arguments;
+        }
+
+        #endregion
+
+        #region AbstractCommand members
+
+        public override void CheckResponse
+            (
+                IrbisServerResponse response
+            )
+        {
+            if (!AllowAnyResponse)
+            {
+                base.CheckResponse(response);
+            }
+        }
+
+        public override IrbisClientQuery CreateQuery()
+        {
+            IrbisClientQuery result = base.CreateQuery();
+
+            result.CommandCode = CommandCode;
+            if (!ReferenceEquals(Arguments, null))
+            {
+                result.Arguments.AddRange(Arguments);
+            }
+
+            return result;
+        }
+
+        #endregion
+    }
+}
