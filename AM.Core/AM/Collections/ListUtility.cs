572b1261c65f044ce40cb0aa272a5c5327aab995
Alexey Mironov <amironov73@gmail.com>
2016-07-21 09:42:55 +0900

    Add ListUtility

--- AM.Core/AM/Collections/ListUtility.cs
+++ AM.Core/AM/Collections/ListUtility.cs
@@ -6,7 +6,6 @@
 
 using System;
 using System.Collections.Generic;
-using System.Text;
 
 using CodeJam;
 
@@ -19,7 +18,10 @@ using MoonSharp.Interpreter;
 
 namespace AM.Collections
 {
-
+    /// <summary>
+    /// Useful routines for <see cref="IList{T}"/>.
+    /// </summary>
+    /// <remarks>Borrowed from Json.NET.</remarks>
     [PublicAPI]
     [MoonSharpUserData]
     public static class ListUtility
@@ -29,7 +31,138 @@ namespace AM.Collections
         #endregion
 
         #region Public methods
-        
+
+        /// <summary>
+        /// Add to list if don't have yet.
+        /// </summary>
+        public static bool AddDistinct<T>
+            (
+                [NotNull] this IList<T> list,
+                T value
+            )
+        {
+            return list.AddDistinct
+                (
+                    value,
+                    EqualityComparer<T>.Default
+                );
+        }
+
+        /// <summary>
+        /// Add to list if don't have yet.
+        /// </summary>
+        public static bool AddDistinct<T>
+            (
+                [NotNull] this IList<T> list,
+                T value,
+                [NotNull] IEqualityComparer<T> comparer
+            )
+        {
+            Code.NotNull(list, "list");
+            Code.NotNull(comparer, "comparer");
+
+            if (list.ContainsValue(value, comparer))
+            {
+                return false;
+            }
+
+            list.Add(value);
+
+            return true;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static bool AddRangeDistinct<T>
+            (
+            [NotNull] this IList<T> list,
+            [NotNull] IEnumerable<T> values,
+            [NotNull] IEqualityComparer<T> comparer
+            )
+        {
+            Code.NotNull(list, "list");
+            Code.NotNull(values, "values");
+            Code.NotNull(comparer, "comparer");
+
+            bool allAdded = true;
+            foreach (T value in values)
+            {
+                if (!list.AddDistinct(value, comparer))
+                {
+                    allAdded = false;
+                }
+            }
+
+            return allAdded;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static bool ContainsValue<TSource>
+            (
+                [NotNull] this IEnumerable<TSource> source,
+                TSource value,
+                [NotNull] IEqualityComparer<TSource> comparer
+            )
+        {
+            Code.NotNull(comparer, "comparer");
+            Code.NotNull(source, "source");
+
+            foreach (TSource local in source)
+            {
+                if (comparer.Equals(local, value))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static int IndexOf<T>
+            (
+            [NotNull] this IEnumerable<T> collection,
+            [NotNull] Func<T, bool> predicate
+            )
+        {
+            Code.NotNull(collection, "collection");
+            Code.NotNull(predicate, "predicate");
+
+            int index = 0;
+            foreach (T value in collection)
+            {
+                if (predicate(value))
+                {
+                    return index;
+                }
+
+                index++;
+            }
+
+            return -1;
+        }
+
+        /// <summary>
+        /// Is the list is <c>null</c> or empty?
+        /// </summary>
+        public static bool IsNullOrEmpty<T>
+            (
+            [CanBeNull] this IList<T> list
+            )
+        {
+            if (!ReferenceEquals(list, null))
+            {
+                return list.Count == 0;
+            }
+
+            return true;
+        }
+
         #endregion
     }
 }
---
+++ UnitTests/AM/Collections/ListUtilityTest.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using AM.Collections;
+
+namespace UnitTests.AM.Collections
+{
+    [TestClass]
+    public class ListUtilityTest
+    {
+        [TestMethod]
+        public void TestListUtilityIsNullOrEmpty()
+        {
+            List<int> list = null;
+            Assert.IsTrue(list.IsNullOrEmpty());
+
+            list = new List<int>();
+            Assert.IsTrue(list.IsNullOrEmpty());
+
+            list.Add(1);
+            Assert.IsFalse(list.IsNullOrEmpty());
+        }
+
+        [TestMethod]
+        public void TestListUtilityContainsValue()
+        {
+            List<int> list = new List<int> { 1, 2, 3 };
+
+            Assert.IsTrue(list.ContainsValue
+                (
+                    1,
+                   EqualityComparer<int>.Default
+                ));
+
+            Assert.IsFalse(list.ContainsValue
+                (
+                    4,
+                   EqualityComparer<int>.Default
+                ));
+        }
+
+        [TestMethod]
+        public void TestListUtilityAddDistinct()
+        {
+            List<int> list = new List<int>{1,2,3};
+
+            Assert.IsTrue(list.AddDistinct(4));
+            Assert.IsFalse(list.AddDistinct(4));
+
+            IEqualityComparer<int> comparer
+                = EqualityComparer<int>.Default;
+
+            Assert.IsTrue(list.AddDistinct(4, comparer));
+            Assert.IsFalse(list.AddDistinct(4, comparer));
+        }
+
+        [TestMethod]
+        public void TestListUtilityAddRangeDistinct()
+        {
+            List<int> list = new List<int> { 1, 2, 3 };
+
+            IEqualityComparer<int> comparer
+                = EqualityComparer<int>.Default;
+
+            Assert.IsTrue(list.AddRangeDistinct(new[]{4,5,6}, comparer));
+            Assert.IsFalse(list.AddRangeDistinct(new[]{6,7,8}, comparer));
+        }
+
+        [TestMethod]
+        public void TestListUtilityIndexOf()
+        {
+            List<int> list = new List<int> { 1, 2, 3 };
+
+            Func<int, bool> predicate = i => i == 2;
+            Assert.AreEqual(1, list.IndexOf(predicate));
+
+            predicate = i => i == 4;
+            Assert.IsTrue(list.IndexOf(predicate) < 0);
+        }
+    }
+}
