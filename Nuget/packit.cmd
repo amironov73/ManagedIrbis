979f4ec2785c8dcd2b019da4ff0d1e84671c9249
amironov73 <amironov73@gmail.com>
2016-07-31 12:25:14 +0800

    Make Nuget build for 3.5-4.6.1

--- Nuget/packit.cmd
+++ Nuget/packit.cmd
@@ -10,16 +10,22 @@ if not exist tools mkdir tools > nul 2> nul
 
 cd lib
 
-if not exist net35 mkdir net35 > nul 2> nul
-if not exist net40 mkdir net40 > nul 2> nul
-if not exist net45 mkdir net45 > nul 2> nul
+if not exist net35  mkdir net35  > nul 2> nul
+if not exist net40  mkdir net40  > nul 2> nul
+if not exist net45  mkdir net45  > nul 2> nul
+if not exist net451 mkdir net451 > nul 2> nul
+if not exist net46  mkdir net46  > nul 2> nul
+if not exist net461 mkdir net461 > nul 2> nul
 
 del /s /q *.* > nul 2> nul
 cd ..
 
-copy %BIN%\%BUILD%35 lib\net35\ > nul
-copy %BIN%\%BUILD%40 lib\net40\ > nul
-copy %BIN%\%BUILD%45 lib\net45\ > nul
+copy %BIN%\%BUILD%35 lib\net35\  > nul
+copy %BIN%\%BUILD%40 lib\net40\  > nul
+copy %BIN%\%BUILD%45 lib\net45\  > nul
+copy %BIN%\%BUILD%45 lib\net451\ > nul
+copy %BIN%\%BUILD%45 lib\net46\  > nul
+copy %BIN%\%BUILD%45 lib\net461\ > nul
 
 cd lib
 del /s /q MoonIrbis.* > nul
