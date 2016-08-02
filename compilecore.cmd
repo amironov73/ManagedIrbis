978fd9264af3e3a5d38b623660598101063619e9
Alexey Mironov <amironov73@gmail.com>
2016-08-02 21:58:24 +0900

    compilecore.cmd again

--- compilecore.cmd
+++ compilecore.cmd
@@ -6,7 +6,7 @@ SET OUTPUT=..\Binaries
 SET PARAMS=/consoleloggerparameters:ErrorsOnly /m
 
 "%MSBUILD%" AM.Core\AM.Core.NetCore.xproj /target:ReBuild /property:Configuration=Debug   /property:OutputPath=%OUTPUT%\DebugCore  /p:DefineConstants="NETCORE"
-"%MSBUILD%" ManagedIrbis\ManagedIrbis.NetCore.xproj /target:ReBuild /property:Configuration=Debug   /property:OutputPath=%OUTPUT%\DebugCore /p:DefineConstants="NETCORE"
+rem "%MSBUILD%" ManagedIrbis\ManagedIrbis.NetCore.xproj /target:ReBuild /property:Configuration=Debug   /property:OutputPath=%OUTPUT%\DebugCore /p:DefineConstants="NETCORE"
 
 "%MSBUILD%" AM.Core\AM.Core.NetCore.xproj /target:ReBuild /property:Configuration=Release /property:OutputPath=%OUTPUT%\ReleaseCore /p:DefineConstants="NETCORE"
-"%MSBUILD%" ManagedIrbis\ManagedIrbis.NetCore.xproj /target:ReBuild /property:Configuration=Release /property:OutputPath=%OUTPUT%\ReleaseCore /p:DefineConstants="NETCORE"
+rem "%MSBUILD%" ManagedIrbis\ManagedIrbis.NetCore.xproj /target:ReBuild /property:Configuration=Release /property:OutputPath=%OUTPUT%\ReleaseCore /p:DefineConstants="NETCORE"
