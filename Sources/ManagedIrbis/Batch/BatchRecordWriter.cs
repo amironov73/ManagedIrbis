14c2a9ef68cf571c7849b262a9c2efd56d6b55dd
Alexey Mironov <amironov73@gmail.com>
2016-08-07 12:41:02 +0800

    Add BatchRecordWriter

---
+++ Sources/ManagedIrbis/Batch/BatchRecordWriter.cs
@@ -0,0 +1,211 @@
+﻿/* BatchRecordWriter.cs -- batch record writer
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AM;
+
+using CodeJam;
+
+using JetBrains.Annotations;
+
+using MoonSharp.Interpreter;
+
+using Newtonsoft.Json;
+
+#endregion
+
+namespace ManagedIrbis.Batch
+{
+    /// <summary>
+    /// Batch writer for <see cref="MarcRecord"/>.
+    /// </summary>
+    [PublicAPI]
+    [MoonSharpUserData]
+    public sealed class BatchRecordWriter
+        : IDisposable
+    {
+        #region Events
+
+        /// <summary>
+        /// Raised on batch write.
+        /// </summary>
+        public event EventHandler BatchWrite;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Actualize records when writing.
+        /// </summary>
+        public bool Actualize { get; set; }
+
+        /// <summary>
+        /// Capacity.
+        /// </summary>
+        public int Capacity { get; private set; }
+
+        /// <summary>
+        /// Connection.
+        /// </summary>
+        [NotNull]
+        public IrbisConnection Connection { get; private set; }
+
+        /// <summary>
+        /// Database name.
+        /// </summary>
+        [NotNull]
+        public string Database { get; private set; }
+
+        /// <summary>
+        /// Total number of records written.
+        /// </summary>
+        public int RecordsWritten { get; private set; }
+
+        #endregion
+
+        #region Construction
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public BatchRecordWriter
+            (
+                [NotNull] IrbisConnection connection,
+                [NotNull] string database,
+                int capacity
+            )
+        {
+            Code.NotNull(connection, "connection");
+            Code.NotNullNorEmpty(database, "database");
+            if (capacity < 1)
+            {
+                throw new ArgumentOutOfRangeException("capacity");
+            }
+
+            Connection = connection;
+            Database = database;
+            Capacity = capacity;
+            Actualize = true;
+            _buffer = new List<MarcRecord>(capacity);
+            _syncRoot = new object();
+        }
+
+        #endregion
+
+        #region Private members
+
+        private readonly List<MarcRecord> _buffer;
+
+        private readonly object _syncRoot;
+
+        #endregion
+
+        #region Public methods
+
+        /// <summary>
+        /// Add many records.
+        /// </summary>
+        [NotNull]
+        public BatchRecordWriter AddRange
+            (
+                [NotNull] IEnumerable<MarcRecord> records
+            )
+        {
+            Code.NotNull(records, "records");
+
+            lock (_syncRoot)
+            {
+                foreach (MarcRecord record in records)
+                {
+                    Append(record);
+                }
+            }
+
+            return this;
+        }
+
+        /// <summary>
+        /// Append one record.
+        /// </summary>
+        [NotNull]
+        public BatchRecordWriter Append
+            (
+                [NotNull] MarcRecord record
+            )
+        {
+            Code.NotNull(record, "record");
+
+            lock(_syncRoot)
+            {
+                _buffer.Add(record);
+                if (_buffer.Count >= Capacity)
+                {
+                    Flush();
+                }
+            }
+
+            return this;
+        }
+
+        /// <summary>
+        /// Flush the buffer.
+        /// </summary>
+        [NotNull]
+        public BatchRecordWriter Flush()
+        {
+            lock(_syncRoot)
+            {
+                if (_buffer.Count != 0)
+                {
+                    try
+                    {
+                        Connection.PushDatabase(Database);
+                        Connection.WriteRecords
+                            (
+                                _buffer.ToArray(),
+                                false,
+                                Actualize
+                            );
+
+                        RecordsWritten += _buffer.Count;
+                    }
+                    finally
+                    {
+                        Connection.PopDatabase();
+                    }
+
+                    BatchWrite.Raise(this);
+                }
+
+                _buffer.Clear();
+            }
+
+            return this;
+        }
+
+        #endregion
+
+        #region IDisposable members
+
+        /// <summary>
+        /// Dispose the batch writer.
+        /// </summary>
+        public void Dispose()
+        {
+            Flush();
+        }
+
+        #endregion
+    }
+}
