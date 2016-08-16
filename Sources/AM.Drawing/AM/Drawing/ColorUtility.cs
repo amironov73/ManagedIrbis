1876f72419c588abca39201b85d762602c8a6a5c
Alexey Mironov <amironov73@gmail.com>
2016-08-16 15:35:24 +0800

    Add AM.Drawing solution

---
+++ Sources/AM.Drawing/AM/Drawing/ColorUtility.cs
@@ -0,0 +1,195 @@
+﻿/* ColorUtility.cs -- useful routines for color manipulations
+ * Ars Magna project, http://arsmagna.ru 
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using System;
+using System.Diagnostics;
+using System.Drawing;
+
+using CodeJam;
+
+using JetBrains.Annotations;
+
+#endregion
+
+namespace AM.Drawing
+{
+    /// <summary>
+    /// <see cref="Color"/> manipulation helper methods.
+    /// </summary>
+    [PublicAPI]
+    public static class ColorUtility
+    {
+        #region Private members
+
+        /// <summary>
+        /// For comparing with zero.
+        /// </summary>
+        private const double Tolerance = 0.001;
+
+        /// <summary>
+        /// Нормализация компонента цвета.
+        /// </summary>
+        private static int _Normalize
+            (
+                float component
+            )
+        {
+            return Math.Max(0, Math.Min(255, (int)component));
+        }
+
+        private static int _ToRGB1
+            (
+                float rm1,
+                float rm2,
+                float rh
+            )
+        {
+            if (rh > 360.0f)
+            {
+                rh -= 360.0f;
+            }
+            else if (rh < 0.0f)
+            {
+                rh += 360.0f;
+            }
+
+            if (rh < 60.0f)
+            {
+                rm1 = rm1 + (rm2 - rm1) * rh / 60.0f;
+            }
+            else if (rh < 180.0f)
+            {
+                rm1 = rm2;
+            }
+            else if (rh < 240.0f)
+            {
+                rm1 = rm1 + (rm2 - rm1) * (240.0f - rh) / 60.0f;
+            }
+
+            return _Normalize(rm1 * 255);
+        }
+
+        #endregion
+
+        #region Public methods
+
+        /// <summary>
+        /// Смешение двух цветов в заданной пропорции.
+        /// </summary>
+        /// <param name="color1">Первый цвет.</param>
+        /// <param name="color2">Второй цвет.</param>
+        /// <param name="amount">Доля второго цвета (число от 0 до 1,
+        /// 0 - остается только первый цвет, 1 - остается только второй
+        /// цвет).</param>
+        /// <returns>Смешанный цвет.</returns>
+        public static Color Blend
+            (
+                Color color1,
+                Color color2,
+                float amount
+            )
+        {
+            float amount1 = 1f - amount;
+            int red = _Normalize(color1.R * amount1 + color2.R * amount);
+            int green = _Normalize(color1.G * amount1 + color2.G * amount);
+            int blue = _Normalize(color1.B * amount1 + color2.B * amount);
+
+            return Color.FromArgb(red, green, blue);
+        }
+
+        /// <summary>
+        /// Затемнение цвета (на самом деле лишь подмешивание черного цвета).
+        /// </summary>
+        /// <param name="color">Затемняемый цвет.</param>
+        /// <param name="amount">Степень затемнения (доля черного цвета,
+        /// число от 0 до 1).</param>
+        /// <returns>Затемненный цвет.</returns>
+        public static Color Darken
+            (
+                Color color,
+                float amount
+            )
+        {
+            return Blend(color, Color.Black, amount);
+        }
+
+        /// <summary>
+        /// Создает цвет из компонент "Hue/Luminance/Saturation".
+        /// </summary>
+        public static Color FromHls
+            (
+                float hue,
+                float luminance,
+                float saturation
+            )
+        {
+            Debug.Assert((hue >= 0f) && (hue <= 360f));
+            Debug.Assert((luminance >= 0f) && (luminance <= 1f));
+            Debug.Assert((saturation >= 0f) && (saturation <= 1f));
+
+            int red, green, blue;
+            if (Math.Abs(saturation) < Tolerance)
+            {
+                red = _Normalize(luminance * 255.0F);
+                green = red;
+                blue = red;
+            }
+            else
+            {
+                float rm2;
+
+                if (luminance <= 0.5f)
+                {
+                    rm2 = luminance + luminance * saturation;
+                }
+                else
+                {
+                    rm2 = luminance + saturation
+                        - luminance * saturation;
+                }
+                var rm1 = 2.0f * luminance - rm2;
+                red = _ToRGB1(rm1, rm2, hue + 120.0f);
+                green = _ToRGB1(rm1, rm2, hue);
+                blue = _ToRGB1(rm1, rm2, hue - 120.0f);
+            }
+
+            return Color.FromArgb(red, green, blue);
+        }
+
+        /// <summary>
+        /// Осветление цвета (на самом деле лишь подмешнивание белого цвета).
+        /// </summary>
+        /// <param name="color">Осветляемый цвет.</param>
+        /// <param name="amount">Степень осветления (доля белого цвета,
+        /// число от 0 до 1).</param>
+        /// <returns>Осветленный цвет.</returns>
+        public static Color Lighten
+            (
+                Color color,
+                float amount
+            )
+        {
+            return Blend(color, Color.White, amount);
+        }
+
+        /// <summary>
+        /// Convert color to Blue-Green-Red representation.
+        /// </summary>
+        /// <param name="color">Color value.</param>
+        /// <returns></returns>
+        public static int ToBgr
+            (
+                Color color
+            )
+        {
+            return unchecked((((color.B << 8) + color.G) << 8) + color.R);
+        }
+
+        #endregion
+    }
+}
---
+++ Sources/AM.Drawing/AM/Drawing/FontUtility.cs
@@ -0,0 +1,114 @@
+﻿/* ColorUtility.cs -- font manipulation helpers
+ * Ars Magna project, http://arsmagna.ru 
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using System;
+using System.Diagnostics;
+using System.Drawing;
+
+using CodeJam;
+
+using JetBrains.Annotations;
+
+#endregion
+
+namespace AM.Drawing
+{
+    /// <summary>
+    /// <see cref="Font"/> manipulation helper methods.
+    /// </summary>
+    [PublicAPI]
+    public static class FontUtility
+    {
+        #region Private members
+
+        #endregion
+
+        #region Public methods
+
+        /// <summary>
+        /// Converts <see cref="Font"/> to a <see cref="String"/>
+        /// (e. g. for XML serialization).
+        /// </summary>
+        [NotNull]
+        public static string FontToString
+            (
+                [NotNull] Font font
+            )
+        {
+            Code.NotNull(font, "font");
+
+            string result = string.Format
+                (
+                    "{0}|{1}|{2}|{3}|{4}|{5}|{6}",
+                    font.Name,
+                    font.Size,
+                    font.Unit,
+                    font.Bold,
+                    font.Italic,
+                    font.Underline,
+                    font.Strikeout
+                );
+            return result;
+        }
+
+        /// <summary>
+        /// Restores <see cref="Font"/> from a <see cref="String"/>
+        /// (e. g. during XML deserialization).
+        /// </summary>
+        [NotNull]
+        public static Font StringToFont
+            (
+                [NotNull] string fontSpecification
+            )
+        {
+            Code.NotNullNorEmpty(fontSpecification, "fontSpecification");
+
+            string[] parts = fontSpecification.Split('|');
+            if (parts.Length != 7)
+            {
+                throw new ArgumentException("fontSpecification");
+            }
+            string fontName = parts[0];
+            float emSize = float.Parse(parts[1]);
+            GraphicsUnit unit
+                = (GraphicsUnit)Enum.Parse(typeof(GraphicsUnit), parts[2]);
+            bool bold = bool.Parse(parts[3]);
+            bool italic = bool.Parse(parts[4]);
+            bool underline = bool.Parse(parts[5]);
+            bool strikeout = bool.Parse(parts[6]);
+            FontStyle style = FontStyle.Regular;
+            if (bold)
+            {
+                style |= FontStyle.Bold;
+            }
+            if (italic)
+            {
+                style |= FontStyle.Italic;
+            }
+            if (underline)
+            {
+                style |= FontStyle.Underline;
+            }
+            if (strikeout)
+            {
+                style |= FontStyle.Strikeout;
+            }
+            Font result = new Font
+                (
+                    fontName,
+                    emSize,
+                    style,
+                    unit
+                );
+
+            return result;
+        }
+
+        #endregion
+    }
+}
