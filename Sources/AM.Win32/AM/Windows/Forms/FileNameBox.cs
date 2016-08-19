21d962c49b5b60cc2938e5c921348f71a30dece2
Alexey Mironov <amironov73@gmail.com>
2016-08-19 14:35:55 +0800

    Add FileNameBox

---
+++ Sources/AM.Win32/AM/Windows/Forms/FileNameBox.cs
@@ -0,0 +1,74 @@
+﻿/* FileNameBox.cs -- TextBox with button and OpenFileDialog
+ * Ars Magna project, http://arsmagna.ru 
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using AM.Win32;
+
+using CodeJam;
+
+using JetBrains.Annotations;
+
+#endregion
+
+namespace AM.Windows.Forms
+{
+    /// <summary>
+    /// TextBox with embedded Button and OpenFileDialog.
+    /// </summary>
+    [PublicAPI]
+    [System.ComponentModel.DesignerCategory("Code")]
+    public class FileNameBox
+        : ButtonedTextBox
+    {
+        #region Properties
+
+        #endregion
+
+        #region Construction
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public FileNameBox()
+        {
+            Button.Click += _Button_Click;
+        }
+
+        #endregion
+
+        #region Private members
+
+        void _Button_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog
+            {
+                FileName = Text
+            };
+
+            if (dialog.ShowDialog(this) == DialogResult.OK)
+            {
+                Text = dialog.FileName;
+            }
+        }
+
+        #endregion
+
+        #region Public methods
+
+        #endregion
+    }
+}
