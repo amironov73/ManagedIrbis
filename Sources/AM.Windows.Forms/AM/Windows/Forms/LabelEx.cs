a7a3c9be7c182e5b4114e85c436ce16987885581
Alexey Mironov <amironov73@gmail.com>
2016-08-20 13:41:43 +0800

    Add LabelEx

---
+++ Sources/AM.Windows.Forms/AM/Windows/Forms/LabelEx.cs
@@ -0,0 +1,75 @@
+﻿/* LabelEx.cs -- 
+ * Ars Magna project, http://arsmagna.ru 
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+using JetBrains.Annotations;
+
+#endregion
+
+namespace AM.Windows.Forms
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [PublicAPI]
+    [System.ComponentModel.DesignerCategory("Code")]
+    public class LabelEx
+        : Label
+    {
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the buddy control.
+        /// </summary>
+        /// <value>The buddy control.</value>
+        [System.ComponentModel.DefaultValue(null)]
+        public Control BuddyControl { get; set; }
+
+        #endregion
+
+        #region Private members
+
+        #endregion
+
+        #region Public methods
+
+        /// <summary>
+        /// Transfers the focus to buddy control.
+        /// </summary>
+        public void TransferFocusToBuddyControl()
+        {
+            if (BuddyControl != null)
+            {
+                BuddyControl.Focus();
+            }
+        }
+
+        #endregion
+
+        #region Control members
+
+        /// <summary>
+        /// Raises the
+        /// <see cref="E:System.Windows.Forms.Control.Click"/>
+        /// event.
+        /// </summary>
+        protected override void OnClick
+            (
+                EventArgs e
+            )
+        {
+            base.OnClick(e);
+            TransferFocusToBuddyControl();
+        }
+
+        #endregion
+    }
+}
