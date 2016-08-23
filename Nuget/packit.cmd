2e2d6651f93c39dc94b934b0617c5245e375aad1
Alexey Mironov <amironov73@gmail.com>
2016-08-23 21:08:25 +0800

    Add .NET Core to NuGet

--- Nuget/packit.cmd
+++ Nuget/packit.cmd
@@ -8,13 +8,13 @@ DEL /q *.nupkg > nul 2> nul
 IF not exist lib mkdir lib   > nul 2> nul
 IF not exist tools mkdir tools > nul 2> nul
 
-CALL :BUILD AM.Core
-CALL :BUILD ManagedIrbis
-CALL :BUILD AM.Drawing
-CALL :BUILD AM.Rfid
-CALL :BUILD AM.Windows.Forms
-CALL :BUILD AM.Win32
-CALL :BUILD IrbisUI
+CALL :BUILD AM.Core            core
+CALL :BUILD ManagedIrbis       core
+CALL :BUILD AM.Drawing         no
+CALL :BUILD AM.Rfid            no
+CALL :BUILD AM.Windows.Forms   no
+CALL :BUILD AM.Win32           no
+CALL :BUILD IrbisUI            no
 
 GOTO :END
 
@@ -23,16 +23,24 @@ ECHO BUILD %1
 
 cd lib
 
-IF not exist net35  mkdir net35  > nul 2> nul
-IF not exist net40  mkdir net40  > nul 2> nul
-IF not exist net45  mkdir net45  > nul 2> nul
+del /s /q *.*   > nul 2> nul
+rmdir /s /q *.* > nul 2> nul
 
-del /s /q *.* > nul 2> nul
+IF not exist net35 mkdir net35 > nul 2> nul
+IF not exist net40 mkdir net40 > nul 2> nul
+IF not exist net45 mkdir net45 > nul 2> nul
+
+IF %2==core mkdir netstandard1.0  > nul 2> nul
+IF %2==no   rmdir netstandard1.0  > nul 2> nul
+
 cd ..
 
 copy %BIN%\%BUILD%35\%1.* lib\net35\  > nul
 copy %BIN%\%BUILD%40\%1.* lib\net40\  > nul
 copy %BIN%\%BUILD%45\%1.* lib\net45\  > nul
+
+IF %2==core copy %BIN%\%BUILD%Core\%1\bin\%BUILD%\netstandard1.6\%1.* lib\netstandard1.0 > nul
+DEL lib\netstandard1.0\*.json > nul 2> nul
 
 PatchNugetVersion.exe %BIN%\%BUILD%40\AM.Core.dll %1.nuspec
 
