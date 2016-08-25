82e7ef495d5fae43e3cc51aced28ac2a7ebd77ba
Alexey Mironov <amironov73@gmail.com>
2016-08-25 18:36:53 +0800

    Add CompressionUtility

---
+++ Sources/Libs/AM.Core/AM/Diagnostics/ConsoleTraceListener.cs
@@ -0,0 +1,51 @@
+﻿/* ConsoleTraceListener.cs -- console TRACE listener.
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using System;
+using System.Diagnostics;
+
+using JetBrains.Annotations;
+
+#endregion
+
+namespace AM.Diagnostics
+{
+    /// <summary>
+    /// Console TRACE listener.
+    /// </summary>
+    [PublicAPI]
+    public sealed class ConsoleTraceListener
+        : TextWriterTraceListener
+    {
+        #region Construction
+
+        /// <summary>
+        /// Initializes a new instance of the 
+        /// <see cref="ConsoleTraceListener"/> class.
+        /// </summary>
+        public ConsoleTraceListener()
+            : base(Console.Out)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the 
+        /// <see cref="ConsoleTraceListener"/> class.
+        /// </summary>
+        /// <param name="initializationData">
+        /// The initialization data.</param>
+        /// <remarks>Called by runtime.</remarks>
+        public ConsoleTraceListener(string initializationData)
+            : this()
+        {
+            Trace.WriteLine(initializationData);
+        }
+
+        #endregion
+    }
+}
---
+++ Sources/UnitTests/AM/IO/CompressionUtilityTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using AM.IO;
+
+namespace UnitTests.AM.IO
+{
+    [TestClass]
+    public class CompressionUtilityTest
+    {
+        private void _TestCompression
+            (
+                byte[] first
+            )
+        {
+            byte[] zipped = CompressionUtility.Compress(first);
+
+            byte[] second = CompressionUtility.Decompress(zipped);
+
+            Assert.AreEqual(first.Length, second.Length);
+
+            for (int i = 0; i < first.Length; i++)
+            {
+                Assert.AreEqual(first[i], second[i]);
+            }
+        }
+
+        [TestMethod]
+        public void TestCompressionUtility()
+        {
+            byte[] bytes = new byte[0];
+            _TestCompression(bytes);
+
+            bytes = new byte[] { 0 };
+            _TestCompression(bytes);
+
+            bytes = new byte[] { 0, 1, 2, 3 };
+            _TestCompression(bytes);
+        }
+    }
+}
