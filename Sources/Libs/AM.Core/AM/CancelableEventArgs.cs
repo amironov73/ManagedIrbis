c2c3d07a76bb95b070c0d3922be1f1321464b3a0
Alexey Mironov <amironov73@gmail.com>
2016-08-25 19:11:10 +0800

    Add CancelableEventArgs and Handler

---
+++ Sources/Libs/AM.Core/AM/CancelableEventArgs.cs
@@ -0,0 +1,74 @@
+﻿/* CancelableEventArgs.cs -- Event arguments for cancelable handling
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using System;
+
+using CodeJam;
+
+using JetBrains.Annotations;
+
+#endregion
+
+namespace AM
+{
+    /// <summary>
+    /// <see cref="T:System.EventArgs"/> for cancelable
+    /// handling.
+    /// </summary>
+    [PublicAPI]
+    public class CancelableEventArgs
+        : EventArgs
+    {
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets a value indicating whether
+        /// event handling must be canceled.
+        /// </summary>
+        /// <value><c>true</c> if cancel; 
+        /// otherwise, <c>false</c>.</value>
+        public bool Cancel { get; set; }
+
+        #endregion
+
+        #region Public methods
+
+        /// <summary>
+        /// Handles the event with specified sender and handler.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="handler">The handler.</param>
+        /// <returns><c>true</c> if handling was canceled,
+        /// <c>false</c> otherwise.</returns>
+        public bool Handle
+            (
+                object sender,
+                CancelableEventHandler handler
+            )
+        {
+            if (handler == null)
+            {
+                return false;
+            }
+
+            Delegate[] list = handler.GetInvocationList();
+            foreach (CancelableEventHandler eventHandler in list)
+            {
+                eventHandler(sender, this);
+                if (Cancel)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        #endregion
+    }
+}
---
+++ Sources/Libs/AM.Core/AM/CancelableEventHandler.cs
@@ -0,0 +1,19 @@
+﻿/* CancelableEventHandler.cs -- Handler of cancellable event.
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+namespace AM
+{
+    /// <summary>
+    /// Handler of event whose handling can be cancelled.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="ea"></param>
+    public delegate void CancelableEventHandler
+        (
+            object sender,
+            CancelableEventArgs ea
+        );
+}
