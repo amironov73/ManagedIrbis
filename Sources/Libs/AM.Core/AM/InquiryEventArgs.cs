0094a7720e2bbe3cf651ec5e933fa8872d364e84
Alexey Mironov <amironov73@gmail.com>
2016-08-25 19:27:56 +0800

    Add InquiryEventArgs

---
+++ Sources/Libs/AM.Core/AM/InquiryEventArgs.cs
@@ -0,0 +1,33 @@
+﻿/* InquiryEventArgs.cs -- 
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using System;
+
+using JetBrains.Annotations;
+
+#endregion
+
+namespace AM
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [PublicAPI]
+    public class InquiryEventArgs
+        : EventArgs
+    {
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets a success indication.
+        /// </summary>
+        public bool Success { get; set; }
+
+        #endregion
+    }
+}
