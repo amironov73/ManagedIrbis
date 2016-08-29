efd2a7670535cedf493f0a7d5d0f4d6ac6d4ce3c
Alexey Mironov <amironov73@gmail.com>
2016-08-29 19:51:17 +0800

    Dependencies handling for AM.Rfid

--- Nuget/packit.cmd
+++ Nuget/packit.cmd
@@ -42,6 +42,49 @@ copy %BIN%\%BUILD%35\System.Threading.* lib\net35\ > nul
 
 IF %2==core copy %BIN%\%BUILD%Core\%1\bin\%BUILD%\netstandard1.6\%1.* lib\netstandard1.0 > nul
 DEL lib\netstandard1.0\*.json > nul 2> nul
+IF %1==AM.Rfid (
+copy %BIN%\%BUILD%35\FeCom.dll                lib\net35 > nul
+copy %BIN%\%BUILD%35\FedmIscCoreVC80.dll      lib\net35 > nul
+copy %BIN%\%BUILD%35\FedmIscMyAxxessVC80.dll  lib\net35 > nul
+copy %BIN%\%BUILD%35\fefu.dll                 lib\net35 > nul
+copy %BIN%\%BUILD%35\FeIsc.dll                lib\net35 > nul
+copy %BIN%\%BUILD%35\fetcl.dll                lib\net35 > nul
+copy %BIN%\%BUILD%35\fetcp.dll                lib\net35 > nul
+copy %BIN%\%BUILD%35\feusb.dll                lib\net35 > nul
+copy %BIN%\%BUILD%35\OBIDISC4NET.dll          lib\net35 > nul
+copy %BIN%\%BUILD%35\OBIDISC4NETnative.dll    lib\net35 > nul
+copy %BIN%\%BUILD%35\pcsc-sharp.dll           lib\net35 > nul
+copy %BIN%\%BUILD%35\pcsc-sharp.pdb           lib\net35 > nul
+copy %BIN%\%BUILD%35\pcsc-sharp.xml           lib\net35 > nul
+
+copy %BIN%\%BUILD%40\FeCom.dll                lib\net40 > nul
+copy %BIN%\%BUILD%40\FedmIscCoreVC100.dll     lib\net40 > nul
+copy %BIN%\%BUILD%40\FedmIscMyAxxessVC100.dll lib\net40 > nul
+copy %BIN%\%BUILD%40\fefu.dll                 lib\net40 > nul
+copy %BIN%\%BUILD%40\FeIsc.dll                lib\net40 > nul
+copy %BIN%\%BUILD%40\fetcl.dll                lib\net40 > nul
+copy %BIN%\%BUILD%40\fetcp.dll                lib\net40 > nul
+copy %BIN%\%BUILD%40\feusb.dll                lib\net40 > nul
+copy %BIN%\%BUILD%40\OBIDISC4NET.dll          lib\net40 > nul
+copy %BIN%\%BUILD%40\OBIDISC4NETnative.dll    lib\net40 > nul
+copy %BIN%\%BUILD%40\pcsc-sharp.dll           lib\net40 > nul
+copy %BIN%\%BUILD%40\pcsc-sharp.pdb           lib\net40 > nul
+copy %BIN%\%BUILD%40\pcsc-sharp.xml           lib\net40 > nul
+
+copy %BIN%\%BUILD%45\FeCom.dll                lib\net45 > nul
+copy %BIN%\%BUILD%45\FedmIscCoreVC100.dll     lib\net45 > nul
+copy %BIN%\%BUILD%45\FedmIscMyAxxessVC100.dll lib\net45 > nul
+copy %BIN%\%BUILD%45\fefu.dll                 lib\net45 > nul
+copy %BIN%\%BUILD%45\FeIsc.dll                lib\net45 > nul
+copy %BIN%\%BUILD%45\fetcl.dll                lib\net45 > nul
+copy %BIN%\%BUILD%45\fetcp.dll                lib\net45 > nul
+copy %BIN%\%BUILD%45\feusb.dll                lib\net45 > nul
+copy %BIN%\%BUILD%45\OBIDISC4NET.dll          lib\net45 > nul
+copy %BIN%\%BUILD%45\OBIDISC4NETnative.dll    lib\net45 > nul
+copy %BIN%\%BUILD%45\pcsc-sharp.dll           lib\net45 > nul
+copy %BIN%\%BUILD%45\pcsc-sharp.pdb           lib\net45 > nul
+copy %BIN%\%BUILD%45\pcsc-sharp.xml           lib\net45 > nul
+)
 
 PatchNugetVersion.exe %BIN%\%BUILD%40\AM.Core.dll %1.nuspec
 
