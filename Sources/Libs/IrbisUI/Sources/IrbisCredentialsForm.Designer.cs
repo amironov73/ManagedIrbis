11f5bbfe3dc07a918e52812ae0862d32d91fb050
Alexey Mironov <amironov73@gmail.com>
2016-09-03 16:47:16 +0800

    Add IrbisCredentialsForm

---
+++ Sources/Libs/IrbisUI/Sources/IrbisCredentialsForm.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace IrbisUI
+{
+    partial class IrbisCredentialsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this._serverBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this._portBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this._loginBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this._passwordBox = new System.Windows.Forms.TextBox();
+            this._okButton = new System.Windows.Forms.Button();
+            this._cancelButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Сервер";
+            // 
+            // _serverBox
+            // 
+            this._serverBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this._serverBox.Location = new System.Drawing.Point(12, 36);
+            this._serverBox.Name = "_serverBox";
+            this._serverBox.Size = new System.Drawing.Size(319, 20);
+            this._serverBox.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Порт";
+            // 
+            // _portBox
+            // 
+            this._portBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this._portBox.Location = new System.Drawing.Point(12, 89);
+            this._portBox.Name = "_portBox";
+            this._portBox.Size = new System.Drawing.Size(319, 20);
+            this._portBox.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Пользователь";
+            // 
+            // _loginBox
+            // 
+            this._loginBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this._loginBox.Location = new System.Drawing.Point(15, 145);
+            this._loginBox.Name = "_loginBox";
+            this._loginBox.Size = new System.Drawing.Size(316, 20);
+            this._loginBox.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 189);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Пароль";
+            // 
+            // _passwordBox
+            // 
+            this._passwordBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this._passwordBox.Location = new System.Drawing.Point(15, 205);
+            this._passwordBox.Name = "_passwordBox";
+            this._passwordBox.PasswordChar = '*';
+            this._passwordBox.Size = new System.Drawing.Size(316, 20);
+            this._passwordBox.TabIndex = 7;
+            // 
+            // _okButton
+            // 
+            this._okButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this._okButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this._okButton.Location = new System.Drawing.Point(175, 246);
+            this._okButton.Name = "_okButton";
+            this._okButton.Size = new System.Drawing.Size(75, 23);
+            this._okButton.TabIndex = 8;
+            this._okButton.Text = "&ОК";
+            this._okButton.UseVisualStyleBackColor = true;
+            // 
+            // _cancelButton
+            // 
+            this._cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this._cancelButton.Location = new System.Drawing.Point(256, 246);
+            this._cancelButton.Name = "_cancelButton";
+            this._cancelButton.Size = new System.Drawing.Size(75, 23);
+            this._cancelButton.TabIndex = 9;
+            this._cancelButton.Text = "О&тмена";
+            this._cancelButton.UseVisualStyleBackColor = true;
+            // 
+            // CredentialsForm
+            // 
+            this.AcceptButton = this._okButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this._cancelButton;
+            this.ClientSize = new System.Drawing.Size(343, 281);
+            this.ControlBox = false;
+            this.Controls.Add(this._cancelButton);
+            this.Controls.Add(this._okButton);
+            this.Controls.Add(this._passwordBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this._loginBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this._portBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this._serverBox);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "CredentialsForm";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Введите данные для подключения к серверу";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox _serverBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox _portBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox _loginBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox _passwordBox;
+        private System.Windows.Forms.Button _okButton;
+        private System.Windows.Forms.Button _cancelButton;
+    }
+}
---
+++ Sources/Libs/IrbisUI/Sources/IrbisCredentialsForm.cs
@@ -0,0 +1,90 @@
+﻿/* IrbisCredentialsForm.cs --
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using CodeJam;
+
+using JetBrains.Annotations;
+
+using ManagedIrbis;
+
+using MoonSharp.Interpreter;
+
+#endregion
+
+namespace IrbisUI
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [PublicAPI]
+    [MoonSharpUserData]
+    public partial class IrbisCredentialsForm
+        : Form
+    {
+        #region Properties
+
+        /// <summary>
+        /// Server.
+        /// </summary>
+        public string Server
+        {
+            get { return _serverBox.Text; }
+            set { _serverBox.Text = value; }
+        }
+
+        /// <summary>
+        /// Port.
+        /// </summary>
+        public string Port
+        {
+            get { return _portBox.Text; }
+            set { _portBox.Text = value; }
+        }
+
+        /// <summary>
+        /// User.
+        /// </summary>
+        public string User
+        {
+            get { return _loginBox.Text; }
+            set { _loginBox.Text = value; }
+        }
+
+        /// <summary>
+        /// Password.
+        /// </summary>
+        public string Password
+        {
+            get { return _passwordBox.Text; }
+            set { _passwordBox.Text = value; }
+        }
+
+        #endregion
+
+        #region Construction
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public IrbisCredentialsForm()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+    }
+}
