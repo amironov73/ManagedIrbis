bf8863ec6423015af8a3ba59025915d8fb6c8cce
Alexey Mironov <amironov73@gmail.com>
2016-09-04 12:02:16 +0800

    Add CollapsibleGroupBoxTest

---
+++ Sources/UITests/Sources/Tests/CollapsibleGroupBoxTest.cs
@@ -0,0 +1,73 @@
+﻿/* CollapsibleGroupBoxTest.cs -- 
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using AM;
+using AM.Windows.Forms;
+
+using CodeJam;
+
+using IrbisUI;
+
+using JetBrains.Annotations;
+
+using ManagedIrbis;
+
+using MoonSharp.Interpreter;
+
+using Newtonsoft.Json;
+
+#endregion
+
+namespace UITests
+{
+    public sealed class CollapsibleGroupBoxTest
+        : IUITest
+    {
+        #region IUITest members
+
+        public void Run
+            (
+                IWin32Window ownerWindow
+            )
+        {
+            using (Form form = new Form())
+            {
+                form.Size = new Size(800, 600);
+
+                CollapsibleGroupBox box1 = new CollapsibleGroupBox
+                {
+                    BackColor = Color.DarkSeaGreen,
+                    Text = "Group box 1",
+                    Location = new Point(10, 10),
+                    Size = new Size(400, 200)
+                };
+                form.Controls.Add(box1);
+                CollapsibleGroupBox box2 = new CollapsibleGroupBox
+                {
+                    BackColor = Color.BlanchedAlmond,
+                    Text = "Group box 2",
+                    Location = new Point(420, 10),
+                    Size = new Size(300, 200)
+                };
+                form.Controls.Add(box2);
+
+                form.ShowDialog(ownerWindow);
+            }
+        }
+
+        #endregion
+    }
+}
