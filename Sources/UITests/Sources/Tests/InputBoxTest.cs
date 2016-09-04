46b02a17a4d71149ea238f5ea0a7f1b78b33bb53
Alexey Mironov <amironov73@gmail.com>
2016-09-04 13:04:46 +0800

    Add InputBoxTest

---
+++ Sources/UITests/Sources/Tests/InputBoxTest.cs
@@ -0,0 +1,67 @@
+﻿/* InputBoxTest.cs -- 
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using AM;
+using AM.Windows.Forms;
+
+using CodeJam;
+
+using IrbisUI;
+
+using JetBrains.Annotations;
+
+using ManagedIrbis;
+
+using MoonSharp.Interpreter;
+
+using Newtonsoft.Json;
+
+#endregion
+
+namespace UITests
+{
+    public sealed class InputBoxTest
+        : IUITest
+    {
+        #region IUITest members
+
+        public void Run
+            (
+                IWin32Window ownerWindow
+            )
+        {
+            string theValue = "Default value";
+            DialogResult dialogResult = InputBox.Query
+                (
+                    "Testing the components",
+                    "Enter something",
+                    "Please, enter something",
+                    ref theValue
+                );
+
+            string text = string.Format
+                (
+                    "Result: {0}{1}Value: {2}",
+                    dialogResult,
+                    Environment.NewLine,
+                    theValue
+                );
+            MessageBox.Show(text);
+        }
+
+        #endregion
+    }
+}
