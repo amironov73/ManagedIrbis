aa3a460760d386c538c1e4a2255d96b56b502db7
Alexey Mironov <amironov73@gmail.com>
2016-09-05 12:00:43 +0800

    Add ComparableObject

---
+++ Sources/Libs/AM.Core/AM/ComparableObject.cs
@@ -0,0 +1,232 @@
+﻿/* ComparableObject.cs --
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#if !NETCORE
+
+#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+using CodeJam;
+
+using JetBrains.Annotations;
+
+using MoonSharp.Interpreter;
+
+#endregion
+
+namespace AM
+{
+    /// <summary>
+    /// Class with automatic Equals method.
+    /// </summary>
+    /// <remarks>Borrowed from Brad Smith's coding blog:
+    /// http://www.brad-smith.info/blog/archives/385
+    /// </remarks>
+    [PublicAPI]
+    [MoonSharpUserData]
+    public class ComparableObject
+    {
+        #region Nested classes
+
+        /// <summary>
+        /// Used to hold delegates for the compiled methods.
+        /// </summary>
+        private class MemberwiseFunctions
+        {
+            /// <summary>
+            /// Delegate for the Equals method.
+            /// </summary>
+            public Func<object, object, bool> EqualsFunc;
+            /// <summary>
+            /// Delegate for the GetHashCode method.
+            /// </summary>
+            public Func<object, int> GetHashCodeFunc;
+        }
+
+        #endregion
+
+        #region Construction
+
+        /// <summary>
+        /// Static constructor.
+        /// </summary>
+        static ComparableObject()
+        {
+            _functions = new Dictionary<Type, MemberwiseFunctions>();
+        }
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <remarks>Dynamically compiles the Equals/GetHashCode
+        /// functions on the 
+        /// first call to a subclass constructor.
+        /// </remarks>
+        public ComparableObject()
+        {
+            Type type = GetType();
+
+            if (!_functions.ContainsKey(type))
+            {
+                MemberwiseFunctions functions = new MemberwiseFunctions
+                {
+                    EqualsFunc = MakeEqualsMethod(type),
+                    GetHashCodeFunc = MakeGetHashCodeMethod(type)
+                };
+
+                _functions[type] = functions;
+            }
+        }
+
+        #endregion
+
+        #region Private members
+
+        // ReSharper disable once InconsistentNaming
+        static readonly Dictionary<Type, MemberwiseFunctions> _functions;
+
+        #endregion
+
+        #region Public methods
+
+        /// <summary>
+        /// Creates the Equals() method.
+        /// </summary>
+        [NotNull]
+        public static Func<object, object, bool> MakeEqualsMethod
+            (
+                [NotNull] Type type
+            )
+        {
+            Code.NotNull(type, "type");
+
+            ParameterExpression pThis = Expression.Parameter
+                (
+                    typeof(object),
+                    "x"
+                );
+            ParameterExpression pThat = Expression.Parameter
+                (
+                    typeof(object),
+                    "y"
+                );
+
+            // cast to the subclass type
+            UnaryExpression pCastThis = Expression.Convert(pThis, type);
+            UnaryExpression pCastThat = Expression.Convert(pThat, type);
+
+            // compound AND expression using short-circuit evaluation
+            Expression last = null;
+            foreach (PropertyInfo property in type.GetProperties())
+            {
+                BinaryExpression equalExpression = Expression.Equal
+                    (
+                        Expression.Property(pCastThis, property),
+                        Expression.Property(pCastThat, property)
+                    );
+
+                last = last == null
+                    ? equalExpression
+                    : Expression.AndAlso
+                        (
+                            last,
+                            equalExpression
+                        );
+            }
+
+            // call Object.Equals if second parameter doesn't match type
+            last = Expression.Condition
+                (
+                    Expression.TypeIs(pThat, type),
+                    last,
+                    Expression.Equal(pThis, pThat)
+                );
+
+            // compile method
+            return Expression.Lambda<Func<object, object, bool>>
+                (
+                    last,
+                    pThis,
+                    pThat
+                )
+                .Compile();
+        }
+
+        /// <summary>
+        /// Creates the GetHashCode() method.
+        /// </summary>
+        [NotNull]
+        static Func<object, int> MakeGetHashCodeMethod
+            (
+                [NotNull] Type type
+            )
+        {
+            Code.NotNull(type, "type");
+
+            ParameterExpression pThis = Expression.Parameter
+                (
+                    typeof(object),
+                    "x"
+                );
+            UnaryExpression pCastThis = Expression.Convert
+                (
+                    pThis,
+                    type
+                );
+
+            Expression last = null;
+            foreach (PropertyInfo property in type.GetProperties())
+            {
+                MethodCallExpression hash = Expression.Call
+                    (
+                        Expression.Property(pCastThis, property),
+                        "GetHashCode",
+                        Type.EmptyTypes
+                    );
+
+                if (last == null)
+                {
+                    last = hash;
+                }
+                else
+                {
+                    last = Expression.ExclusiveOr(last, hash);
+                }
+            }
+
+            return Expression.Lambda<Func<object, int>>(last, pThis)
+                .Compile();
+        }
+
+        #endregion
+
+        #region Object members
+
+        /// <inheritdoc />
+        public override int GetHashCode()
+        {
+            // ReSharper disable once NonReadonlyMemberInGetHashCode
+            return _functions[GetType()].GetHashCodeFunc(this);
+        }
+
+        /// <inheritdoc />
+        public override bool Equals(object obj)
+        {
+            return _functions[GetType()].EqualsFunc(this, obj);
+        }
+
+        #endregion
+    }
+}
+
+#endif
---
+++ Sources/UnitTests/AM/ComparableObjectTest.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using AM;
+
+namespace UnitTests.AM
+{
+    [TestClass]
+    public class ComparableObjectTest
+    {
+        class MyClass
+            : ComparableObject
+        {
+            public int Id { get; set; }
+
+            public string Title { get; set; }
+        }
+
+        [TestMethod]
+        public void TestComparableObject_Equals()
+        {
+            MyClass first = new MyClass
+            {
+                Id = 1,
+                Title = "one"
+            };
+            MyClass second = new MyClass
+            {
+                Id = 1,
+                Title = "one"
+            };
+            Assert.IsTrue(first.Equals(second));
+
+            MyClass third = new MyClass
+            {
+                Id = 1,
+                Title = "two"
+            };
+            Assert.IsFalse(first.Equals(third));
+
+            MyClass fourth = new MyClass
+            {
+                Id = 2,
+                Title = "two"
+            };
+            Assert.IsFalse(third.Equals(fourth));
+
+            third.Id = 2;
+            Assert.IsTrue(third.Equals(fourth));
+        }
+
+        [TestMethod]
+        public void TestComparableObject_Equals_WithNull()
+        {
+            MyClass first = new MyClass
+            {
+                Id = 1,
+                Title = null
+            };
+            MyClass second = new MyClass
+            {
+                Id = 1,
+                Title = null
+            };
+            Assert.IsTrue(first.Equals(second));
+
+            MyClass third = new MyClass
+            {
+                Id = 1,
+                Title = "two"
+            };
+            Assert.IsFalse(first.Equals(third));
+        }
+
+        [TestMethod]
+        public void TestComparableObject_GetHashCode()
+        {
+            MyClass first = new MyClass
+            {
+                Id = 1,
+                Title = "one"
+            };
+            MyClass second = new MyClass
+            {
+                Id = 1,
+                Title = "one"
+            };
+            Assert.AreEqual(first.GetHashCode(), second.GetHashCode());
+
+            MyClass third = new MyClass
+            {
+                Id = 1,
+                Title = "two"
+            };
+            Assert.AreNotEqual(first.GetHashCode(), third.GetHashCode());
+        }
+
+        [TestMethod]
+        [Ignore]
+        public void TestComparableObject_GetHashCode_WithNull()
+        {
+            MyClass first = new MyClass
+            {
+                Id = 1,
+                Title = null
+            };
+            MyClass second = new MyClass
+            {
+                Id = 1,
+                Title = null
+            };
+            Assert.AreEqual(first.GetHashCode(), second.GetHashCode());
+
+            MyClass third = new MyClass
+            {
+                Id = 1,
+                Title = "two"
+            };
+            Assert.AreNotEqual(first.GetHashCode(), third.GetHashCode());
+        }
+    }
+}
