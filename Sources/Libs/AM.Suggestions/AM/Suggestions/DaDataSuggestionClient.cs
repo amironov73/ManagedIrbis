604ad3bb34c6c9f2563c63e0f39b38194d580c40
Alexey Mironov <amironov73@gmail.com>
2016-09-05 13:49:10 +0800

    Add AM.Suggestions project

---
+++ Sources/Libs/AM.Suggestions/AM/Suggestions/DaDataSuggestionClient.cs
@@ -0,0 +1,129 @@
+﻿/* DaDataSuggestionClient.cs --
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+
+using CodeJam;
+
+using JetBrains.Annotations;
+
+using MoonSharp.Interpreter;
+
+using RestSharp;
+
+#endregion
+
+namespace AM.Suggestions
+{
+    using DaData;
+
+    #pragma warning disable 1591
+
+    /// <summary>
+    /// Client for dadata.ru
+    /// </summary>
+    [PublicAPI]
+    [MoonSharpUserData]
+    public sealed class DaDataSuggestionClient
+    {
+        const string SUGGESTIONS_URL = "{0}/suggest";
+        const string ADDRESS_RESOURCE = "address";
+        const string PARTY_RESOURCE = "party";
+        const string BANK_RESOURCE = "bank";
+        const string FIO_RESOURCE = "fio";
+        const string EMAIL_RESOURCE = "email";
+
+        RestClient client;
+        string token;
+        ContentType contentType = ContentType.XML;
+
+        public IWebProxy Proxy
+        {
+            get { return client.Proxy; }
+            set { client.Proxy = value; }
+        }
+
+        static DaDataSuggestionClient()
+        {
+            // use SSL v3
+            ServicePointManager.SecurityProtocol
+                = SecurityProtocolType.Ssl3;
+        }
+
+        public DaDataSuggestionClient
+            (
+                string token,
+                string baseUrl
+            )
+        {
+            this.token = token;
+            client = new RestClient
+                (
+                    String.Format(SUGGESTIONS_URL, baseUrl)
+                );
+        }
+
+        public SuggestAddressResponse QueryAddress(string address)
+        {
+            var request = new RestRequest(ADDRESS_RESOURCE, Method.POST);
+            var query = new SuggestQuery(address);
+            return Execute<SuggestAddressResponse>(request, query, this.contentType);
+        }
+
+        public SuggestBankResponse QueryBank(string bank)
+        {
+            var request = new RestRequest(BANK_RESOURCE, Method.POST);
+            var query = new SuggestQuery(bank);
+            return Execute<SuggestBankResponse>(request, query, this.contentType);
+        }
+
+        public SuggestEmailResponse QueryEmail(string email)
+        {
+            var request = new RestRequest(EMAIL_RESOURCE, Method.POST);
+            var query = new SuggestQuery(email);
+            return Execute<SuggestEmailResponse>(request, query, this.contentType);
+        }
+
+        public SuggestFioResponse QueryFio(string fio)
+        {
+            var request = new RestRequest(FIO_RESOURCE, Method.POST);
+            var query = new SuggestQuery(fio);
+            return Execute<SuggestFioResponse>(request, query, this.contentType);
+        }
+
+        public SuggestPartyResponse QueryParty(string party)
+        {
+            var request = new RestRequest(PARTY_RESOURCE, Method.POST);
+            var query = new SuggestQuery(party);
+            return Execute<SuggestPartyResponse>(request, query, this.contentType);
+        }
+
+        private T Execute<T>(RestRequest request, SuggestQuery query, ContentType contentType) where T : new()
+        {
+            request.AddHeader("Authorization", "Token " + this.token);
+            request.AddHeader("Content-Type", contentType.Name);
+            request.AddHeader("Accept", contentType.Name);
+            request.RequestFormat = contentType.Format;
+            request.XmlSerializer.ContentType = contentType.Name;
+            request.AddBody(query);
+            var response = client.Execute<T>(request);
+
+            if (response.ErrorException != null)
+            {
+                throw response.ErrorException;
+            }
+            return response.Data;
+        }
+    }
+}
---
+++ Sources/Libs/AM.Suggestions/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AM.Suggestions")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AM.Suggestions")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("88F0B2D0-75DA-4455-AD7F-1D45558666D6")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
