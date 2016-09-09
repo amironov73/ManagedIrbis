bcbd887ca246ef49107aa322fa69455c092cbcba
Alexey Mironov <amironov73@gmail.com>
2016-09-09 15:51:34 +0800

    Add BorderInfo and BorderInfoControl

---
+++ Sources/Libs/AM.Windows.Forms/AM/Windows/Forms/BorderInfo.cs
@@ -0,0 +1,234 @@
+﻿/* BorderInfo.cs -- Информация о границе контрола.
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using System;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Design;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+using System.Windows.Forms.ComponentModel;
+using System.Windows.Forms.Design;
+
+#endregion
+
+namespace AM.Windows.Forms
+{
+    /// <summary>
+    /// Информация о границе контрола или его части.
+    /// </summary>
+    [TypeConverter(typeof(ExpandableObjectConverter))]
+    [Editor(typeof(BorderInfo.Editor), typeof(UITypeEditor))]
+    public class BorderInfo
+    {
+        #region Properties
+
+        private const bool _DefaultBorder = true;
+        private bool _drawBorder = _DefaultBorder;
+
+        ///<summary>
+        /// Рисовать границу или нет.
+        ///</summary>
+        [DefaultValue(_DefaultBorder)]
+        [Description("Draw border or not.")]
+        public bool DrawBorder
+        {
+            [DebuggerStepThrough]
+            get
+            {
+                return _drawBorder;
+            }
+            [DebuggerStepThrough]
+            set
+            {
+                _drawBorder = value;
+            }
+        }
+
+        private const bool _Default3D = true;
+        private bool _draw3D = _Default3D;
+
+        ///<summary>
+        /// Граница трехмерная или нет.
+        ///</summary>
+        [DefaultValue(_Default3D)]
+        [Description("Is border 3D.")]
+        public bool Draw3D
+        {
+            [DebuggerStepThrough]
+            get
+            {
+                return _draw3D;
+            }
+            [DebuggerStepThrough]
+            set
+            {
+                _draw3D = value;
+            }
+        }
+
+        private const ButtonBorderStyle _DefaultStyle2D =
+            ButtonBorderStyle.Solid;
+        private ButtonBorderStyle _style2D = _DefaultStyle2D;
+
+        ///<summary>
+        /// Стиль двухмерной границы.
+        ///</summary>
+        [DefaultValue(_DefaultStyle2D)]
+        [Description("Style of 2D border.")]
+        public ButtonBorderStyle Style2D
+        {
+            [DebuggerStepThrough]
+            get
+            {
+                return _style2D;
+            }
+            [DebuggerStepThrough]
+            set
+            {
+                _style2D = value;
+            }
+        }
+
+        private const string _DefaultColor = "Black";
+        private Color _borderColor = Color.FromName(_DefaultColor);
+
+        ///<summary>
+        /// Цвет двухмерной границы.
+        ///</summary>
+        [DefaultValue(typeof(Color), _DefaultColor)]
+        [Description("Color for 2D border.")]
+        public Color BorderColor
+        {
+            [DebuggerStepThrough]
+            get
+            {
+                return _borderColor;
+            }
+            [DebuggerStepThrough]
+            set
+            {
+                _borderColor = value;
+            }
+        }
+
+        private const Border3DStyle _DefaultStyle3D =
+            Border3DStyle.Etched;
+        private Border3DStyle _style3D = _DefaultStyle3D;
+
+        ///<summary>
+        /// Стиль трехмерной границы.
+        ///</summary>
+        [DefaultValue(_DefaultStyle3D)]
+        [Description("Style for 3D border")]
+        public Border3DStyle Style3D
+        {
+            [DebuggerStepThrough]
+            get
+            {
+                return _style3D;
+            }
+            [DebuggerStepThrough]
+            set
+            {
+                _style3D = value;
+            }
+        }
+
+        #endregion
+
+        #region Public methods
+
+        /// <summary>
+        /// Отрисовка границы контрола.
+        /// </summary>
+        /// <param name="g"></param>
+        /// <param name="r"></param>
+        public void Draw(Graphics g, Rectangle r)
+        {
+            if (DrawBorder)
+            {
+                if (Draw3D)
+                {
+                    ControlPaint.DrawBorder3D(g, r, Style3D);
+                }
+                else
+                {
+                    ControlPaint.DrawBorder(g, r, BorderColor, Style2D);
+                }
+            }
+        }
+
+        #endregion
+
+        #region Designer
+
+        public class Editor : UITypeEditor
+        {
+            private BorderInfo _bi;
+
+            public Editor()
+            {
+            }
+
+            public Editor(BorderInfo bi)
+            {
+                _bi = bi;
+            }
+
+            public override UITypeEditorEditStyle GetEditStyle(
+                ITypeDescriptorContext context)
+            {
+                return UITypeEditorEditStyle.DropDown;
+            }
+
+            public override object EditValue(
+                ITypeDescriptorContext context,
+                IServiceProvider provider, object value)
+            {
+                BorderInfo binfo = (BorderInfo)value;
+
+                IWindowsFormsEditorService edSvc = (IWindowsFormsEditorService)provider.GetService(typeof(IWindowsFormsEditorService));
+                if (edSvc != null)
+                {
+                    BorderInfoControl form = new BorderInfoControl(binfo,
+                        edSvc, context, provider);
+                    edSvc.DropDownControl(form);
+                    if (form.Result != null)
+                    {
+                        return form.Result;
+                    }
+                }
+
+                //throw new NotImplementedException ();
+                //return new BorderInfo ();
+                return binfo;
+            }
+
+            public override void PaintValue(PaintValueEventArgs e)
+            {
+                Graphics g = e.Graphics;
+                using (Font font = new Font("Arial", 8))
+                using (Brush brush = new SolidBrush(Color.Black))
+                {
+                    g.DrawString("Not implemented", font, brush, 0, 0);
+                }
+                //_bi.Draw ( g, e.Bounds );
+            }
+
+            public override bool GetPaintValueSupported(ITypeDescriptorContext context)
+            {
+                return false;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Sources/Libs/AM.Windows.Forms/AM/Windows/Forms/BorderInfoControl.cs
@@ -0,0 +1,268 @@
+﻿/* BorderInfoControl.cs -- 
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using System;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Design;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+using System.Windows.Forms.ComponentModel;
+using System.Windows.Forms.Design;
+
+#endregion
+
+namespace AM.Windows.Forms
+{
+    class BorderInfoControl
+        : UserControl
+    {
+        private IWindowsFormsEditorService _svc;
+        private ITypeDescriptorContext _context;
+        private IServiceProvider _provider;
+
+        private System.Windows.Forms.CheckBox drawBox;
+        private System.Windows.Forms.CheckBox draw3D;
+        private System.Windows.Forms.ComboBox style2D;
+        private System.Windows.Forms.ComboBox style3D;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Panel color2D;
+
+        public BorderInfo Result = null;
+
+        public BorderInfoControl
+            (
+                BorderInfo binfo,
+                IWindowsFormsEditorService svc,
+                ITypeDescriptorContext context,
+                IServiceProvider provider
+            )
+        {
+            _svc = svc;
+            _context = context;
+            _provider = provider;
+            InitializeComponent();
+
+            drawBox.Checked = binfo.DrawBorder;
+            draw3D.Checked = binfo.Draw3D;
+
+            foreach (object o in Enum.GetValues(typeof(ButtonBorderStyle)))
+            {
+                style2D.Items.Add(o);
+            }
+            style2D.SelectedItem = binfo.Style2D;
+            foreach (object o in Enum.GetValues(typeof(Border3DStyle)))
+            {
+                style3D.Items.Add(o);
+            }
+            style3D.SelectedItem = binfo.Style3D;
+            color2D.BackColor = binfo.BorderColor;
+        }
+
+        private BorderInfo _Border()
+        {
+            BorderInfo result = new BorderInfo();
+            result.DrawBorder = drawBox.Checked;
+            result.Draw3D = draw3D.Checked;
+            result.Style2D = (ButtonBorderStyle)style2D.SelectedItem;
+            result.Style3D = (Border3DStyle)style3D.SelectedItem;
+            result.BorderColor = color2D.BackColor;
+            return result;
+        }
+
+        private void okButton_Click(object sender, EventArgs e)
+        {
+            Result = _Border();
+            _svc.CloseDropDown();
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            Result = null;
+            _svc.CloseDropDown();
+        }
+
+        private void drawBox_CheckedChanged(object sender, EventArgs e)
+        {
+            panel1.Invalidate();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            BorderInfo b = _Border();
+            b.Draw(e.Graphics, panel1.ClientRectangle);
+        }
+
+        private void color2D_Click(object sender, EventArgs e)
+        {
+            using (ColorDialog dialog = new ColorDialog())
+            {
+                dialog.Color = color2D.BackColor;
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    color2D.BackColor = dialog.Color;
+                    drawBox_CheckedChanged(sender, e);
+                }
+            }
+        }
+
+        private void BorderInfoControl_Load(object sender, EventArgs e)
+        {
+            BackColor = SystemColors.Control;
+            //ParentForm.AcceptButton = okButton;
+            //ParentForm.CancelButton = cancelButton;
+        }
+
+        private void InitializeComponent()
+        {
+            this.drawBox = new System.Windows.Forms.CheckBox();
+            this.draw3D = new System.Windows.Forms.CheckBox();
+            this.style2D = new System.Windows.Forms.ComboBox();
+            this.style3D = new System.Windows.Forms.ComboBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.okButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.color2D = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // drawBox
+            // 
+            this.drawBox.AutoSize = true;
+            this.drawBox.Location = new System.Drawing.Point(13, 13);
+            this.drawBox.Name = "drawBox";
+            this.drawBox.Size = new System.Drawing.Size(80, 17);
+            this.drawBox.TabIndex = 0;
+            this.drawBox.Text = "Draw border";
+            this.drawBox.CheckedChanged += new System.EventHandler(this.drawBox_CheckedChanged);
+            // 
+            // draw3D
+            // 
+            this.draw3D.AutoSize = true;
+            this.draw3D.Location = new System.Drawing.Point(122, 13);
+            this.draw3D.Name = "draw3D";
+            this.draw3D.Size = new System.Drawing.Size(69, 17);
+            this.draw3D.TabIndex = 1;
+            this.draw3D.Text = "3D border";
+            this.draw3D.CheckedChanged += new System.EventHandler(this.drawBox_CheckedChanged);
+            // 
+            // style2D
+            // 
+            this.style2D.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.style2D.FormattingEnabled = true;
+            this.style2D.Location = new System.Drawing.Point(73, 37);
+            this.style2D.Margin = new System.Windows.Forms.Padding(2, 3, 3, 3);
+            this.style2D.Name = "style2D";
+            this.style2D.Size = new System.Drawing.Size(107, 21);
+            this.style2D.TabIndex = 2;
+            this.style2D.SelectedIndexChanged += new System.EventHandler(this.drawBox_CheckedChanged);
+            // 
+            // style3D
+            // 
+            this.style3D.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.style3D.FormattingEnabled = true;
+            this.style3D.Location = new System.Drawing.Point(73, 95);
+            this.style3D.Name = "style3D";
+            this.style3D.Size = new System.Drawing.Size(107, 21);
+            this.style3D.TabIndex = 4;
+            this.style3D.SelectedIndexChanged += new System.EventHandler(this.drawBox_CheckedChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(14, 124);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(166, 72);
+            this.panel1.TabIndex = 5;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 44);
+            this.label1.Margin = new System.Windows.Forms.Padding(3, 3, 1, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 14);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "2D style";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 14);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "2D color";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 14);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "3D style";
+            // 
+            // okButton
+            // 
+            this.okButton.Location = new System.Drawing.Point(14, 203);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(79, 23);
+            this.okButton.TabIndex = 9;
+            this.okButton.Text = "OK";
+            this.okButton.Click += new System.EventHandler(this.okButton_Click);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(100, 203);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(80, 23);
+            this.cancelButton.TabIndex = 10;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // color2D
+            // 
+            this.color2D.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.color2D.Location = new System.Drawing.Point(73, 68);
+            this.color2D.Name = "color2D";
+            this.color2D.Size = new System.Drawing.Size(106, 19);
+            this.color2D.TabIndex = 11;
+            this.color2D.Click += new System.EventHandler(this.color2D_Click);
+            // 
+            // BorderInfoControl
+            // 
+            this.Controls.Add(this.color2D);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.okButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.style3D);
+            this.Controls.Add(this.style2D);
+            this.Controls.Add(this.draw3D);
+            this.Controls.Add(this.drawBox);
+            this.Name = "BorderInfoControl";
+            this.Size = new System.Drawing.Size(195, 232);
+            this.Load += new System.EventHandler(this.BorderInfoControl_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
