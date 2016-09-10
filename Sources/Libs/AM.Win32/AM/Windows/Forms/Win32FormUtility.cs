4676e30a98a8629b32817da2353699ab9e9b835e
Alexey Mironov <amironov73@gmail.com>
2016-09-10 14:09:08 +0800

    Add PopupForm and PopupFormTest

---
+++ Sources/Libs/AM.Win32/AM/Windows/Forms/Win32FormUtility.cs
@@ -0,0 +1,70 @@
+﻿/* Win32FormUtility.cs -- Win32-specific extension methods for System.Windows.Forms
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using AM.Win32;
+using AM.Text.Output;
+
+using CodeJam;
+
+using JetBrains.Annotations;
+
+using MoonSharp.Interpreter;
+
+#endregion
+
+namespace AM.Windows.Forms
+{
+    /// <summary>
+    /// Win32-specific extension methods for <see cref="Form"/>.
+    /// </summary>
+    [PublicAPI]
+    [MoonSharpUserData]
+    public static class Win32FormUtility
+    {
+        #region Public methods
+
+        /// <summary>
+        /// Shows the form without activation.
+        /// </summary>
+        public static void ShowNoActivate
+            (
+                [NotNull] Form form
+            )
+        {
+            Code.NotNull(form, "form");
+
+            User32.SetWindowPos
+                (
+                    form.Handle,
+                    User32.HWND_TOPMOST,
+                    0,
+                    0,
+                    0,
+                    0,
+                    SetWindowPosFlags.SWP_SHOWWINDOW
+                    | SetWindowPosFlags.SWP_NOACTIVATE
+                    | SetWindowPosFlags.SWP_NOSIZE
+                    | SetWindowPosFlags.SWP_NOMOVE
+                  );
+            form.Show();
+        }
+
+        #endregion
+    }
+}
---
+++ Sources/UITests/Sources/Tests/PopupFormTest.cs
@@ -0,0 +1,105 @@
+﻿/* PopupFormTest.cs -- 
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using AM;
+using AM.Windows.Forms;
+
+using CodeJam;
+
+using IrbisUI;
+
+using JetBrains.Annotations;
+
+using ManagedIrbis;
+
+using MoonSharp.Interpreter;
+
+using Newtonsoft.Json;
+
+#endregion
+
+namespace UITests
+{
+    public sealed class PopupFormTest
+        : IUITest
+    {
+        #region IUITest members
+
+        public void Run
+            (
+                IWin32Window ownerWindow
+            )
+        {
+            using (Form form = new Form())
+            {
+                form.Size = new Size(800, 600);
+
+                TextBox titleBox = new TextBox
+                {
+                    Text = "Form title",
+                    Location = new Point(10, 10),
+                    Width = 200
+                };
+                form.Controls.Add(titleBox);
+
+                TextBox messageBox = new TextBox
+                {
+                    Text = "Some message <b>with formatting</b>",
+                    Location = new Point(10, 40),
+                    Width = 200
+                };
+                form.Controls.Add(messageBox);
+
+                NumericUpDown timeoutBox = new NumericUpDown
+                {
+                    Minimum = 10m,
+                    Maximum = 100000m,
+                    Value = 2000m,
+                    DecimalPlaces = 0,
+                    Location = new Point(10, 70),
+                    Width = 200
+                };
+                form.Controls.Add(timeoutBox);
+
+                Button button = new Button
+                {
+                    Text = "Show popup",
+                    Location = new Point(10, 100),
+                    Width = 200
+                };
+                form.Controls.Add(button);
+
+                button.Click += (sender, args) =>
+                {
+                    string title = titleBox.Text;
+                    string message = messageBox.Text;
+                    int timeout = Convert.ToInt32(timeoutBox.Value);
+
+                    PopupForm.Popup
+                        (
+                            title,
+                            message,
+                            timeout
+                        );
+                };
+
+                form.ShowDialog(ownerWindow);
+            }
+        }
+
+        #endregion
+    }
+}
