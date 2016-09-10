51c61e43cf561e51c5312faf74ade234b275012e
Alexey Mironov <amironov73@gmail.com>
2016-09-10 13:05:17 +0800

    Add ExceptionBoxTest

---
+++ Sources/UITests/Sources/Tests/ExceptionBoxTest.cs
@@ -0,0 +1,60 @@
+﻿/* ExceptionBoxTest.cs -- 
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using AM;
+using AM.Windows.Forms;
+
+using CodeJam;
+
+using IrbisUI;
+
+using JetBrains.Annotations;
+
+using ManagedIrbis;
+
+using MoonSharp.Interpreter;
+
+using Newtonsoft.Json;
+
+#endregion
+
+namespace UITests
+{
+    public sealed class ExceptionBoxTest
+        : IUITest
+    {
+        #region IUITest members
+
+        public void Run
+            (
+                IWin32Window ownerWindow
+            )
+        {
+            try
+            {
+                Exception exception
+                    = new ApplicationException("Некая ошибка в приложении");
+                throw exception;
+            }
+            catch (Exception ex)
+            {
+                ExceptionBox.Show(ownerWindow, ex);
+            }
+        }
+
+        #endregion
+    }
+}
