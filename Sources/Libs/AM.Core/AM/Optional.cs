3c9dcdde7594a1888fa1363d6172dd2e510575a2
Alexey Mironov <amironov73@gmail.com>
2016-09-12 12:24:58 +0800

    Add Option<T> (from StackOverflow)

---
+++ Sources/Libs/AM.Core/AM/Optional.cs
@@ -0,0 +1,153 @@
+﻿/* BusyStripe.cs -- 
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using System;
+
+using JetBrains.Annotations;
+
+using MoonSharp.Interpreter;
+
+#endregion
+
+namespace AM
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// Borrowed from StackOverflow:
+    /// http://stackoverflow.com/questions/16199227/optional-return-in-c-net
+    /// </remarks>
+    [PublicAPI]
+    [MoonSharpUserData]
+    public struct Optional<T>
+        : IEquatable<Optional<T>>
+    {
+        #region Properties
+
+        /// <summary>
+        /// Has value?
+        /// </summary>
+        public readonly bool HasValue;
+
+        /// <summary>
+        /// Value itself.
+        /// </summary>
+        [CanBeNull]
+        public T Value
+        {
+            get
+            {
+                if (HasValue)
+                {
+                    return _value;
+                }
+
+                throw new InvalidOperationException();
+            }
+        }
+
+        #endregion
+
+        #region Construction
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public Optional
+            (
+                [CanBeNull] T value
+            )
+        {
+            _value = value;
+            HasValue = true;
+        }
+
+        #endregion
+
+        #region Private members
+
+        private readonly T _value;
+
+        #endregion
+
+        #region Public methods
+
+        /// <summary>
+        /// Convert Optional to value.
+        /// </summary>
+        public static explicit operator T
+            (
+                Optional<T> optional
+            )
+        {
+            return optional.Value;
+        }
+
+        /// <summary>
+        /// Convert value to Optional.
+        /// </summary>
+        public static implicit operator Optional<T>
+            (
+                [CanBeNull] T value
+            )
+        {
+            return new Optional<T>(value);
+        }
+
+        #endregion
+
+        #region Object members
+
+        /// <inheritdoc />
+        public override bool Equals
+            (
+                [CanBeNull] object obj
+            )
+        {
+            if (obj is Optional<T>)
+            {
+                return Equals((Optional<T>)obj);
+            }
+
+            return false;
+        }
+
+        /// <inheritdoc />
+        public bool Equals
+            (
+                Optional<T> other
+            )
+        {
+            if (HasValue && other.HasValue)
+            {
+                return Equals(_value, other._value);
+            }
+            
+            return HasValue == other.HasValue;
+        }
+
+        /// <inheritdoc />
+        public override int GetHashCode()
+        {
+            if (!HasValue)
+            {
+                return 0;
+            }
+
+            if (ReferenceEquals(_value, null))
+            {
+                return -1;
+            }
+
+            return _value.GetHashCode();
+        }
+
+        #endregion
+    }
+}
---
+++ Sources/UnitTests/AM/OptionalTest.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using AM;
+
+namespace UnitTests.AM
+{
+    [TestClass]
+    public class OptionalTest
+    {
+        [TestMethod]
+        public void TestOptional_DefaultConstructor()
+        {
+            Optional<object> optional = new Optional<object>();
+
+            Assert.IsFalse(optional.HasValue);
+        }
+
+        [TestMethod]
+        public void TestOptional_Constructor()
+        {
+            Optional<string> optional = new Optional<string>("Hello");
+
+            Assert.IsTrue(optional.HasValue);
+            Assert.AreEqual("Hello", optional.Value);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void TestOptional_Exception()
+        {
+            Optional<object> optional = new Optional<object>();
+            object value = optional.Value;
+        }
+
+        [TestMethod]
+        public void TestOptional_Operators()
+        {
+            Optional<string> optional = "Hello";
+            Assert.IsTrue(optional.HasValue);
+            Assert.AreEqual("Hello", optional.Value);
+
+            string hello = (string) optional;
+            Assert.AreEqual("Hello", hello);
+        }
+
+        [TestMethod]
+        public void TestOptional_Equals1()
+        {
+            Optional<int> first = 1;
+            Optional<int> second = 1;
+            Assert.IsTrue(first.Equals(second));
+
+            Optional<int> third = 2;
+            Assert.IsFalse(first.Equals(third));
+
+            Optional<int> fourth = new Optional<int>();
+            Assert.IsFalse(first.Equals(fourth));
+        }
+
+        [TestMethod]
+        public void TestOptional_Equals2()
+        {
+            Optional<int> first = 1;
+            object second = new Optional<int>(1);
+            Assert.IsTrue(first.Equals(second));
+
+            object third = new Optional<int>(2);
+            Assert.IsFalse(first.Equals(third));
+
+            object fourth = new Optional<int>();
+            Assert.IsFalse(first.Equals(fourth));
+
+            object fifth = new Optional<string>("Hello");
+            Assert.IsFalse(first.Equals(fifth));
+        }
+
+        [TestMethod]
+        public void TestOptional_GetHashCode()
+        {
+            Optional<int> first = new Optional<int>();
+            Assert.AreEqual(0, first.GetHashCode());
+
+            Optional<string> second = new Optional<string>(null);
+            Assert.AreEqual(-1, second.GetHashCode());
+
+            Optional<int> third = 1;
+            Assert.AreEqual(1, third.GetHashCode());
+        }
+    }
+}
