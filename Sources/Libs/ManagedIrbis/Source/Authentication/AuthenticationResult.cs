a8d561e989d524675305ddcc003df9f7b4ae163d
Alexey Mironov <amironov73@gmail.com>
2016-09-13 19:41:46 +0800

    Add AbstractAuthenticator

---
+++ Sources/Libs/ManagedIrbis/Source/Authentication/AuthenticationResult.cs
@@ -0,0 +1,50 @@
+﻿/* AuthentificationResult.cs -- 
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+using CodeJam;
+
+using JetBrains.Annotations;
+
+using MoonSharp.Interpreter;
+
+using Newtonsoft.Json;
+
+#endregion
+
+namespace ManagedIrbis.Authentication
+{
+    /// <summary>
+    /// Credentials.
+    /// </summary>
+    [PublicAPI]
+    [MoonSharpUserData]
+    public sealed class AuthenticationResult
+    {
+        #region Properties
+
+        /// <summary>
+        /// Successfull?
+        /// </summary>
+        public bool Success { get; internal set; }
+
+        /// <summary>
+        /// Error message (if any).
+        /// </summary>
+        [CanBeNull]
+        public string ErrorMessage { get; internal set; }
+
+        #endregion
+    }
+}
