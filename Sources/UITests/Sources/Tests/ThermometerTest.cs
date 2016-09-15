78ca09cfc4b976fc9fc1b5ec324a6d8bc112a4c7
Alexey Mironov <amironov73@gmail.com>
2016-09-15 16:56:02 +0800

    Add Thermometer

---
+++ Sources/UITests/Sources/Tests/ThermometerTest.cs
@@ -0,0 +1,80 @@
+﻿/* ThermometerTest.cs -- 
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using AM;
+using AM.Windows.Forms;
+
+using CodeJam;
+
+using IrbisUI;
+
+using JetBrains.Annotations;
+
+using ManagedIrbis;
+
+using MoonSharp.Interpreter;
+
+using Newtonsoft.Json;
+
+#endregion
+
+namespace UITests
+{
+    public sealed class ThermometerTest
+        : IUITest
+    {
+        #region IUITest members
+
+        public void Run
+            (
+                IWin32Window ownerWindow
+            )
+        {
+            using (Form form = new Form())
+            {
+                form.Size = new Size(800, 600);
+
+                Thermometer thermometer = new Thermometer
+                {
+                    Location = new Point(10, 10),
+                    Size = new Size(50, 200),
+                    CurrentTemperature = 36.7
+                };
+                form.Controls.Add(thermometer);
+
+                TrackBar trackBar = new TrackBar
+                {
+                    Location = new Point(100, 10),
+                    Width = 300,
+                    Minimum = 0,
+                    Maximum = 100,
+                    Value = 37
+                };
+                form.Controls.Add(trackBar);
+
+                trackBar.ValueChanged += (sender, args) =>
+                {
+                    thermometer.CurrentTemperature = trackBar.Value;
+                };
+
+
+                form.ShowDialog(ownerWindow);
+            }
+        }
+
+        #endregion
+    }
+}
