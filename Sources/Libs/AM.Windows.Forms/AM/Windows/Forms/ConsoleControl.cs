7e45902df504cbe38877b4db80cf013d021c2697
Alexey Mironov <amironov73@gmail.com>
2016-09-15 19:09:20 +0800

    Add ConsoleControl

---
+++ Sources/Libs/AM.Windows.Forms/AM/Windows/Forms/ConsoleControl.cs
@@ -0,0 +1,411 @@
+﻿/* ConsoleControl.cs --
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using System;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Design;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+using System.Windows.Forms.ComponentModel;
+using System.Windows.Forms.Design;
+
+using CodeJam;
+
+using JetBrains.Annotations;
+
+using MoonSharp.Interpreter;
+
+#endregion
+
+namespace AM.Windows.Forms
+{
+    /// <summary>
+    /// Pseudo-console.
+    /// </summary>
+    [PublicAPI]
+    [MoonSharpUserData]
+    [System.ComponentModel.DesignerCategory("Code")]
+    [ToolboxBitmap(typeof(BusyStripe), "Images.ConsoleControl.bmp")]
+    public sealed class ConsoleControl
+        : Control
+    {
+        #region Constants
+
+        /// <summary>
+        /// Default window height.
+        /// </summary>
+        public const int DefaultWindowHeight = 25;
+
+        /// <summary>
+        /// Default window width.
+        /// </summary>
+        public const int DefaultWindowWidth = 80;
+
+        /// <summary>
+        /// Default font name.
+        /// </summary>
+        public const string DefaultFontName = "Courier New";
+
+        /// <summary>
+        /// Default font size.
+        /// </summary>
+        public const float DefaultFontSize = 8f;
+
+        #endregion
+
+        #region Nested classes
+
+        struct Cell
+        {
+            public char Character;
+
+            public Color ForeColor;
+
+            public Color BackColor;
+
+            public override string ToString()
+            {
+                return new string(Character, 1);
+            }
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Column position of the cursor.
+        /// </summary>
+        public int CursorLeft { get; set; }
+
+        /// <summary>
+        /// Row position of the cursor.
+        /// </summary>
+        public int CursorTop { get; set; }
+
+        /// <summary>
+        /// Window height.
+        /// </summary>
+        [DefaultValue(DefaultWindowHeight)]
+        public int WindowHeight { get; set; }
+
+        /// <summary>
+        /// Window width.
+        /// </summary>
+        [DefaultValue(DefaultWindowWidth)]
+        public int WindowWidth { get; set; }
+
+        #endregion
+
+        #region Construction
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public ConsoleControl()
+        {
+            DoubleBuffered = true;
+
+            WindowHeight = DefaultWindowHeight;
+            WindowWidth = DefaultWindowWidth;
+
+            Font = new Font
+                (
+                    DefaultFontName,
+                    DefaultFontSize,
+                    FontStyle.Regular
+                );
+            BackColor = Color.Black;
+            ForeColor = Color.LightGray;
+
+            _cellWidth = 8;
+            _cellHeight = 12;
+
+            _cells = new Cell[WindowHeight * WindowWidth];
+        }
+
+        #endregion
+
+        #region Private members
+
+        private Cell[] _cells;
+
+        private int _cellWidth;
+        private int _cellHeight;
+
+        private void _AdvanceCursor()
+        {
+            CursorLeft++;
+            if (CursorLeft >= WindowWidth)
+            {
+                CursorLeft = 0;
+                CursorTop++;
+            }
+            if (CursorTop >= WindowHeight)
+            {
+                ScrollUp();
+                CursorTop--;
+            }
+        }
+
+        #endregion
+
+        #region Public methods
+
+        /// <summary>
+        /// Clear the console.
+        /// </summary>
+        public void Clear()
+        {
+            CursorTop = 0;
+            CursorLeft = 0;
+
+            int total = WindowWidth * WindowHeight;
+
+            Cell empty = new Cell
+            {
+                Character = ' ',
+                ForeColor = ForeColor,
+                BackColor = BackColor
+            };
+            for (int i = 0; i < total; i++)
+            {
+                _cells[i] = empty;
+            }
+
+            Invalidate();
+        }
+
+        /// <summary>
+        /// Scroll up by one line.
+        /// </summary>
+        public void ScrollUp()
+        {
+            int total = WindowWidth * WindowHeight;
+            int scrollingEnd = total - WindowWidth;
+
+            for (int i = 0; i < scrollingEnd; i++)
+            {
+                _cells[i] = _cells[i + WindowWidth];
+            }
+
+            Cell empty = new Cell
+            {
+                Character = ' ',
+                ForeColor = ForeColor,
+                BackColor = BackColor
+            };
+            for (int i = scrollingEnd; i < total; i++)
+            {
+                _cells[i] = empty;
+            }
+            
+            Invalidate();
+        }
+
+        /// <summary>
+        /// WriteCharacter.
+        /// </summary>
+        public void Write
+            (
+                char c,
+                Color foreColor,
+                Color backColor
+            )
+        {
+            Cell cell = new Cell
+            {
+                Character = c,
+                BackColor = backColor,
+                ForeColor = foreColor
+            };
+
+            _cells[CursorTop*WindowWidth + CursorLeft] = cell;
+            _AdvanceCursor();
+            Invalidate();
+        }
+
+        /// <summary>
+        /// Write character.
+        /// </summary>
+        public void Write
+            (
+                int row,
+                int column,
+                char c,
+                Color foreColor,
+                Color backColor
+            )
+        {
+            if (
+                row < 0
+                || row >= WindowHeight
+                || column < 0
+                || column >= WindowWidth
+                )
+            {
+                return;
+            }
+
+            Cell cell = new Cell
+            {
+                Character = c,
+                BackColor = backColor,
+                ForeColor = foreColor
+            };
+            _cells[row*WindowWidth + column] = cell;
+            Invalidate();
+        }
+
+        /// <summary>
+        /// Write text.
+        /// </summary>
+        public void Write
+            (
+                [CanBeNull] string text,
+                Color foreColor,
+                Color backColor
+            )
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                return;
+            }
+
+            foreach (char c in text)
+            {
+                Write(c, foreColor, backColor);
+            }
+        }
+
+        /// <summary>
+        /// Write text.
+        /// </summary>
+        public void Write
+            (
+                [CanBeNull] string text
+            )
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                return;
+            }
+
+            foreach (char c in text)
+            {
+                Write(c, ForeColor, BackColor);
+            }
+        }
+
+        /// <summary>
+        /// Move cursor to beginning of next line.
+        /// </summary>
+        public void WriteLine()
+        {
+            CursorLeft = 0;
+            CursorTop++;
+            if (CursorTop >= WindowHeight)
+            {
+                ScrollUp();
+                CursorTop--;
+            }
+        }
+
+        /// <summary>
+        /// Write text and move cursor to beginning of next line.
+        /// </summary>
+        public void WriteLine
+            (
+                [CanBeNull] string text
+            )
+        {
+            Write(text);
+            WriteLine();
+        }
+
+        #endregion
+
+        #region Control members
+
+        /// <inheritdoc />
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams result = base.CreateParams;
+
+                return result;
+            }
+        }
+
+        /// <inheritdoc />
+        protected override Size DefaultSize
+        {
+            get { return new Size(640, 300); }
+        }
+
+        /// <inheritdoc />
+        protected override void OnPaint
+            (
+                PaintEventArgs paintEvent
+            )
+        {
+            Graphics graphics = paintEvent.Graphics;
+
+            int cellOffset = 0;
+            int rowOffset = 0;
+
+            for (int row = 0; row < WindowHeight; row++)
+            {
+                int columnOffset = 0;
+
+                for (int column = 0; column < WindowWidth; column++)
+                {
+                    Cell cell = _cells[cellOffset];
+
+                    Rectangle rectangle = new Rectangle
+                        (
+                            columnOffset,
+                            rowOffset,
+                            _cellWidth,
+                            _cellHeight
+                        );
+
+                    using (Brush foreBrush = new SolidBrush(cell.ForeColor))
+                    using (Brush backBrush = new SolidBrush(cell.BackColor))
+                    {
+                        graphics.FillRectangle(backBrush, rectangle);
+                        string s = new string(cell.Character, 1);
+                        graphics.DrawString(s, Font, foreBrush, rectangle);
+                    }
+
+                    cellOffset++;
+                    columnOffset += _cellWidth;
+                }
+
+                rowOffset += _cellHeight;
+            }
+
+            base.OnPaint(paintEvent);
+        }
+
+        /// <inheritdoc />
+        protected override void OnPaintBackground
+            (
+                PaintEventArgs paintEvent
+            )
+        {
+            // Do nothing
+        }
+
+        #endregion
+    }
+}
---
+++ Sources/UITests/Sources/Tests/ConsoleControlTest.cs
@@ -0,0 +1,147 @@
+﻿/* ConsoleControlTest.cs -- 
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using AM;
+using AM.Windows.Forms;
+
+using CodeJam;
+
+using IrbisUI;
+
+using JetBrains.Annotations;
+
+using ManagedIrbis;
+
+using MoonSharp.Interpreter;
+
+using Newtonsoft.Json;
+
+#endregion
+
+namespace UITests
+{
+    public sealed class ConsoleControlTest
+        : IUITest
+    {
+        #region IUITest members
+
+        public void Run
+            (
+                IWin32Window ownerWindow
+            )
+        {
+            using (Form form = new Form())
+            {
+                form.Size = new Size(800, 600);
+
+                TextBox textBox = new TextBox
+                {
+                    Location = new Point(10, 10),
+                    Width = 200,
+                    Text = "Hello, world! "
+                };
+                form.Controls.Add(textBox);
+
+                Button button1 = new Button
+                {
+                    Location = new Point(230, 10),
+                    Width = 100,
+                    Text = "Write"
+                };
+                form.Controls.Add(button1);
+
+                Button button2 = new Button
+                {
+                    Location = new Point(340, 10),
+                    Width = 100,
+                    Text = "Many"
+                };
+                form.Controls.Add(button2);
+
+                Button button3 = new Button
+                {
+                    Location = new Point(450, 10),
+                    Width = 100,
+                    Text = "Clear"
+                };
+                form.Controls.Add(button3);
+
+                ConsoleControl console = new ConsoleControl
+                {
+                    Location = new Point(10, 50),
+                };
+                form.Controls.Add(console);
+
+                for (int row = 0; row < 4; row++)
+                {
+                    for (int column = 0; column < 80; column++)
+                    {
+                        if (column%10 == 0)
+                        {
+                            console.Write
+                                (
+                                    row,
+                                    column,
+                                    '0',
+                                    Color.Red,
+                                    Color.White
+                                );
+                        }
+                        else
+                        {
+                            char c = (char)('0' + column%10);
+                            console.Write
+                                (
+                                    row,
+                                    column,
+                                    c,
+                                    Color.Blue, 
+                                    Color.GreenYellow
+                                );
+                        }
+
+                    }
+                }
+                console.CursorTop = 4;
+
+                button1.Click += (sender, args) =>
+                {
+                    console.Write(textBox.Text);
+                };
+
+                button2.Click += (sender, args) =>
+                {
+                    console.ForeColor = Color.LimeGreen;
+                    for (int i = 0; i < 100; i++)
+                    {
+                        console.Write("Mary has a little lamb. ");
+                        Application.DoEvents();
+                    }
+                };
+
+                button3.Click += (sender, args) =>
+                {
+                    console.Clear();
+                };
+
+                form.ShowDialog(ownerWindow);
+            }
+        }
+
+        #endregion
+    }
+}
