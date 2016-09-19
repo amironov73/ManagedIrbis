1b3ef8e4155541d4e4f61eabbb74df4e880f212f
Alexey Mironov <amironov73@gmail.com>
2016-09-19 18:30:00 +0800

    Work on IniFile

---
+++ Sources/Libs/AM.Core/AM/DuplicateKeyException.cs
@@ -0,0 +1,58 @@
+﻿/* DuplicateKeyException.cs --
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using System;
+
+using JetBrains.Annotations;
+
+#endregion
+
+namespace AM
+{
+    /// <summary>
+    /// Duplicate key found.
+    /// </summary>
+    [PublicAPI]
+    public sealed class DuplicateKeyException
+        : ArsMagnaException
+    {
+        #region Construction
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public DuplicateKeyException()
+        {
+        }
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public DuplicateKeyException
+            (
+                string message
+            )
+            : base(message)
+        {
+        }
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public DuplicateKeyException
+            (
+                string message,
+                Exception innerException
+            )
+            : base(message, innerException)
+        {
+        }
+
+        #endregion
+    }
+}
