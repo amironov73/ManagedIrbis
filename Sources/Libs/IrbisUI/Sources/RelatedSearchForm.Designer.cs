83819da60002616571acdf90e7eb7b01808f0242
Alexey Mironov <amironov73@gmail.com>
2016-09-20 19:22:33 +0800

    Add RelatedSearchForm

---
+++ Sources/Libs/IrbisUI/Sources/RelatedSearchForm.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace IrbisUI
+{
+    partial class RelatedSearchForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RelatedSearchForm));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this._okButton = new System.Windows.Forms.Button();
+            this._cancelButton = new System.Windows.Forms.Button();
+            this._listBox = new System.Windows.Forms.ListBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this._cancelButton);
+            this.panel1.Controls.Add(this._okButton);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 243);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(482, 60);
+            this.panel1.TabIndex = 0;
+            // 
+            // _okButton
+            // 
+            this._okButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this._okButton.Location = new System.Drawing.Point(12, 8);
+            this._okButton.Name = "_okButton";
+            this._okButton.Size = new System.Drawing.Size(120, 40);
+            this._okButton.TabIndex = 0;
+            this._okButton.Text = "O&K";
+            this._okButton.UseVisualStyleBackColor = true;
+            // 
+            // _cancelButton
+            // 
+            this._cancelButton.Location = new System.Drawing.Point(350, 8);
+            this._cancelButton.Name = "_cancelButton";
+            this._cancelButton.Size = new System.Drawing.Size(120, 40);
+            this._cancelButton.TabIndex = 1;
+            this._cancelButton.Text = "&Cancel";
+            this._cancelButton.UseVisualStyleBackColor = true;
+            // 
+            // _listBox
+            // 
+            this._listBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this._listBox.FormattingEnabled = true;
+            this._listBox.ItemHeight = 16;
+            this._listBox.Location = new System.Drawing.Point(0, 0);
+            this._listBox.Name = "_listBox";
+            this._listBox.Size = new System.Drawing.Size(482, 243);
+            this._listBox.TabIndex = 1;
+            // 
+            // RelatedSearchForm
+            // 
+            this.AcceptButton = this._okButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this._okButton;
+            this.ClientSize = new System.Drawing.Size(482, 303);
+            this.Controls.Add(this._listBox);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "RelatedSearchForm";
+            this.ShowInTaskbar = false;
+            this.Text = "Related search";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button _cancelButton;
+        private System.Windows.Forms.Button _okButton;
+        private System.Windows.Forms.ListBox _listBox;
+    }
+}
---
+++ Sources/Libs/IrbisUI/Sources/RelatedSearchForm.cs
@@ -0,0 +1,53 @@
+﻿/* RelatedSearch.cs --
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using CodeJam;
+
+using JetBrains.Annotations;
+
+using MoonSharp.Interpreter;
+
+#endregion
+
+namespace IrbisUI
+{
+    /// <summary>
+    /// Related search.
+    /// </summary>
+    [PublicAPI]
+    [MoonSharpUserData]
+    public partial class RelatedSearchForm
+        : Form
+    {
+        #region Properties
+
+        #endregion
+
+        #region Construction
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public RelatedSearchForm()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+    }
+}
