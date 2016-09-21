3cb42f05ed169eafe5eaa75207836e09bec82df2
Alexey Mironov <amironov73@gmail.com>
2016-09-21 14:07:40 +0800

    Add ConsoleControlOutput

---
+++ Sources/Libs/AM.Windows.Forms/AM/Windows/Forms/Console/ConsoleControlOutput.cs
@@ -0,0 +1,107 @@
+﻿/* ConsoleControlOutput.cs --
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using System;
+using System.Drawing;
+
+using AM.Text.Output;
+
+using CodeJam;
+
+using JetBrains.Annotations;
+
+using MoonSharp.Interpreter;
+
+#endregion
+
+namespace AM.Windows.Forms
+{
+    /// <summary>
+    /// Output to ConsoleControl.
+    /// </summary>
+    [PublicAPI]
+    [MoonSharpUserData]
+    public sealed class ConsoleControlOutput
+        : AbstractOutput
+    {
+        #region Properties
+
+        /// <summary>
+        /// Console control.
+        /// </summary>
+        [NotNull]
+        public ConsoleControl ConsoleControl { get; private set; }
+
+        #endregion
+
+        #region Construction
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public ConsoleControlOutput
+            (
+                [NotNull] ConsoleControl console
+            )
+        {
+            Code.NotNull(console, "console");
+
+            ConsoleControl = console;
+        }
+
+        #endregion
+
+        #region AbstractOutput members
+
+        /// <inheritdoc />
+        public override bool HaveError { get; set; }
+
+        /// <inheritdoc />
+        public override AbstractOutput Clear()
+        {
+            ConsoleControl.Clear();
+            HaveError = false;
+
+            return this;
+        }
+
+        /// <inheritdoc />
+        public override AbstractOutput Configure
+            (
+                string configuration
+            )
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public override AbstractOutput Write
+            (
+                string text
+            )
+        {
+            ConsoleControl.Write(text);
+
+            return this;
+        }
+
+        /// <inheritdoc />
+        public override AbstractOutput WriteError
+            (
+                string text
+            )
+        {
+            ConsoleControl.Write(Color.Red, text);
+            HaveError = true;
+
+            return this;
+        }
+
+        #endregion
+    }
+}
---
+++ Sources/UITests/Sources/Tests/ConsoleControlOutputTest.cs
@@ -0,0 +1,88 @@
+﻿/* ConsoleControlOutputTest.cs -- 
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using AM;
+using AM.Text.Output;
+using AM.Windows.Forms;
+
+using CodeJam;
+
+using IrbisUI;
+
+using JetBrains.Annotations;
+
+using ManagedIrbis;
+
+using MoonSharp.Interpreter;
+
+using Newtonsoft.Json;
+
+#endregion
+
+namespace UITests
+{
+    public sealed class ConsoleControlOutputTest
+        : IUITest
+    {
+        #region IUITest members
+
+        public void Run
+            (
+                IWin32Window ownerWindow
+            )
+        {
+            using (Form form = new Form())
+            {
+                form.Size = new Size(800, 600);
+
+                TextBox textBox = new TextBox
+                {
+                    Location = new Point(10, 10),
+                    Width = 200
+                };
+                form.Controls.Add(textBox);
+
+                Button button = new Button
+                {
+                    Location = new Point(250, 10),
+                    Text = "Enter"
+                };
+                form.Controls.Add(button);
+
+                ConsoleControl console = new ConsoleControl
+                {
+                    Location = new Point(10, 50),
+                    ForeColor = Color.Yellow
+                };
+                form.Controls.Add(console);
+
+                AbstractOutput output
+                    = new ConsoleControlOutput(console);
+
+                button.Click += (sender, args) =>
+                {
+                    string text = textBox.Text;
+                    output.WriteLine(text);
+                };
+
+                form.ShowDialog(ownerWindow);
+            }
+        }
+
+        #endregion
+    }
+}
