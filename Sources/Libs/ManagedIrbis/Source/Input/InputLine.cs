61848e00b561d5599e137d728cad0372840e02dd
Alexey Mironov <amironov73@gmail.com>
2016-09-25 12:15:06 +0800

    Add InputLine and InputSpecification

---
+++ Sources/Libs/ManagedIrbis/Source/Input/InputLine.cs
@@ -0,0 +1,130 @@
+﻿/* InputLine.cs --
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using System.IO;
+using System.Xml.Serialization;
+
+using AM;
+using AM.IO;
+using AM.Runtime;
+
+using CodeJam;
+
+using JetBrains.Annotations;
+
+using MoonSharp.Interpreter;
+
+using Newtonsoft.Json;
+
+#endregion
+
+namespace ManagedIrbis.Input
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [PublicAPI]
+    [XmlRoot("line")]
+    [MoonSharpUserData]
+    public sealed class InputLine
+        : IHandmadeSerializable,
+        IVerifiable
+    {
+        #region Properties
+
+        /// <summary>
+        /// Hint.
+        /// </summary>
+        [CanBeNull]
+        [XmlAttribute("hint")]
+        [JsonProperty("hint")]
+        public string Hint { get; set; }
+
+        /// <summary>
+        /// Mandatory?
+        /// </summary>
+        [XmlAttribute("mandatory")]
+        public bool Mandatory { get; set; }
+
+        /// <summary>
+        /// Tag (e. g. "200^a").
+        /// </summary>
+        [CanBeNull]
+        [XmlAttribute("tag")]
+        [JsonProperty("tag")]
+        public string Tag { get; set; }
+
+        #endregion
+
+        #region Construction
+
+        #endregion
+
+        #region Private members
+
+        #endregion
+
+        #region Public methods
+
+        #endregion
+
+        #region IHandmadeSerializable members
+
+        /// <inheritdoc />
+        public void RestoreFromStream
+            (
+                BinaryReader reader
+            )
+        {
+            Code.NotNull(reader, "reader");
+
+            Hint = reader.ReadNullableString();
+            Mandatory = reader.ReadBoolean();
+            Tag = reader.ReadNullableString();
+        }
+
+        /// <inheritdoc />
+        public void SaveToStream
+            (
+                BinaryWriter writer
+            )
+        {
+            Code.NotNull(writer, "writer");
+
+            writer
+                .WriteNullable(Hint)
+                .Write(Mandatory);
+            writer
+                .WriteNullable(Tag);
+        }
+
+        #endregion
+
+        #region IVerifiable members
+
+        /// <inheritdoc />
+        public bool Verify
+            (
+                bool throwOnError
+            )
+        {
+            Verifier<InputLine> verifier = new Verifier<InputLine>
+                (
+                    this,
+                    throwOnError
+                );
+
+            verifier
+                .NotNullNorEmpty(Tag, "Tag");
+
+            return verifier.Result;
+        }
+
+        #endregion
+    }
+}
---
+++ Sources/Libs/ManagedIrbis/Source/Input/InputSpecification.cs
@@ -0,0 +1,178 @@
+﻿/* InputSpecification.cs --
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using System.IO;
+using System.Xml.Serialization;
+
+using AM;
+using AM.Json;
+using AM.IO;
+using AM.Runtime;
+using AM.Xml;
+
+using CodeJam;
+
+using JetBrains.Annotations;
+
+using MoonSharp.Interpreter;
+
+using Newtonsoft.Json;
+
+#endregion
+
+namespace ManagedIrbis.Input
+{
+    /// <summary>
+    /// Input specification.
+    /// </summary>
+    [PublicAPI]
+    [XmlRoot("input")]
+    [MoonSharpUserData]
+    public sealed class InputSpecification
+        : IHandmadeSerializable,
+        IVerifiable
+    {
+        #region Properties
+
+        /// <summary>
+        /// Input lines.
+        /// </summary>
+        [CanBeNull]
+        [XmlElement("line")]
+        [JsonProperty("lines")]
+        public InputLine[] Lines { get; set; }
+
+        /// <summary>
+        /// Name.
+        /// </summary>
+        [CanBeNull]
+        [XmlAttribute("name")]
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Title.
+        /// </summary>
+        [CanBeNull]
+        [XmlAttribute("title")]
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        #endregion
+
+        #region Construction
+
+        #endregion
+
+        #region Private members
+
+        #endregion
+
+        #region Public methods
+
+        /// <summary>
+        /// Load specification from JSON file.
+        /// </summary>
+        [NotNull]
+        public static InputSpecification LoadJsonFile
+            (
+                [NotNull] string fileName
+            )
+        {
+            Code.NotNullNorEmpty(fileName, "fileName");
+
+            InputSpecification result 
+                = JsonUtility.ReadObjectFromFile<InputSpecification>
+                (
+                    fileName
+                );
+
+            return result;
+        }
+
+        /// <summary>
+        /// Load specification from XML file.
+        /// </summary>
+        [NotNull]
+        public static InputSpecification LoadXmlFile
+            (
+                [NotNull] string fileName
+            )
+        {
+            Code.NotNullNorEmpty(fileName, "fileName");
+
+            InputSpecification result
+                = XmlUtility.Deserialize<InputSpecification>
+                (
+                    fileName
+                );
+
+            return result;
+        }
+
+        #endregion
+
+        #region IHandmadeSerializable
+
+        /// <inheritdoc />
+        public void RestoreFromStream
+            (
+                BinaryReader reader
+            )
+        {
+            Code.NotNull(reader, "reader");
+
+            Name = reader.ReadNullableString();
+            Title = reader.ReadNullableString();
+            reader.RestoreArray<InputLine>();
+        }
+
+        /// <inheritdoc />
+        public void SaveToStream
+            (
+                BinaryWriter writer
+            )
+        {
+            Code.NotNull(writer, "writer");
+
+            writer
+                .WriteNullable(Name)
+                .WriteNullable(Title);
+            Lines.SaveToStream(writer);
+        }
+
+        #endregion
+
+        #region IVerifiable members
+
+        /// <inheritdoc />
+        public bool Verify
+            (
+                bool throwOnError
+            )
+        {
+            Verifier<InputSpecification> verifier 
+                = new Verifier<InputSpecification>
+                (
+                    this,
+                    throwOnError
+                );
+
+            verifier
+                .NotNullNorEmpty(Name, "Name")
+                .NotNullNorEmpty(Title, "Title")
+                .NotNull(Lines, "Lines")
+                // ReSharper disable once PossibleNullReferenceException
+                .Assert(Lines.Length != 0, "Lines.Count");
+
+            return verifier.Result;
+        }
+
+        #endregion
+    }
+}
