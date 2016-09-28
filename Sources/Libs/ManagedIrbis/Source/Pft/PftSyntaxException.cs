f18e59fee2ed99ea4b28125eade385fbbdbd4be5
Alexey Mironov <amironov73@gmail.com>
2016-09-28 13:37:02 +0900

    Add PftSemanticException and PftSyntaxException

---
+++ Sources/Libs/ManagedIrbis/Source/Pft/PftSyntaxException.cs
@@ -0,0 +1,69 @@
+﻿/* PftSyntaxException.cs --
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using System;
+
+using JetBrains.Annotations;
+
+#endregion
+
+namespace ManagedIrbis.Pft
+{
+    /// <summary>
+    /// Исключение, возникающее при разборе PFT-скрипта.
+    /// </summary>
+    public sealed class PftSyntaxException
+        : PftException
+    {
+        #region Properties
+
+        #endregion
+
+        #region Construction
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public PftSyntaxException()
+        {
+        }
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public PftSyntaxException
+            (
+                string message
+            )
+            : base(message)
+        {
+        }
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public PftSyntaxException
+            (
+                string message,
+                Exception innerException
+            )
+            : base
+            (
+                message,
+                innerException
+            )
+        {
+        }
+
+        #endregion
+
+        #region Public methods
+
+        #endregion
+    }
+}
