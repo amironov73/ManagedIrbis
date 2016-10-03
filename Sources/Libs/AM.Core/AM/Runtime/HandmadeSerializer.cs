f7a2518dce41cc4f56949f7d2961fe65c86f79b1
Alexey Mironov <amironov73@gmail.com>
2016-10-03 10:26:30 +0900

    Add HandmadeSerializer

---
+++ Sources/Libs/AM.Core/AM/Runtime/HandmadeSerializer.cs
@@ -0,0 +1,236 @@
+﻿/* HandmadeSerializer.cs --
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+using AM.IO;
+#if !NETCORE
+
+#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CodeJam;
+
+using JetBrains.Annotations;
+
+using MoonSharp.Interpreter;
+
+#endregion
+
+namespace AM.Runtime
+{
+    /// <summary>
+    /// Simple serializer.
+    /// </summary>
+    [PublicAPI]
+    [MoonSharpUserData]
+    public sealed class HandmadeSerializer
+    {
+        #region Properties
+
+        /// <summary>
+        /// Namespace for short type names.
+        /// </summary>
+        [CanBeNull]
+        public string Namespace { get; set; }
+
+        /// <summary>
+        /// Prefix length.
+        /// </summary>
+        public PrefixLength PrefixLength { get; set; }
+
+        #endregion
+
+        #region Construction
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public HandmadeSerializer()
+        {
+            PrefixLength = PrefixLength.Full;
+        }
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public HandmadeSerializer
+            (
+                PrefixLength prefixLength
+            )
+        {
+            PrefixLength = prefixLength;
+        }
+
+        #endregion
+
+        #region Private members
+
+        #endregion
+
+        #region Public methods
+
+        /// <summary>
+        /// Deserialize object.
+        /// </summary>
+        [NotNull]
+        public IHandmadeSerializable Deserialize
+            (
+                [NotNull] BinaryReader reader
+            )
+        {
+            Code.NotNull(reader, "reader");
+
+            string typeName = reader.ReadString();
+
+            if (PrefixLength == PrefixLength.Short)
+            {
+                typeName = Namespace + "." + typeName;
+            }
+
+            Type type = Type.GetType(typeName, true);
+            IHandmadeSerializable result
+                = (IHandmadeSerializable) Activator.CreateInstance(type);
+
+            result.RestoreFromStream(reader);
+
+            return result;
+        }
+
+        /// <summary>
+        /// Deserialize array of objects.
+        /// </summary>
+        [NotNull]
+        [ItemNotNull]
+        public IHandmadeSerializable[] DeserializeArray
+            (
+                [NotNull] BinaryReader reader
+            )
+        {
+            Code.NotNull(reader, "reader");
+
+            int count = reader.ReadPackedInt32();
+            string typeName = reader.ReadString();
+
+            if (PrefixLength == PrefixLength.Short)
+            {
+                typeName = Namespace + "." + typeName;
+            }
+            Type type = Type.GetType(typeName, true);
+
+            IHandmadeSerializable[] result
+                = new IHandmadeSerializable[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                IHandmadeSerializable obj
+                    = (IHandmadeSerializable) Activator.CreateInstance(type);
+                obj.RestoreFromStream(reader);
+                result[i] = obj;
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Get name of specified type.
+        /// </summary>
+        [NotNull]
+        public string GetTypeName
+            (
+                [NotNull] object obj
+            )
+        {
+            Code.NotNull(obj, "obj");
+
+            Type type = obj.GetType();
+            string result;
+
+            switch (PrefixLength)
+            {
+                case PrefixLength.Short:
+                    result = type.Name;
+                    break;
+
+                case PrefixLength.Moderate:
+                    result = type.FullName;
+                    break;
+
+                case PrefixLength.Full:
+                    result = type.AssemblyQualifiedName;
+                    break;
+
+                default:
+                    throw new InvalidOperationException();
+            }
+
+            return result.ThrowIfNull("result");
+        }
+
+        /// <summary>
+        /// Serialize the object.
+        /// </summary>
+        [NotNull]
+        public HandmadeSerializer Serialize
+            (
+                [NotNull] BinaryWriter writer,
+                [NotNull] IHandmadeSerializable obj
+            )
+        {
+            Code.NotNull(writer, "writer");
+            Code.NotNull(obj, "obj");
+
+            string typeName = GetTypeName(obj);
+            writer.Write(typeName);
+
+            obj.SaveToStream(writer);
+
+            return this;
+        }
+
+        /// <summary>
+        /// Serialize the object.
+        /// </summary>
+        [NotNull]
+        public HandmadeSerializer Serialize
+            (
+                [NotNull] BinaryWriter writer,
+                [NotNull][ItemNotNull] IHandmadeSerializable[] array
+            )
+        {
+            Code.NotNull(writer, "writer");
+            Code.NotNull(array, "array");
+
+            int count = array.Length;
+            if (count == 0)
+            {
+                throw new ArgumentException();
+            }
+
+            writer.WritePackedInt32(count);
+
+            IHandmadeSerializable first = array[0];
+
+            string typeName = GetTypeName(first);
+            writer.Write(typeName);
+
+            foreach (IHandmadeSerializable obj in array)
+            {
+                obj.SaveToStream(writer);
+            }
+
+            return this;
+        }
+
+        #endregion
+    }
+}
+
+#endif
---
+++ Sources/UnitTests/AM/Runtime/HandmadeSerializerTest.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using AM.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using AM.Runtime;
+
+namespace UnitTests.AM.Runtime
+{
+    class MyFirstClass
+        : IHandmadeSerializable
+    {
+        public int FirstField { get; set; }
+
+        public void RestoreFromStream
+            (
+                BinaryReader reader
+            )
+        {
+            FirstField = reader.ReadPackedInt32();
+        }
+
+        public void SaveToStream
+            (
+                BinaryWriter writer
+            )
+        {
+            writer.WritePackedInt32(FirstField);
+        }
+    }
+
+    class MySecondClass
+        : IHandmadeSerializable
+    {
+        public string SecondField { get; set; }
+
+        public void RestoreFromStream
+            (
+                BinaryReader reader
+            )
+        {
+            SecondField = reader.ReadNullableString();
+        }
+
+        public void SaveToStream
+            (
+                BinaryWriter writer
+            )
+        {
+            writer.WriteNullable(SecondField);
+        }
+    }
+
+    class MyThirdClass
+        : IHandmadeSerializable
+    {
+        public bool ThirdField { get; set; }
+
+        public double FourthField { get; set; }
+
+        public void RestoreFromStream
+            (
+                BinaryReader reader
+            )
+        {
+            ThirdField = reader.ReadBoolean();
+            FourthField = reader.ReadDouble();
+        }
+
+        public void SaveToStream
+            (
+                BinaryWriter writer
+            )
+        {
+            writer.Write(ThirdField);
+            writer.Write(FourthField);
+        }
+    }
+
+
+    [TestClass]
+    public class HandmadeSerializerTest
+    {
+
+        [TestMethod]
+        public void HandmadeSerializer_Roundtrip()
+        {
+            MyFirstClass first1 = new MyFirstClass
+            {
+                FirstField = 73
+            };
+            MySecondClass second1 = new MySecondClass
+            {
+                SecondField = "Hello"
+            };
+            MyThirdClass third1 = new MyThirdClass
+            {
+                ThirdField = true,
+                FourthField = 123.45
+            };
+
+            HandmadeSerializer serializer = new HandmadeSerializer();
+            MemoryStream stream = new MemoryStream();
+            using (BinaryWriter writer = new BinaryWriter(stream))
+            {
+                serializer.Serialize(writer, first1);
+                serializer.Serialize(writer, second1);
+                serializer.Serialize(writer, third1);
+            }
+
+            byte[] buffer = stream.ToArray();
+
+            stream = new MemoryStream(buffer);
+            using (BinaryReader reader = new BinaryReader(stream))
+            {
+                MyFirstClass first2
+                    = (MyFirstClass) serializer.Deserialize(reader);
+                Assert.AreEqual(first1.FirstField, first2.FirstField);
+
+                MySecondClass second2
+                    = (MySecondClass) serializer.Deserialize(reader);
+                Assert.AreEqual(second1.SecondField, second2.SecondField);
+
+                MyThirdClass third2
+                    = (MyThirdClass) serializer.Deserialize(reader);
+                Assert.AreEqual(third1.ThirdField, third2.ThirdField);
+                Assert.AreEqual(third1.FourthField, third2.FourthField);
+            }
+        }
+
+        [TestMethod]
+        public void HandmadeSerializer_Array()
+        {
+            MyFirstClass[] sourceArray =
+            {
+                new MyFirstClass{FirstField = 1}, 
+                new MyFirstClass{FirstField = 2}, 
+                new MyFirstClass{FirstField = 3}, 
+                new MyFirstClass{FirstField = 4}, 
+            };
+
+
+            HandmadeSerializer serializer = new HandmadeSerializer();
+            MemoryStream stream = new MemoryStream();
+            using (BinaryWriter writer = new BinaryWriter(stream))
+            {
+                serializer.Serialize(writer, sourceArray);
+            }
+
+            byte[] buffer = stream.ToArray();
+
+            stream = new MemoryStream(buffer);
+            using (BinaryReader reader = new BinaryReader(stream))
+            {
+                IHandmadeSerializable[] tempArray
+                    = serializer.DeserializeArray(reader);
+                MyFirstClass[] targetArray
+                    = tempArray.Cast<MyFirstClass>().ToArray();
+                Assert.AreEqual(sourceArray.Length, targetArray.Length);
+
+                for (int i = 0; i < sourceArray.Length; i++)
+                {
+                    Assert.AreEqual
+                        (
+                            sourceArray[i].FirstField,
+                            targetArray[i].FirstField
+                        );
+                }
+            }
+        }
+    }
+}
