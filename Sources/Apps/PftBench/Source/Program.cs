a3dd8b7c4e9305466e225dfa5cfbb9c7b11842a4
Alexey Mironov <amironov73@gmail.com>
2016-10-05 14:09:00 +0900

    Add PftBench.csproj

---
+++ Sources/Apps/PftBench/Source/Program.cs
@@ -0,0 +1,32 @@
+﻿/* Program.cs --
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+#endregion
+
+namespace PftBench
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+}
