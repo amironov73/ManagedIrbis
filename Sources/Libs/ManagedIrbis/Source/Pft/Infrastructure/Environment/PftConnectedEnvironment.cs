cd81d5fcab71d156f0f21923c0e96308792abbf4
Alexey Mironov <amironov73@gmail.com>
2016-10-05 13:42:18 +0900

    Add PftEnvironmentAbstraction and friends

---
+++ Sources/Libs/ManagedIrbis/Source/Pft/Infrastructure/Environment/PftConnectedEnvironment.cs
@@ -0,0 +1,39 @@
+﻿/* PftConnectedEnvironment.cs --
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using System.Diagnostics;
+using System.Globalization;
+using System.Text;
+
+using AM;
+using AM.Text;
+
+using CodeJam;
+
+using JetBrains.Annotations;
+
+using ManagedIrbis.Infrastructure;
+
+using MoonSharp.Interpreter;
+
+#endregion
+
+namespace ManagedIrbis.Pft.Infrastructure.Environment
+{
+    /// <summary>
+    /// Connected to the server.
+    /// </summary>
+    [PublicAPI]
+    [MoonSharpUserData]
+    public sealed class PftConnectedEnvironment
+    {
+        #region PftEnvironmentAbstraction members
+
+        #endregion
+    }
+}
