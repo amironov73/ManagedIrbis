a9ed6c4828e789d100f356c6fefdfd12a067f9aa
Alexey Mironov <amironov73@gmail.com>
2016-10-08 15:57:11 +0800

    Add compiledroid.cmd

---
+++ compiledroid.cmd
@@ -0,0 +1,18 @@
+@echo off
+
+cd Source\Droid
+
+SET MSBUILD=%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe
+SET OUTPUT=..\..\..\Binaries
+SET PARAMS=/consoleloggerparameters:ErrorsOnly /m
+SET NUGET=..\..\Nuget\nuget.exe
+
+%NUGET% restore
+
+"%MSBUILD%" AM.Core\AM.Core.csproj           /target:ReBuild /property:Configuration=Debug   /property:OutputPath=%OUTPUT%\DebugDroid /p:DefineConstants="DEBUG;ANDROID" %PARAMS%
+"%MSBUILD%" ManagedIrbis\ManagedIrbis.csproj /target:ReBuild /property:Configuration=Debug   /property:OutputPath=%OUTPUT%\DebugDroid /p:DefineConstants="DEBUG;ANDROID" %PARAMS%
+
+"%MSBUILD%" AM.Core\AM.Core.csproj           /target:ReBuild /property:Configuration=Release /property:OutputPath=%OUTPUT%\ReleaseDroid /p:DefineConstants="ANDROID"     %PARAMS%
+"%MSBUILD%" ManagedIrbis\ManagedIrbis.csproj /target:ReBuild /property:Configuration=Release /property:OutputPath=%OUTPUT%\ReleaseDroid /p:DefineConstants="ANDROID"     %PARAMS%
+
+cd ..\..
