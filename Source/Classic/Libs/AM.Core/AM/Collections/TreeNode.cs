154769f636c66fd76ccada70824e2c925177e385
Alexey Mironov <amironov73@gmail.com>
2016-10-15 14:33:17 +0800

    Add TreeNode class

---
+++ Source/Classic/Libs/AM.Core/AM/Collections/TreeNode.cs
@@ -0,0 +1,133 @@
+﻿/* TreeNode.cs --
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CodeJam;
+
+using JetBrains.Annotations;
+
+using MoonSharp.Interpreter;
+
+#endregion
+
+namespace AM.Collections
+{
+    /// <summary>
+    /// Generic tree node.
+    /// </summary>
+    [PublicAPI]
+    [MoonSharpUserData]
+    public class TreeNode<T>
+    {
+        #region Properties
+
+        /// <summary>
+        /// Children nodes.
+        /// </summary>
+        [NotNull]
+        public NonNullCollection<TreeNode<T>> Children
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// User defined value.
+        /// </summary>
+        public T Value { get; set; }
+
+        #endregion
+
+        #region Construction
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public TreeNode()
+        {
+            Children = new NonNullCollection<TreeNode<T>>();
+        }
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public TreeNode
+            (
+                T value
+            )
+            : this()
+        {
+            Value = value;
+        }
+
+        #endregion
+
+        #region Private members
+
+        #endregion
+
+        #region Public methods
+
+        /// <summary>
+        /// Add child with a specified value.
+        /// </summary>
+        public TreeNode<T> AddChild
+            (
+                T value
+            )
+        {
+            TreeNode<T> child = new TreeNode<T>(value);
+            Children.Add(child);
+
+            return child;
+        }
+
+        /// <summary>
+        /// Get descendants of the node.
+        /// </summary>
+        [NotNull]
+        [ItemNotNull]
+        public NonNullCollection<TreeNode<T>> GetDescendants()
+        {
+            NonNullCollection<TreeNode<T>> result
+                = new NonNullCollection<TreeNode<T>>();
+
+            foreach (TreeNode<T> child in Children)
+            {
+                result.Add(child);
+                result.AddRange(child.GetDescendants());
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Walk through the tree starting the current node.
+        /// </summary>
+        public void Walk
+            (
+                [NotNull] Action<TreeNode<T>> action
+            )
+        {
+            Code.NotNull(action, "action");
+
+            action(this);
+            foreach (TreeNode<T> child in Children)
+            {
+                child.Walk(action);
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Source/UnitTests/AM/Collections/TreeNodeTest.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using AM.Collections;
+
+namespace UnitTests.AM.Collections
+{
+    [TestClass]
+    public class TreeNodeTest
+    {
+        [TestMethod]
+        public void TreeNode_Construction()
+        {
+            TreeNode<char> node = new TreeNode<char>('a');
+            Assert.IsNotNull(node.Children);
+            Assert.AreEqual(0, node.Children);
+            Assert.AreEqual('a', node.Value);
+        }
+
+        [TestMethod]
+        public void TreeNode_AddChild()
+        {
+            TreeNode<char> root = new TreeNode<char>('a');
+            TreeNode<char> level1 = root.AddChild('b');
+            TreeNode<char> level2 = level1.AddChild('c');
+            TreeNode<char> level3 = level2.AddChild('d');
+            Assert.AreEqual(1, root.Children.Count);
+            Assert.AreEqual(1, level1.Children.Count);
+            Assert.AreEqual(1, level2.Children.Count);
+            Assert.AreEqual(0, level3.Children.Count);
+        }
+
+        [TestMethod]
+        public void TreeNode_GetDescendants()
+        {
+            TreeNode<char> root = new TreeNode<char>('a');
+            TreeNode<char> level1 = root.AddChild('b');
+            TreeNode<char> level2 = level1.AddChild('c');
+            TreeNode<char> level3 = level2.AddChild('d');
+
+            NonNullCollection<TreeNode<char>> descendants
+                = root.GetDescendants();
+            Assert.AreEqual(3, descendants.Count);
+            Assert.IsTrue(descendants.Contains(level1));
+            Assert.IsTrue(descendants.Contains(level2));
+            Assert.IsTrue(descendants.Contains(level3));
+        }
+
+        [TestMethod]
+        public void TreeNode_Walk()
+        {
+            TreeNode<char> root = new TreeNode<char>('a');
+            TreeNode<char> level1 = root.AddChild('b');
+            TreeNode<char> level2 = level1.AddChild('c');
+            TreeNode<char> level3 = level2.AddChild('d');
+
+            StringBuilder builder = new StringBuilder();
+            root.Walk(node => builder.Append(node.Value));
+            Assert.AreEqual("abcd", builder.ToString());
+        }
+    }
+}
