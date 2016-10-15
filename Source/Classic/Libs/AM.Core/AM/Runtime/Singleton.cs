850a2c1e3f8110de57e529b2537a993e3828f956
Alexey Mironov <amironov73@gmail.com>
2016-10-15 15:48:43 +0800

    Add Singleton class

---
+++ Source/Classic/Libs/AM.Core/AM/Runtime/Singleton.cs
@@ -0,0 +1,82 @@
+﻿/* Singleton.cs --
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#if CLASSIC
+
+#region Using directives
+
+using System;
+using System.Collections.Generic;
+
+using JetBrains.Annotations;
+
+using MoonSharp.Interpreter;
+
+#endregion
+
+namespace AM.Runtime
+{
+    /// <summary>
+    /// Хранилище объектов по принципу "каждого типа по одной штуке".
+    /// </summary>
+    [PublicAPI]
+    [MoonSharpUserData]
+    public static class Singleton
+    {
+        #region Private members
+
+        private static Dictionary<Type, object> _dictionary
+            = new Dictionary<Type, object>();
+
+        #endregion
+
+        #region Public methods
+
+        /// <summary>
+        /// Do we have instance of specified type?
+        /// </summary>
+        public static bool HaveInstance<T>()
+            where T : class
+        {
+            Type type = typeof(T);
+
+            lock (_dictionary)
+            {
+                return _dictionary.ContainsKey(type);
+            }
+        }
+
+        /// <summary>
+        /// Выдает объект указанного типа.
+        /// </summary>
+        public static T Instance<T>()
+                where T : class
+        {
+            T result;
+            Type type = typeof(T);
+
+            lock (_dictionary)
+            {
+                object value;
+                if (_dictionary.TryGetValue(type, out value))
+                {
+                    result = (T)value;
+                }
+                else
+                {
+                    result = Activator.CreateInstance<T>();
+                }
+            }
+
+            return result;
+        }
+
+        #endregion
+    }
+}
+
+#endif
+
