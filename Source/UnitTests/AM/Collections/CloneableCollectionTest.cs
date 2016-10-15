6cf0ec86501c55f8d219480eb5cdc85865172895
Alexey Mironov <amironov73@gmail.com>
2016-10-15 14:57:09 +0800

    Add CloneabeDictionary class

---
+++ Source/UnitTests/AM/Collections/CloneableCollectionTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using AM.Collections;
+
+namespace UnitTests.AM.Collections
+{
+    [TestClass]
+    public class CloneableCollectionTest
+    {
+        [TestMethod]
+        public void CloneableCollection_Clone()
+        {
+            CloneableCollection<int> source
+                = new CloneableCollection<int>
+                {
+                    212,
+                    85,
+                    06
+                };
+            CloneableCollection<int> clone
+                = (CloneableCollection<int>) source.Clone();
+
+            Assert.AreEqual(source.Count, clone.Count);
+            for (int i = 0; i < source.Count; i++)
+            {
+                Assert.AreEqual(source[i], clone[i]);
+            }
+        }
+    }
+}
