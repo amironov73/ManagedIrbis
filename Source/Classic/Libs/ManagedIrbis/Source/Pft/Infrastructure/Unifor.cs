1ae0ad0294f00aa186d7cc3a1c885af59d31f082
Alexey Mironov <amironov73@gmail.com>
2016-10-20 10:30:30 +0900

    Add Unifor Q

--- Source/Classic/Libs/ManagedIrbis/Source/Pft/Infrastructure/Unifor.cs
+++ Source/Classic/Libs/ManagedIrbis/Source/Pft/Infrastructure/Unifor.cs
@@ -11,6 +11,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 using AM;
@@ -20,7 +21,7 @@ using CodeJam;
 using JetBrains.Annotations;
 
 using ManagedIrbis.ImportExport;
-
+using ManagedIrbis.Pft.Infrastructure.Unifors;
 using MoonSharp.Interpreter;
 
 #endregion
@@ -79,11 +80,13 @@ StringComparer.InvariantCultureIgnoreCase
         private static void RegisterActions()
         {
             Registry.Add("0", FormatAll);
-            Registry.Add("3", PrintDate);
+            Registry.Add("3", Unifor3.PrintDate);
             Registry.Add("9", RemoveDoubleQuotes);
             Registry.Add("A", GetFieldRepeat);
+            Registry.Add("Q", ToLower);
             Registry.Add("R", RandomNumber);
             Registry.Add("T", Transliterate);
+            Registry.Add("X", RemoveAngleBrackets);
             Registry.Add("+90", GetIndex);
         }
 
@@ -197,219 +200,6 @@ StringComparer.InvariantCultureIgnoreCase
             context.OutputFlag = true;
         }
 
-        private static string[] monthNames1 =
-        {
-            "январь", "февраль", "март", "апрель", "май", "июнь",
-            "июль", "август", "сентябрь", "октябрь", "ноябрь", "декабрь"
-        };
-
-        private static string[] monthNames2 =
-        {
-            "января", "февраля", "марта", "апреля", "мая", "июня",
-            "июля", "августа", "сентября", "октября", "ноября", "декабря"
-        };
-
-        private static string[] monthNames3 =
-        {
-            "january", "february", "march", "april", "may", "june",
-            "july", "august", "september", "october", "november", "december"
-        };
-
-        private static string _GetMonthName
-            (
-                string expression,
-                string[] table
-            )
-        {
-            int index;
-
-            if (expression.Length != 3)
-            {
-                return null;
-            }
-            if (!int.TryParse(expression.Substring(1), out index))
-            {
-                return null;
-            }
-            index--;
-            if (index < 0 || index >= table.Length)
-            {
-                return null;
-            }
-
-            return table[index];
-        }
-
-        private static string _AddDate
-            (
-                string expression,
-                bool changeSign
-            )
-        {
-            if (expression.Length < 11)
-            {
-                return null;
-            }
-
-            string dateString = expression.Substring(1, 8);
-            DateTime date;
-            if (!DateTime.TryParseExact
-                (
-                    dateString,
-                    "yyyyMMdd",
-                    null,
-                    DateTimeStyles.None,
-                    out date
-                ))
-            {
-                return null;
-            }
-            string deltaString = expression.Substring(10);
-            int delta;
-            if (!int.TryParse(deltaString, out delta))
-            {
-                return null;
-            }
-            if (changeSign)
-            {
-                delta = -delta;
-            }
-            date = date.AddDays(delta);
-
-            return date.ToString("yyyyMMdd");
-        }
-
-        private static string _ToJulianDate
-            (
-                string expression
-            )
-        {
-#if CLASSIC
-
-            if (expression.Length < 9)
-            {
-                return null;
-            }
-
-            DateTime date;
-            if (!DateTime.TryParseExact
-                (
-                    expression.Substring(1),
-                    "yyyyMMdd",
-                    null,
-                    DateTimeStyles.None,
-                    out date
-                ))
-            {
-                return null;
-            }
-
-            return DateTimeUtility.FromJulianDate(date);
-
-#else
-
-            return null;
-
-#endif
-        }
-
-        /// <summary>
-        /// Print current date.
-        /// </summary>
-        public static void PrintDate
-            (
-                PftContext context,
-                PftNode node,
-                string expression
-            )
-        {
-            if (ReferenceEquals(expression, null))
-            {
-                return;
-            }
-
-            char secondChar = expression.Length == 0
-                ? '\0'
-                : expression[0];
-            string format = null;
-            DateTime now = DateTime.Now;
-            switch (secondChar)
-            {
-                case '\0':
-                    format = "{0:yyyyMMdd}";
-                    break;
-
-                case '0':
-                    format = "{0:yyyy}";
-                    break;
-
-                case '1':
-                    format = "{0:MM}";
-                    break;
-
-                case '2':
-                    format = "{0:dd}";
-                    break;
-
-                case '3':
-                    format = "{0:yy}";
-                    break;
-
-                case '4':
-                    format = now.Month.ToInvariantString();
-                    break;
-
-                case '5':
-                    format = now.Day.ToInvariantString();
-                    break;
-
-                case '6':
-                    format = _GetMonthName(expression, monthNames1);
-                    break;
-
-                case '7':
-                    format = _GetMonthName(expression, monthNames2);
-                    break;
-
-                case '8':
-                    format = _GetMonthName(expression, monthNames3);
-                    break;
-
-                case '9':
-                    format = "{0:hh:mm:ss}";
-                    break;
-
-                case 'a':
-                case 'A':
-                    format = now.DayOfYear.ToInvariantString();
-                    break;
-
-                case 'b':
-                case 'B':
-                    format = _AddDate(expression, false);
-                    break;
-
-                case 'c':
-                case 'C':
-                    format = _AddDate(expression, true);
-                    break;
-
-                case 'j':
-                case 'J':
-                    format = _ToJulianDate(expression);
-                    break;
-
-                default:
-                    return;
-            }
-
-            if (!string.IsNullOrEmpty(format))
-            {
-                string output = string.Format(format, now);
-                context.Write(node, output);
-                context.OutputFlag = true;
-            }
-        }
 
         /// <summary>
         /// Generate random number.
@@ -441,6 +231,25 @@ StringComparer.InvariantCultureIgnoreCase
             string format = new string('0', length);
             string output = result.ToString(format);
             context.Write(node, output);
+            context.OutputFlag = true;
+        }
+
+        /// <summary>
+        /// Remove text surrounded with angle brackets.
+        /// </summary>
+        public static void RemoveAngleBrackets
+            (
+                PftContext context,
+                PftNode node,
+                string expression
+            )
+        {
+            if (!string.IsNullOrEmpty(expression))
+            {
+                string clear = Regex.Replace(expression, "<.*?>", string.Empty);
+                context.Write(node, clear);
+                context.OutputFlag = true;
+            }
         }
 
         /// <summary>
@@ -457,6 +266,25 @@ StringComparer.InvariantCultureIgnoreCase
             {
                 string clear = expression.Replace("\"", string.Empty);
                 context.Write(node, clear);
+                context.OutputFlag = true;
+            }
+        }
+
+        /// <summary>
+        /// Convert the string to lower case.
+        /// </summary>
+        public static void ToLower
+            (
+                PftContext context,
+                PftNode node,
+                string expression
+            )
+        {
+            if (!string.IsNullOrEmpty(expression))
+            {
+                string output = expression.ToLowerInvariant();
+                context.Write(node, output);
+                context.OutputFlag = true;
             }
         }
 
@@ -480,7 +308,7 @@ StringComparer.InvariantCultureIgnoreCase
             };
 
         /// <summary>
-        /// Remove double quotes from the string.
+        /// Transliterate cyrillic to latin.
         /// </summary>
         public static void Transliterate
             (
@@ -511,6 +339,7 @@ StringComparer.InvariantCultureIgnoreCase
                 }
 
                 context.Write(node, result.ToString());
+                context.OutputFlag = true;
             }
         }
 
