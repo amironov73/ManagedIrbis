03e2ba30c8f648d3d505ed9fd2806209ac7a20e6
Alexey Mironov <amironov73@gmail.com>
2016-10-22 16:12:22 +0800

    Add PftL and PftRef classes

---
+++ Source/Classic/Libs/ManagedIrbis/Source/Pft/Infrastructure/Ast/PftL.cs
@@ -0,0 +1,86 @@
+﻿/* PftL.cs --
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CodeJam;
+
+using JetBrains.Annotations;
+
+using MoonSharp.Interpreter;
+
+#endregion
+
+namespace ManagedIrbis.Pft.Infrastructure.Ast
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [PublicAPI]
+    [MoonSharpUserData]
+    public sealed class PftL
+        : PftNode
+    {
+        #region Properties
+
+        #endregion
+
+        #region Construction
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public PftL()
+        {
+        }
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public PftL
+            (
+                [NotNull] PftToken token
+            )
+            : base(token)
+        {
+            Code.NotNull(token, "token");
+            token.MustBe(PftTokenKind.L);
+        }
+
+        #endregion
+
+        #region Private members
+
+        #endregion
+
+        #region Public methods
+
+        #endregion
+
+        #region PftNode members
+
+        /// <inheritdoc />
+        public override void Execute
+            (
+                PftContext context
+            )
+        {
+            OnBeforeExecution(context);
+
+            base.Execute(context);
+
+            OnAfterExecution(context);
+        }
+
+        #endregion
+    }
+}
---
+++ Source/Classic/Libs/ManagedIrbis/Source/Pft/Infrastructure/Ast/PftRef.cs
@@ -0,0 +1,86 @@
+﻿/* PftRef.cs --
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CodeJam;
+
+using JetBrains.Annotations;
+
+using MoonSharp.Interpreter;
+
+#endregion
+
+namespace ManagedIrbis.Pft.Infrastructure.Ast
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [PublicAPI]
+    [MoonSharpUserData]
+    public sealed class PftRef
+        : PftNode
+    {
+        #region Properties
+
+        #endregion
+
+        #region Construction
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public PftRef()
+        {
+        }
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public PftRef
+            (
+                [NotNull] PftToken token
+            )
+            : base(token)
+        {
+            Code.NotNull(token, "token");
+            token.MustBe(PftTokenKind.Ref);
+        }
+
+        #endregion
+
+        #region Private members
+
+        #endregion
+
+        #region Public methods
+
+        #endregion
+
+        #region PftNode members
+
+        /// <inheritdoc />
+        public override void Execute
+            (
+                PftContext context
+            )
+        {
+            OnBeforeExecution(context);
+
+            base.Execute(context);
+
+            OnAfterExecution(context);
+        }
+
+        #endregion
+    }
+}
