f2ef46c18cee03412255bc7ffb3d427465fc8a5b
Alexey Mironov <amironov73@gmail.com>
2016-10-24 10:35:50 +0900

    Add AliasManager class

---
+++ Source/Classic/Libs/ManagedIrbis/Source/Client/Alias.cs
@@ -0,0 +1,47 @@
+﻿/* Alias.cs --
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AM;
+using AM.IO;
+
+using CodeJam;
+
+using JetBrains.Annotations;
+
+using MoonSharp.Interpreter;
+
+#endregion
+
+namespace ManagedIrbis.Client
+{
+    sealed class Alias
+    {
+        #region Properties
+
+        /// <summary>
+        /// Name of the alias.
+        /// </summary>
+        [CanBeNull]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Value of the alias.
+        /// </summary>
+        [CanBeNull]
+        public string Value { get; set; }
+
+        #endregion
+    }
+}
