85f8487953f875f1449177f5bb5a8241810de46b
Alexey Mironov <amironov73@gmail.com>
2016-10-28 20:13:18 +0800

    Add ProgramCache class

---
+++ Source/Classic/Libs/ManagedIrbis/Source/Pft/Infrastructure/ProgramCache.cs
@@ -0,0 +1,120 @@
+﻿/* ProgramCache.cs --
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CodeJam;
+
+using JetBrains.Annotations;
+
+using MoonSharp.Interpreter;
+
+#endregion
+
+namespace ManagedIrbis.Pft.Infrastructure
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    static class ProgramCache
+    {
+        #region Properties
+
+        /// <summary>
+        /// Registry.
+        /// </summary>
+        [NotNull]
+        public static Dictionary<string, PftProgram> Registry
+        {
+            get;
+            private set;
+        }
+
+        #endregion
+
+        #region Construction
+
+        static ProgramCache()
+        {
+            Registry = new Dictionary<string, PftProgram>
+                (
+                    StringComparer.CurrentCultureIgnoreCase
+                );
+            _sync = new object();
+        }
+
+        #endregion
+
+        #region Private members
+
+        // ReSharper disable once InconsistentNaming
+        private static readonly object _sync;
+
+        #endregion
+
+        #region Public methods
+
+        /// <summary>
+        /// Add program for the sourceText.
+        /// </summary>
+        public static void AddProgram
+            (
+                [NotNull] string sourceText,
+                [NotNull] PftProgram program
+            )
+        {
+            Code.NotNull(sourceText, "sourceText");
+            Code.NotNull(program, "program");
+
+            lock (_sync)
+            {
+                Registry[sourceText] = program;
+            }
+        }
+
+        /// <summary>
+        /// Clear the cache.
+        /// </summary>
+        public static void Clear()
+        {
+            lock (_sync)
+            {
+                Registry.Clear();
+            }
+        }
+
+        /// <summary>
+        /// Get program for the text.
+        /// </summary>
+        [CanBeNull]
+        public static PftProgram GetProgram
+            (
+                [CanBeNull] string sourceText
+            )
+        {
+            if (string.IsNullOrEmpty(sourceText))
+            {
+                return null;
+            }
+
+            lock (_sync)
+            {
+                PftProgram result;
+                Registry.TryGetValue(sourceText, out result);
+
+                return result;
+            }
+        }
+
+        #endregion
+    }
+}
