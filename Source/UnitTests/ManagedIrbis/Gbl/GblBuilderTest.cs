ae85242f2042658483aced53d2f71c38cce9d187
Alexey Mironov <amironov73@gmail.com>
2016-10-31 15:46:11 +0900

    Add tests

---
+++ Source/UnitTests/ManagedIrbis/Gbl/GblBuilderTest.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using ManagedIrbis.Gbl;
+
+namespace UnitTests.ManagedIrbis.Gbl
+{
+    [TestClass]
+    public class GblBuilderTest
+    {
+        [TestMethod]
+        public void GblBuilder_Construction()
+        {
+            GblBuilder builder = new GblBuilder();
+            Assert.AreEqual(0, builder.ToStatements().Length);
+        }
+
+        [TestMethod]
+        public void GblBuilder_Add()
+        {
+            GblBuilder builder = new GblBuilder();
+            builder.Add("910", "^a0^b1");
+            GblStatement[] statements = builder.ToStatements();
+            Assert.AreEqual("ADD", statements[0].Command);
+            Assert.AreEqual("910", statements[0].Parameter1);
+            Assert.AreEqual("^a0^b1", statements[0].Format1);
+        }
+
+        [TestMethod]
+        public void GblBuilder_Change()
+        {
+            GblBuilder builder = new GblBuilder();
+            builder.Change("910", "^a0^b1", "^a9^b1");
+            GblStatement[] statements = builder.ToStatements();
+            Assert.AreEqual("CHA", statements[0].Command);
+            Assert.AreEqual("910", statements[0].Parameter1);
+            Assert.AreEqual("^a0^b1", statements[0].Format1);
+            Assert.AreEqual("^a9^b1", statements[0].Format2);
+        }
+
+        [TestMethod]
+        public void GblBuilder_Delete()
+        {
+            GblBuilder builder = new GblBuilder();
+            builder.Delete("910", "1");
+            GblStatement[] statements = builder.ToStatements();
+            Assert.AreEqual("DEL", statements[0].Command);
+            Assert.AreEqual("910", statements[0].Parameter1);
+            Assert.AreEqual("1", statements[0].Parameter2);
+        }
+
+        [TestMethod]
+        public void GblBuilder_DeleteRecord()
+        {
+            GblBuilder builder = new GblBuilder();
+            builder.DeleteRecord();
+            GblStatement[] statements = builder.ToStatements();
+            Assert.AreEqual("DELR", statements[0].Command);
+        }
+    }
+}
---
+++ Source/UnitTests/ManagedIrbis/Gbl/GblStatementTest.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.IO;
+using AM.Runtime;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using ManagedIrbis.Gbl;
+
+namespace UnitTests.ManagedIrbis.Gbl
+{
+    [TestClass]
+    public class GblStatementTest
+    {
+        [TestMethod]
+        public void GblStatement_Construction()
+        {
+            GblStatement statement = new GblStatement();
+            Assert.AreEqual(null, statement.Command);
+            Assert.AreEqual(null, statement.Parameter1);
+            Assert.AreEqual(null, statement.Parameter2);
+            Assert.AreEqual(null, statement.Format1);
+            Assert.AreEqual(null, statement.Format2);
+        }
+
+        [TestMethod]
+        public void GblStatement_EncodeProtocol()
+        {
+            GblStatement statement = new GblStatement
+            {
+                Command = "ADD",
+                Parameter1 = "910",
+                Parameter2 = "XXXXX",
+                Format1 = "^a0^b1",
+                Format2 = "XXXXX"
+            };
+            Assert.AreEqual
+                (
+                    "ADD" + GblStatement.Delimiter
+                    + "910" + GblStatement.Delimiter
+                    + "XXXXX" + GblStatement.Delimiter
+                    + "^a0^b1" + GblStatement.Delimiter
+                    + "XXXXX" + GblStatement.Delimiter,
+                    statement.EncodeForProtocol()
+                );
+        }
+
+        [TestMethod]
+        public void GblStatement_ParseStream()
+        {
+            const string text = "ADD\r\n910\r\nXXXXX\r\n^a0^b1\r\nXXXXX\r\n";
+            TextReader reader = new StringReader(text);
+            GblStatement statement = GblStatement.ParseStream(reader);
+            Assert.IsNotNull(statement);
+            Assert.AreEqual("ADD", statement.Command);
+            Assert.AreEqual("910", statement.Parameter1);
+            Assert.AreEqual("XXXXX", statement.Parameter2);
+            Assert.AreEqual("^a0^b1", statement.Format1);
+            Assert.AreEqual("XXXXX", statement.Format2);
+        }
+
+        private void _TestSerialize
+            (
+                GblStatement first
+            )
+        {
+            byte[] bytes = first.SaveToMemory();
+
+            GblStatement second = bytes
+                .RestoreObjectFromMemory<GblStatement>();
+
+            Assert.AreEqual(first.Command, second.Command);
+            Assert.AreEqual(first.Parameter1, second.Parameter1);
+            Assert.AreEqual(first.Parameter2, second.Parameter2);
+            Assert.AreEqual(first.Format1, second.Format1);
+            Assert.AreEqual(first.Format2, second.Format2);
+        }
+
+        [TestMethod]
+        public void GblStatement_Serialize()
+        {
+            GblStatement statement = new GblStatement();
+            _TestSerialize(statement);
+
+            statement = new GblStatement
+            {
+                Command = "ADD",
+                Parameter1 = "910",
+                Parameter2 = "XXXXX",
+                Format1 = "^a0^b1",
+                Format2 = "XXXXX"
+            };
+            _TestSerialize(statement);
+        }
+
+        [TestMethod]
+        public void GblStatement_Verify()
+        {
+            GblStatement statement = new GblStatement();
+            Assert.AreEqual(false, statement.Verify(false));
+
+            statement = new GblStatement
+            {
+                Command = "ADD",
+                Parameter1 = "910",
+                Parameter2 = "XXXXX",
+                Format1 = "^a0^b1",
+                Format2 = "XXXXX"
+            };
+            Assert.AreEqual(true, statement.Verify(false));
+        }
+
+        [TestMethod]
+        public void GblStatement_ToString()
+        {
+            GblStatement statement = new GblStatement
+            {
+                Command = "ADD",
+                Parameter1 = "910",
+                Parameter2 = "XXXXX",
+                Format1 = "^a0^b1",
+                Format2 = "XXXXX"
+            };
+            string expected = "Command: ADD," + Environment.NewLine
+                + "Parameter1: 910," + Environment.NewLine
+                + "Parameter2: XXXXX," + Environment.NewLine
+                + "Format1: ^a0^b1," + Environment.NewLine
+                + "Format2: XXXXX";
+            Assert.AreEqual
+                (
+                    expected,
+                    statement.ToString()
+                );
+        }
+    }
+}
