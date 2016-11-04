9553e0de92fe78e34908b431ddb76e764eb77a95
Alexey Mironov <amironov73@gmail.com>
2016-11-04 20:35:43 +0800

    Add MethodRunner class

---
+++ Source/Classic/Libs/ManagedIrbis/Source/PlatformSpecific/MethodResult.cs
@@ -0,0 +1,69 @@
+﻿/* MethodResult.cs --
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#if CLASSIC || DESKTOP
+
+using System.Diagnostics;
+
+#endif
+
+using AM;
+
+using CodeJam;
+
+using JetBrains.Annotations;
+
+using MoonSharp.Interpreter;
+
+#endregion
+
+namespace ManagedIrbis.PlatformSpecific
+{
+    /// <summary>
+    /// Result of external method call.
+    /// </summary>
+    sealed class MethodResult
+    {
+        #region Properties
+
+        /// <summary>
+        /// Return code.
+        /// </summary>
+        public int ReturnCode { get; set; }
+
+        /// <summary>
+        /// Input.
+        /// </summary>
+        public string Input { get; set; }
+
+        /// <summary>
+        /// Output.
+        /// </summary>
+        public string Output { get; set; }
+
+        #endregion
+
+        #region Construction
+
+        #endregion
+
+        #region Private members
+
+        #endregion
+
+        #region Public methods
+
+        #endregion
+    }
+}
