f95afaf59245af678ddc95b9027502719d46e609
Alexey Mironov <amironov73@gmail.com>
2016-11-04 20:43:25 +0800

    Add RawRecord class

---
+++ Source/Classic/Libs/ManagedIrbis/Source/RawRecord.cs
@@ -0,0 +1,70 @@
+﻿/* RawRecord.cs --
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CodeJam;
+
+using JetBrains.Annotations;
+
+using MoonSharp.Interpreter;
+
+#endregion
+
+namespace ManagedIrbis
+{
+    /// <summary>
+    /// Raw (not decoded) record.
+    /// </summary>
+    [PublicAPI]
+    [MoonSharpUserData]
+    public sealed class RawRecord
+    {
+        #region Properties
+
+        /// <summary>
+        /// Database name.
+        /// </summary>
+        [CanBeNull]
+        public string Database { get; set; }
+
+        /// <summary>
+        /// MFN.
+        /// </summary>
+        public int Mfn { get; set; }
+
+        /// <summary>
+        /// Status.
+        /// </summary>
+        public RecordStatus Status { get; set; }
+
+        /// <summary>
+        /// Lines of text.
+        /// </summary>
+        [CanBeNull]
+        public string[] Lines { get; set; }
+
+        #endregion
+
+        #region Construction
+
+        #endregion
+
+        #region Private members
+
+        #endregion
+
+        #region Public methods
+
+        #endregion
+    }
+}
