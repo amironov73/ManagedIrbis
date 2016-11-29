7599cd472f469c1457bc38dd8482faf019d90849
Alexey Mironov <amironov73@gmail.com>
2016-11-29 17:16:31 +0900

    Add SiberianDateCell class

---
+++ Source/Classic/Libs/IrbisUI/Source/Grid/SiberianDateCell.cs
@@ -0,0 +1,176 @@
+﻿/* SiberianDateCell.cs -- 
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+using AM;
+using AM.Collections;
+using AM.IO;
+using AM.Runtime;
+
+using CodeJam;
+
+using JetBrains.Annotations;
+
+using MoonSharp.Interpreter;
+
+#endregion
+
+namespace IrbisUI.Grid
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [PublicAPI]
+    [MoonSharpUserData]
+    public class SiberianDateCell
+        : SiberianCell
+    {
+        #region Properties
+
+        /// <summary>
+        /// Date.
+        /// </summary>
+        public DateTime Date
+        {
+            get { return _date; }
+            set { _SetDate(value); }
+        }
+
+        /// <summary>
+        /// Text.
+        /// </summary>
+        [CanBeNull]
+        public string Text
+        {
+            get { return _text; }
+            set { _SetText(value); }
+        }
+
+        #endregion
+
+        #region Construction
+
+        #endregion
+
+        #region Private members
+
+        private DateTime _date;
+
+        private string _text;
+
+        private void _SetDate
+            (
+                DateTime date
+            )
+        {
+            _date = date;
+            Column.PutData(Row.Data, this);
+            Grid.Invalidate();
+        }
+
+        private void _SetText
+            (
+                string text
+            )
+        {
+            _text = text;
+            Grid.Invalidate();
+        }
+
+        #endregion
+
+        #region Public methods
+
+        #endregion
+
+        #region SiberianCell members
+
+        /// <inheritdoc />
+        public override void CloseEditor
+            (
+                bool accept
+            )
+        {
+            if (!ReferenceEquals(Grid.Editor, null))
+            {
+                if (accept)
+                {
+                    // State = ....
+                }
+            }
+
+            base.CloseEditor(accept);
+        }
+
+        /// <inheritdoc/>
+        public override void Paint
+            (
+                PaintEventArgs args
+            )
+        {
+            Graphics graphics = args.Graphics;
+            Rectangle rectangle = args.ClipRectangle;
+            Rectangle textRectangle = new Rectangle
+                (
+                    rectangle.Left + 20,
+                    rectangle.Y,
+                    rectangle.Width - 20,
+                    rectangle.Height
+                );
+
+            Color foreColor = Color.Black;
+            if (ReferenceEquals(Row, Grid.CurrentRow))
+            {
+                foreColor = Color.White;
+            }
+
+            if (ReferenceEquals(this, Grid.CurrentCell))
+            {
+                Color backColor = Color.Blue;
+                using (Brush brush = new SolidBrush(backColor))
+                {
+                    graphics.FillRectangle(brush, rectangle);
+                }
+            }
+
+            TextFormatFlags flags
+                = TextFormatFlags.TextBoxControl
+                | TextFormatFlags.EndEllipsis
+                | TextFormatFlags.NoPrefix
+                | TextFormatFlags.VerticalCenter;
+
+            TextRenderer.DrawText
+                (
+                    graphics,
+                    Text,
+                    Grid.Font,
+                    textRectangle,
+                    foreColor,
+                    flags
+                );
+
+        }
+
+        #endregion
+
+        #region Object members
+
+        #endregion
+
+    }
+}
---
+++ Source/Classic/Libs/IrbisUI/Source/Grid/SiberianDateColumn.cs
@@ -0,0 +1,149 @@
+﻿/* SiberianDateColumn.cs -- 
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using AM;
+using AM.Collections;
+using AM.IO;
+using AM.Reflection;
+using AM.Runtime;
+
+using CodeJam;
+
+using JetBrains.Annotations;
+
+using MoonSharp.Interpreter;
+
+#endregion
+
+namespace IrbisUI.Grid
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [PublicAPI]
+    [MoonSharpUserData]
+    public class SiberianDateColumn
+        : SiberianColumn
+    {
+        #region Properties
+
+        #endregion
+
+        #region Construction
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public SiberianDateColumn()
+        {
+            //BackColor = Color.White;
+        }
+
+        #endregion
+
+        #region Private members
+
+        #endregion
+
+        #region Public methods
+
+        #endregion
+
+        #region SiberianColumn members
+
+        /// <inheritdoc/>
+        public override SiberianCell CreateCell()
+        {
+            SiberianCell result = new SiberianDateCell();
+            result.Column = this;
+
+            return result;
+        }
+
+        /// <inheritdoc />
+        public override Control CreateEditor
+            (
+                SiberianCell cell,
+                bool edit,
+                object state
+            )
+        {
+            Code.NotNull(cell, "cell");
+
+            return null;
+        }
+
+        /// <inheritdoc />
+        public override void GetData
+            (
+                object theObject,
+                SiberianCell cell
+            )
+        {
+            SiberianDateCell dateCell = (SiberianDateCell)cell;
+
+            if (!string.IsNullOrEmpty(Member)
+                && !ReferenceEquals(theObject, null))
+            {
+                Type type = theObject.GetType();
+                MemberInfo memberInfo = type.GetMember(Member)
+                    .First();
+                PropertyOrField property = new PropertyOrField
+                    (
+                        memberInfo
+                    );
+
+                object value = property.GetValue(theObject);
+                dateCell.Date = ReferenceEquals(value, null)
+                    ? DateTime.MinValue
+                    : (DateTime)value;
+            }
+        }
+
+        /// <inheritdoc />
+        public override void PutData
+            (
+                object theObject,
+                SiberianCell cell
+            )
+        {
+            SiberianDateCell dateCell = (SiberianDateCell)cell;
+
+            if (!string.IsNullOrEmpty(Member)
+                && !ReferenceEquals(theObject, null))
+            {
+                Type type = theObject.GetType();
+                MemberInfo memberInfo = type.GetMember(Member)
+                    .First();
+                PropertyOrField property = new PropertyOrField
+                    (
+                        memberInfo
+                    );
+
+                property.SetValue(theObject, dateCell.Date);
+            }
+        }
+
+        #endregion
+
+        #region Object members
+
+        #endregion
+    }
+}
