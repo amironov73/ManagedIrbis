09b5416b89106a0d27437e2c2a597e5da89890f3
Alexey Mironov <amironov73@gmail.com>
2016-12-07 09:51:44 +0900

    Add ServerContext class

---
+++ Source/Classic/Libs/ManagedIrbis/Source/Server/Commands/ServerCommand.cs
@@ -0,0 +1,93 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+/* ServerCommand.cs -- 
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AM;
+using AM.Collections;
+using AM.IO;
+using AM.Runtime;
+
+using CodeJam;
+
+using JetBrains.Annotations;
+
+using ManagedIrbis.Infrastructure;
+
+using MoonSharp.Interpreter;
+
+#endregion
+
+namespace ManagedIrbis.Server.Commands
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [PublicAPI]
+    [MoonSharpUserData]
+    public abstract class ServerCommand
+    {
+        #region Properties
+
+        /// <summary>
+        /// Context.
+        /// </summary>
+        public ServerContext Context { get; set; }
+
+        #endregion
+
+        #region Construction
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        protected ServerCommand
+            (
+                [NotNull] ServerContext context
+            )
+        {
+            Code.NotNull(context, "context");
+
+            Context = context;
+        }
+
+        #endregion
+
+        #region Private members
+
+        #endregion
+
+        #region Public methods
+
+        /// <summary>
+        /// Execute the command.
+        /// </summary>
+        public virtual void Execute
+            (
+                [NotNull] ClientQuery query
+            )
+        {
+            // Nothing to do here?
+        }
+
+        #endregion
+
+        #region Object members
+
+        #endregion
+    }
+}
