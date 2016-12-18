1907be5486751b4811e6c8c79c58c41ee3578225
Alexey Mironov <amironov73@gmail.com>
2016-12-19 08:55:17 +0900

    Add AffixEntry, AffixRule and AffixUtility classes

---
+++ Source/Classic/Libs/AM.AOT/AM/AOT/Spelling/AffixEntry.cs
@@ -0,0 +1,95 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+/* AffixEntry.cs -- 
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AM;
+using AM.Collections;
+using AM.IO;
+using AM.Runtime;
+
+using CodeJam;
+
+using JetBrains.Annotations;
+
+using MoonSharp.Interpreter;
+
+#endregion
+
+namespace AM.AOT.Spelling
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [PublicAPI]
+    [MoonSharpUserData]
+    public class AffixEntry
+    {
+        #region Properties
+
+        /// <summary>
+        /// The characters to add to the string.
+        /// </summary>
+        [CanBeNull]
+        public string AddCharacters { get; set; }
+
+        /// <summary>
+        /// The condition to be met in order to add characters.
+        /// </summary>
+        public int[] Conditions { get; set; }
+
+        /// <summary>
+        /// The characters to remove before adding characters.
+        /// </summary>
+        [CanBeNull]
+        public string StripCharacters { get; set; }
+
+        /// <summary>
+        /// The number of conditions that must be met
+        /// </summary>
+        public int ConditionCount { get; set; }
+
+        #endregion
+
+        #region Construction
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public AffixEntry()
+        {
+            AddCharacters = string.Empty;
+            Conditions = new int[256];
+            StripCharacters = string.Empty;
+            ConditionCount = 0;
+        }
+
+        #endregion
+
+        #region Private members
+
+        #endregion
+
+        #region Public methods
+
+        #endregion
+
+        #region Object members
+
+        #endregion
+    }
+}
---
+++ Source/Classic/Libs/AM.AOT/AM/AOT/Spelling/AffixRule.cs
@@ -0,0 +1,87 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+/* AffixRule.cs -- 
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AM;
+using AM.Collections;
+using AM.IO;
+using AM.Runtime;
+
+using CodeJam;
+
+using JetBrains.Annotations;
+
+using MoonSharp.Interpreter;
+
+#endregion
+
+namespace AM.AOT.Spelling
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [PublicAPI]
+    [MoonSharpUserData]
+    public class AffixRule
+    {
+        #region Properties
+
+        /// <summary>
+        /// Allow combining prefix and suffix.
+        /// </summary>
+        public bool AllowCombine { get; set; }
+
+        /// <summary>
+        /// Collection of text entries that make up this rule.
+        /// </summary>
+        public List<AffixEntry> AffixEntries { get; set; }
+
+        /// <summary>
+        /// Name of the Affix rule
+        /// </summary>
+        public string Name { get; set; }
+
+        #endregion
+
+        #region Construction
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public AffixRule()
+        {
+            AllowCombine = false;
+            AffixEntries = new List<AffixEntry>();
+            Name = string.Empty;
+        }
+
+        #endregion
+
+        #region Private members
+
+        #endregion
+
+        #region Public methods
+
+        #endregion
+
+        #region Object members
+
+        #endregion
+    }
+}
