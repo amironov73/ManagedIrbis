c01d6863d297c1c0d4c133f9f298f5d24692e66a
Alexey Mironov <amironov73@gmail.com>
2017-01-09 10:56:25 +0900

    Add ColorUtility.ToColor method

--- Source/Classic/Libs/AM.Drawing/AM/Drawing/ColorUtility.cs
+++ Source/Classic/Libs/AM.Drawing/AM/Drawing/ColorUtility.cs
@@ -183,14 +183,153 @@ namespace AM.Drawing
         /// <summary>
         /// Convert color to Blue-Green-Red representation.
         /// </summary>
-        /// <param name="color">Color value.</param>
-        /// <returns></returns>
         public static int ToBgr
             (
                 Color color
             )
         {
             return unchecked((((color.B << 8) + color.G) << 8) + color.R);
+        }
+
+        /// <summary>
+        /// Returns a color based on XAML color string.
+        /// </summary>
+        /// <param name="colorString">The color string.
+        /// Any format used in XAML should work.</param>
+        /// <returns>Parsed color</returns>
+        /// <remarks>
+        /// Borrowed from UWPCommunityToolkit:
+        /// https://github.com/Microsoft/UWPCommunityToolkit/blob/dev/Microsoft.Toolkit.Uwp/Helpers/ColorHelper.cs
+        /// </remarks>
+        public static Color ToColor
+            (
+                [NotNull] this string colorString
+            )
+        {
+            Code.NotNullNorEmpty(colorString, "colorString");
+
+            if (colorString[0] == '#')
+            {
+                uint temp;
+                byte a, r, g, b;
+
+                switch (colorString.Length)
+                {
+                    case 9:
+                        temp = Convert.ToUInt32(colorString.Substring(1), 16);
+                        a = (byte)(temp >> 24);
+                        r = (byte)((temp >> 16) & 0xff);
+                        g = (byte)((temp >> 8) & 0xff);
+                        b = (byte)(temp & 0xff);
+
+                        return Color.FromArgb(a, r, g, b);
+
+                    case 7:
+                        temp = Convert.ToUInt32(colorString.Substring(1), 16);
+                        r = (byte)((temp >> 16) & 0xff);
+                        g = (byte)((temp >> 8) & 0xff);
+                        b = (byte)(temp & 0xff);
+
+                        return Color.FromArgb(255, r, g, b);
+
+                    case 5:
+                        temp = Convert.ToUInt16(colorString.Substring(1), 16);
+                        a = (byte)(temp >> 12);
+                        r = (byte)((temp >> 8) & 0xf);
+                        g = (byte)((temp >> 4) & 0xf);
+                        b = (byte)(temp & 0xf);
+                        a = (byte)(a << 4 | a);
+                        r = (byte)(r << 4 | r);
+                        g = (byte)(g << 4 | g);
+                        b = (byte)(b << 4 | b);
+
+                        return Color.FromArgb(a, r, g, b);
+
+                    case 4:
+                        temp = Convert.ToUInt16(colorString.Substring(1), 16);
+                        r = (byte)((temp >> 8) & 0xf);
+                        g = (byte)((temp >> 4) & 0xf);
+                        b = (byte)(temp & 0xf);
+                        r = (byte)(r << 4 | r);
+                        g = (byte)(g << 4 | g);
+                        b = (byte)(b << 4 | b);
+
+                        return Color.FromArgb(255, r, g, b);
+
+                    default:
+                        throw new FormatException
+                            (
+                                string.Format
+                                (
+                                    "The {0} string passed in the colorString "
+                                    + "argument is not a recognized Color format.",
+                                    colorString
+                                )
+                            );
+                }
+            }
+
+            if (
+                    colorString.Length > 3
+                    && colorString[0] == 's'
+                    && colorString[1] == 'c'
+                    && colorString[2] == '#'
+                )
+            {
+                string[] values = colorString.Split(',');
+
+                if (values.Length == 4)
+                {
+                    double scA = double.Parse(values[0].Substring(3));
+                    double scR = double.Parse(values[1]);
+                    double scG = double.Parse(values[2]);
+                    double scB = double.Parse(values[3]);
+
+                    return Color.FromArgb
+                        (
+                            (byte)(scA * 255),
+                            (byte)(scR * 255),
+                            (byte)(scG * 255),
+                            (byte)(scB * 255)
+                        );
+                }
+
+                if (values.Length == 3)
+                {
+                    double scR = double.Parse(values[0].Substring(3));
+                    double scG = double.Parse(values[1]);
+                    double scB = double.Parse(values[2]);
+
+                    return Color.FromArgb
+                        (
+                            255,
+                            (byte)(scR * 255),
+                            (byte)(scG * 255),
+                            (byte)(scB * 255)
+                        );
+                }
+
+                throw new FormatException
+                    (
+                        string.Format
+                        (
+                            "The {0} string passed in the colorString "
+                            + "argument is not a recognized Color format "
+                            + "(sc#[scA,]scR,scG,scB).", 
+                            colorString
+                        )
+                    );
+            }
+
+            throw new FormatException
+                (
+                    string.Format
+                    (
+                        "The {0} string passed in the colorString argument "
+                        + "is not a recognized Color.", 
+                        colorString
+                    )
+                );
         }
 
         #endregion
