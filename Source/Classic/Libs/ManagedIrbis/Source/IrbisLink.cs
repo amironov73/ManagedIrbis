9af5be5a3a1cf1b407f5ec6de4a2bc0e2b3f890a
Alexey Mironov <amironov73@gmail.com>
2017-02-09 09:30:24 +0900

    Add tests for IrbisLink class

--- Source/Classic/Libs/ManagedIrbis/Source/IrbisLink.cs
+++ Source/Classic/Libs/ManagedIrbis/Source/IrbisLink.cs
@@ -21,6 +21,7 @@ using AM;
 using AM.Collections;
 using AM.IO;
 using AM.Runtime;
+using AM.Text;
 
 using CodeJam;
 
@@ -179,6 +180,46 @@ namespace ManagedIrbis
     // FREEPARnn
     //
 
+    //
+    // Examples:
+    // IRBIS:1,,IBIS,FULLW0_WN,@6
+    // IRBIS:?C21COM=1&I21DBN=IBIS&PFTNAME=FULLW0_WN&KEY=@6
+    // IRBIS:1,,IBIS,,@6?PFTNAME=FULLW0_WN
+    //
+    //
+    //
+    //
+    // '<img src="IRBIS:!!',v910^h,'!!" align="top" height="40px">'
+    // '<A HREF="IRBIS:1,,,bibl390_H_Full?&KEY=@',mfn,'">'
+    // '<img src="IRBIS:10,,textfolder.gif">'
+    // '<A HREF=IRBIS:?C21COM=1&KEY=@',mfn,'&I21DBN=',&uf('+D'),'&PFTNAME=fullw0_wn> Полностью...</A>'
+    // '<A HREF="IRBIS:?C21COM=1&I21DBN=',&uf('+D'),'&KEY=@',mfn,'&PFTNAME=rqst_IC_wn&FREEPAR3=',g1103,'&FREEPAR0=',g1100,'&FREEPAR5=',g1105,'">Заказать...</A>',
+    // '<b>','<IRBIS type=0>. ','</b>'
+    // '<A HREF="IRBIS:2,,,I='v430^d'">',v430^a,'</A>'
+    // '<a href="IRBIS:5,,,MNU_WN?FREEPAR0=',v1100,'">Отобранные</a>'
+    // '<IMG SRC="IRBIS:12,',&uf('+D'),',',mfn,',1">'
+    // '<A HREF="IRBIS:3,12,,',mfn,',','1','">','<IMG  style="width:105 px" SRC="IRBIS:12,,',mfn,',','1','"></A>'
+    // (if p(v951)then if v951^h:'02a'and v951^a<>''then '<A HREF="IRBIS:3?PATH=11&FILENAME=',v951^a,'">','<IMG  style="width:150 px" SRC="IRBIS:11,,',v951^a,'"></A>'BREAK,fi,fi)
+    //
+
+    //
+    // WEB FORM:
+    //
+    // '<form name="CONTINUE1" action="IRBIS:" enctype="Multipart/form-data" method="GET">',
+    // '<input id="anchor" name="T21TRM" value="',&uf('AV1004^A#1')'" onkeydown="if (event.keyCode==13) {CONTINUE1.submit();}">',
+    // ' <input type="submit" name="" value="Продолжить" id="anchor" >',
+    // '<IMG SRC="IRBIS:10,,arrow_up.jpg">',
+    // '<input type="hidden" name="T21NEXT" value="0">',
+    // '<input type="hidden" name="C21COM" value="4">',
+    // '<input type="hidden" name="I21DBN" value="',v1000,'">',
+    // '<input type="hidden" name="PFTNAME" value="',v1012,'">',
+    // '<input type="hidden" name="T21PRF" value="',v1011,'">',
+    // '<input type="hidden" name="T21CNR" value="',v1006,'">',
+    // '<input type="hidden" name="FREEPAR0" value="',v1100,'">',
+    // '<input type="hidden" name="FREEPAR1" value="',v1101,'">',
+    // '</form>'
+    //
+
     /// <summary>
     /// 
     /// </summary>
@@ -188,21 +229,231 @@ namespace ManagedIrbis
     {
         #region Properties
 
+        /// <summary>
+        /// Command (C21COM).
+        /// </summary>
+        /// <remarks>
+        /// E. g.: "3".
+        /// </remarks>
+        [CanBeNull]
+        public string Command { get; set; }
+
+        /// <summary>
+        /// Path (PATH).
+        /// </summary>
+        /// <remarks>
+        /// E. g.: "10".
+        /// </remarks>
+        [CanBeNull]
+        public string Path { get; set; }
+
+        /// <summary>
+        /// Database name (I21DBN).
+        /// </summary>
+        /// <remarks>
+        /// E. g.: "IBIS".
+        /// </remarks>
+        [CanBeNull]
+        public string Database { get; set; }
+
+        /// <summary>
+        /// PFT file name (PFTNAME).
+        /// </summary>
+        [CanBeNull]
+        public string Format { get; set; }
+
+        /// <summary>
+        /// File name (FILENAME).
+        /// </summary>
+        [CanBeNull]
+        public string FileName { get; set; }
+
+        /// <summary>
+        /// Search key (KEY).
+        /// </summary>
+        [CanBeNull]
+        public string Key { get; set; }
+
+        /// <summary>
+        /// Parameters
+        /// </summary>
+        [NotNull]
+        public Dictionary<string, string> Parameters { get; private set; }
+
         #endregion
 
         #region Construction
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public IrbisLink()
+        {
+            Parameters = new Dictionary<string, string>();
+        }
 
         #endregion
 
         #region Private members
 
+        private static string _GetPositional
+            (
+                TextNavigator navigator
+            )
+        {
+            string result = navigator.ReadUntil(',', '?');
+            if (navigator.PeekChar() == ',')
+            {
+                navigator.ReadChar();
+            }
+
+            return result;
+        }
+
+        private static bool _GetNamed
+            (
+                TextNavigator navigator,
+                Dictionary<string, string> dictionary
+            )
+        {
+            string key = navigator.ReadUntil('=');
+            if (string.IsNullOrEmpty(key)
+                || navigator.ReadChar() != '=')
+            {
+                return false;
+            }
+
+            string value = navigator.ReadUntil('&');
+            dictionary.Add(key, value);
+
+            return navigator.ReadChar() == '&';
+        }
+
+        private static string _SetPositional
+            (
+                Dictionary<string,string> dictionary,
+                string key,
+                string value
+            )
+        {
+            if (dictionary.ContainsKey(key))
+            {
+                value = dictionary[key];
+            }
+
+            return value;
+        }
+
         #endregion
 
         #region Public methods
 
+        /// <summary>
+        /// Parse text for IRBIS-link.
+        /// </summary>
+        [NotNull]
+        public static IrbisLink Parse
+            (
+                [NotNull] string text
+            )
+        {
+            Code.NotNullNorEmpty(text, "text");
+
+            IrbisLink result = new IrbisLink();
+            TextNavigator navigator = new TextNavigator(text);
+            string prefix = navigator.PeekString(6);
+            if (prefix.SameString("IRBIS:"))
+            {
+                navigator.ReadString(6);
+            }
+
+            result.Command = _GetPositional(navigator);
+            if (result.Command.SameString("3"))
+            {
+                result.Path = _GetPositional(navigator);
+                result.Database = _GetPositional(navigator);
+                result.FileName = _GetPositional(navigator);
+            }
+            else
+            {
+                _GetPositional(navigator);
+                result.Database = _GetPositional(navigator);
+                result.Format = _GetPositional(navigator);
+                result.Key = _GetPositional(navigator);
+            }
+
+            if (navigator.ReadChar() == '?')
+            {
+                while (_GetNamed(navigator, result.Parameters))
+                {
+                    // Do nothing
+                }
+
+                result.Command = _SetPositional(result.Parameters, "C21COM", result.Command);
+                result.Database = _SetPositional(result.Parameters, "I21DBN", result.Database);
+                result.Format = _SetPositional(result.Parameters, "PFTNAME", result.Format);
+                result.Key = _SetPositional(result.Parameters, "KEY", result.Key);
+                result.Path = _SetPositional(result.Parameters, "PATH", result.Path);
+                result.FileName = _SetPositional(result.Parameters, "FILENAME", result.FileName);
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Parse text for IRBIS-link.
+        /// </summary>
+        [NotNull]
+        public static IrbisLink ParseForImage
+            (
+                [NotNull] string text
+            )
+        {
+            Code.NotNullNorEmpty(text, "text");
+
+            IrbisLink result = new IrbisLink();
+            TextNavigator navigator = new TextNavigator(text);
+            string prefix = navigator.PeekString(6);
+            if (prefix.SameString("IRBIS:"))
+            {
+                navigator.ReadString(6);
+            }
+
+            result.Command = "3";
+            result.Path = _GetPositional(navigator);
+            result.Database = _GetPositional(navigator).EmptyToNull();
+            result.FileName = _GetPositional(navigator);
+
+            if (navigator.ReadChar() == '?')
+            {
+                while (_GetNamed(navigator, result.Parameters))
+                {
+                    // Do nothing
+                }
+
+                result.Database = _SetPositional(result.Parameters, "I21DBN", result.Database);
+                result.Format = _SetPositional(result.Parameters, "PFTNAME", result.Format);
+                result.Key = _SetPositional(result.Parameters, "KEY", result.Key);
+                result.Path = _SetPositional(result.Parameters, "PATH", result.Path);
+                result.FileName = _SetPositional(result.Parameters, "FILENAME", result.FileName);
+            }
+
+            return result;
+        }
+
         #endregion
 
         #region Object members
+
+        /// <inheritdoc/>
+        public override string ToString()
+        {
+            return string.Format
+                (
+                    "Command: {0}",
+                    Command.ToVisibleString()
+                );
+        }
 
         #endregion
     }
---
+++ Source/UnitTests/ManagedIrbis/IrbisLinkTest.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using ManagedIrbis;
+
+namespace UnitTests.ManagedIrbis
+{
+    [TestClass]
+    public class IrbisLinkTest
+    {
+        [TestMethod]
+        public void IrbisLink_Constructor()
+        {
+            IrbisLink link = new IrbisLink();
+
+            Assert.IsNull(link.Command);
+            Assert.IsNull(link.Database);
+            Assert.IsNull(link.FileName);
+            Assert.IsNull(link.Format);
+            Assert.IsNull(link.Key);
+            Assert.IsNull(link.Path);
+            Assert.IsNotNull(link.Parameters);
+            Assert.AreEqual(0,link.Parameters.Count);
+        }
+
+        [TestMethod]
+        public void IrbisLink_Parse1()
+        {
+            IrbisLink link = IrbisLink.Parse("IRBIS:1,,IBIS,FULLW0_WN,@6");
+            Assert.AreEqual("1", link.Command);
+            Assert.AreEqual("IBIS", link.Database);
+            Assert.AreEqual("FULLW0_WN", link.Format);
+            Assert.AreEqual("@6", link.Key);
+        }
+
+        [TestMethod]
+        public void IrbisLink_Parse2()
+        {
+            IrbisLink link = IrbisLink.Parse("IRBIS:?C21COM=1&I21DBN=IBIS&PFTNAME=FULLW0_WN&KEY=@6");
+            Assert.AreEqual("1", link.Command);
+            Assert.AreEqual("IBIS", link.Database);
+            Assert.AreEqual("FULLW0_WN", link.Format);
+            Assert.AreEqual("@6", link.Key);
+        }
+
+        [TestMethod]
+        public void IrbisLink_Parse3()
+        {
+            IrbisLink link = IrbisLink.Parse("IRBIS:1,,IBIS,,@6?PFTNAME=FULLW0_WN");
+            Assert.AreEqual("1", link.Command);
+            Assert.AreEqual("IBIS", link.Database);
+            Assert.AreEqual("FULLW0_WN", link.Format);
+            Assert.AreEqual("@6", link.Key);
+        }
+
+        [TestMethod]
+        public void IrbisLink_ParseForImage1()
+        {
+            IrbisLink link = IrbisLink.ParseForImage("IRBIS:10,,textfolder.gif");
+            Assert.AreEqual("3", link.Command);
+            Assert.AreEqual("10", link.Path);
+            Assert.IsNull(link.Database);
+            Assert.AreEqual("textfolder.gif", link.FileName);
+        }
+    }
+}
