9f085cbc1514625a6b35960a1801c43cd172e042
Alexey Mironov <amironov73@gmail.com>
2017-02-12 13:52:46 +0800

    Add OsmiRegistration project

---
+++ Source/Classic/Apps/OsmiRegistration/Source/MainForm.cs
@@ -0,0 +1,37 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+/* MainForm.cs --
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+#endregion
+
+namespace OsmiRegistration
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public partial class MainForm 
+        : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
