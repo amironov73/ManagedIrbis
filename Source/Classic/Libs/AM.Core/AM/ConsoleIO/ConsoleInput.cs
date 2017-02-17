18911f6bb7f3c56b03d85e380ca41ab8468d685c
Alexey Mironov <amironov73@gmail.com>
2017-02-17 19:45:47 +0800

    Start ConsoleIO

---
+++ Source/Classic/Libs/AM.Core/AM/ConsoleIO/ConsoleInput.cs
@@ -0,0 +1,62 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+/* ConsoleInput.cs --
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CodeJam;
+
+using JetBrains.Annotations;
+
+using MoonSharp.Interpreter;
+
+#endregion
+
+namespace AM.ConsoleIO
+{
+    /// <summary>
+    /// Console input.
+    /// </summary>
+    [PublicAPI]
+    [MoonSharpUserData]
+    public sealed class ConsoleInput
+    {
+        #region Properties
+
+        #endregion
+
+        #region Construction
+
+        #endregion
+
+        #region Private members
+
+        #endregion
+
+        #region Public methods
+
+        /// <summary>
+        /// Read line.
+        /// </summary>
+        /// <returns><c>null</c> if nothing entered.
+        /// </returns>
+        [CanBeNull]
+        public static string ReadLine()
+        {
+            return null;
+        }
+
+        #endregion
+    }
+}
---
+++ Source/Classic/Libs/AM.Core/AM/ConsoleIO/InputHistory.cs
@@ -0,0 +1,51 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+/* InputHistory.cs --
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CodeJam;
+
+using JetBrains.Annotations;
+
+using MoonSharp.Interpreter;
+
+#endregion
+
+namespace AM.ConsoleIO
+{
+    /// <summary>
+    /// Input history.
+    /// </summary>
+    [PublicAPI]
+    [MoonSharpUserData]
+    public static class InputHistory
+    {
+        #region Properties
+
+        #endregion
+
+        #region Construction
+
+        #endregion
+
+        #region Private members
+
+        #endregion
+
+        #region Public methods
+
+        #endregion
+    }
+}
