fb3bd4c5e59de2ea52d3c56b203627ecfd92ecc3
Alexey Mironov <amironov73@gmail.com>
2017-02-20 14:57:41 +0900

    Add Run32bit project

---
+++ Source/Classic/Apps/Run32bit/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace Run32bit
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static int Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                MessageBox.Show("Usage: Run32bit <program.exe>");
+                return 1;
+            }
+
+            try
+            {
+                string fileName = Path.GetFullPath(args[0]);
+                Assembly assembly = Assembly.LoadFile(fileName);
+                MethodInfo entryPoint = assembly.EntryPoint;
+                ParameterInfo[] parameters = entryPoint.GetParameters();
+
+                if (parameters.Length == 0)
+                {
+                    entryPoint.Invoke(null, null);
+                }
+                else
+                {
+                    string[] args2 = args.Skip(1).ToArray();
+
+                    entryPoint.Invoke(null, new object[] {args2});
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                return 1;
+            }
+
+            return 0;
+        }
+    }
+}
