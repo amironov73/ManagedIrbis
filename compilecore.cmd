cff0bdda240c8ce8ec561192447982720ea05984
Alexey Mironov <amironov73@gmail.com>
2017-03-27 13:04:46 +0800

    Migrate .NET Core to MSBUILD (CSPROJ)

--- compilecore.cmd
+++ compilecore.cmd
@@ -1,17 +1,14 @@
 @echo off
 
+cd Source/NetCore
+
 SET MSBUILD=%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe
-SET OUTPUT=..\..\..\Binaries
+SET OUTPUT=../../../Binaries
 SET PARAMS=/consoleloggerparameters:ErrorsOnly /m
 
-dotnet restore Source\NetCore\AM.Core\project.json
-dotnet restore Source\NetCore\ManagedIrbis\project.json
-dotnet restore Source\NetCore\RestfulIrbis\project.json
+dotnet restore
 
-"%MSBUILD%" Source\NetCore\AM.Core\AM.Core.xproj           /target:ReBuild /property:Configuration=Debug   /property:OutputPath=%OUTPUT%\DebugCore /p:DefineConstants="DEBUG;NETCORE" %PARAMS%
-"%MSBUILD%" Source\NetCore\ManagedIrbis\ManagedIrbis.xproj /target:ReBuild /property:Configuration=Debug   /property:OutputPath=%OUTPUT%\DebugCore /p:DefineConstants="DEBUG;NETCORE" %PARAMS%
-"%MSBUILD%" Source\NetCore\RestfulIrbis\RestfulIrbis.xproj /target:ReBuild /property:Configuration=Debug   /property:OutputPath=%OUTPUT%\DebugCore /p:DefineConstants="DEBUG;NETCORE" %PARAMS%
+dotnet msbuild /target:ReBuild /property:Configuration=Debug   /property:OutputPath=%OUTPUT%\DebugCore   %PARAMS%
+dotnet msbuild /target:ReBuild /property:Configuration=Release /property:OutputPath=%OUTPUT%\ReleaseCore %PARAMS%
 
-"%MSBUILD%" Source\NetCore\AM.Core\AM.Core.xproj           /target:ReBuild /property:Configuration=Release /property:OutputPath=%OUTPUT%\ReleaseCore /p:DefineConstants="NETCORE"     %PARAMS%
-"%MSBUILD%" Source\NetCore\ManagedIrbis\ManagedIrbis.xproj /target:ReBuild /property:Configuration=Release /property:OutputPath=%OUTPUT%\ReleaseCore /p:DefineConstants="NETCORE"     %PARAMS%
-"%MSBUILD%" Source\NetCore\RestfulIrbis\RestfulIrbis.xproj /target:ReBuild /property:Configuration=Release /property:OutputPath=%OUTPUT%\ReleaseCore /p:DefineConstants="NETCORE"     %PARAMS%
+cd ../..
