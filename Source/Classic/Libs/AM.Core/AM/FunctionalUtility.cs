c9bd56485534f02d6293e2f23ebc7e181a0b9f3f
Alexey Mironov <amironov73@gmail.com>
2017-04-04 08:52:08 +0800

    Add FunctionalUtility.cs

---
+++ Source/Classic/Libs/AM.Core/AM/FunctionalUtility.cs
@@ -0,0 +1,79 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+/* FunctionalUtility.cs --
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CodeJam;
+
+using JetBrains.Annotations;
+
+using MoonSharp.Interpreter;
+
+#endregion
+
+namespace AM
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [PublicAPI]
+    [MoonSharpUserData]
+    public static class FunctionalUtility
+    {
+        #region Public methods
+
+        /// <summary>
+        /// Ternary operator.
+        /// </summary>
+        [CanBeNull]
+        public static TResult Either<TInput, TResult>
+            (
+                [CanBeNull] this TInput input,
+                [NotNull] Func<TInput, bool> condition,
+                [NotNull] Func<TInput, TResult> trueAction,
+                [NotNull] Func<TInput, TResult> falseAction
+            )
+        {
+            Code.NotNull(condition, "condition");
+            Code.NotNull(trueAction, "trueAction");
+            Code.NotNull(falseAction, "falseAction");
+
+            TResult result = condition(input)
+                ? trueAction(input)
+                : falseAction(input);
+
+            return result;
+        }
+
+        /// <summary>
+        /// Pipe the data to the function.
+        /// </summary>
+        [CanBeNull]
+        public static TResult PipeTo<TInput, TResult>
+            (
+                [CanBeNull] this TInput input,
+                [NotNull] Func<TInput, TResult> func
+            )
+        {
+            Code.NotNull(func, "func");
+
+            TResult result = func(input);
+
+            return result;
+        }
+
+        #endregion
+    }
+}
---
+++ Source/UnitTests/AM/FunctionalUtilityTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using AM;
+
+namespace UnitTests.AM
+{
+    [TestClass]
+    public class FunctionalUtilityTest
+    {
+        [TestMethod]
+        public void FunctionalUtility_PipeTo_1()
+        {
+            const string expected = "100";
+            string actual = 10
+                .PipeTo(x => x * x)
+                .PipeTo(x => x.ToInvariantString());
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FunctionalUtility_Either_1()
+        {
+            const string expected = "Positive 10";
+            string actual = 10
+                .Either
+                (
+                    x => x > 0,
+                    x => "Positive " + x,
+                    x => "Negative " + x
+                );
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
