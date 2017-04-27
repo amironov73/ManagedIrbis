ce7f73e420437f5b34e510b97097fec624825cb7
Alexey Mironov <amironov73@gmail.com>
2017-04-27 16:01:43 +0800

    Add ServiceRepository class

---
+++ Source/Classic/Libs/AM.Core/AM/IOC/ServiceRepository.cs
@@ -0,0 +1,148 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+/* ServiceRepository.cs --
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using System;
+using System.Collections.Generic;
+
+using AM.Logging;
+
+using CodeJam;
+
+using JetBrains.Annotations;
+
+using MoonSharp.Interpreter;
+
+#endregion
+
+namespace AM.IOC
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [PublicAPI]
+    [MoonSharpUserData]
+    public sealed class ServiceRepository
+        : IDisposable
+    {
+        #region Properties
+
+        #endregion
+
+        #region Construction
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public ServiceRepository()
+        {
+            Log.Trace("ServiceRepository::Constructor");
+
+            _lock = new object();
+            _dictionary = new Dictionary<Type, object>();
+        }
+
+        #endregion
+
+        #region Private members
+
+        private readonly object _lock;
+
+        private readonly Dictionary<Type, object> _dictionary;
+
+        #endregion
+
+        #region Public methods
+
+        /// <summary>
+        /// Clear the repository.
+        /// </summary>
+        public ServiceRepository Clear()
+        {
+            Log.Trace("ServiceRepository::Clear");
+
+            lock (_lock)
+            {
+                _dictionary.Clear();
+            }
+
+            return this;
+        }
+
+        /// <summary>
+        /// Register service of given type.
+        /// </summary>
+        [NotNull]
+        public ServiceRepository Register
+            (
+                [NotNull] Type type,
+                [NotNull] object service
+            )
+        {
+            Code.NotNull(type, "type");
+            Code.NotNull(service, "service");
+
+            Log.Trace("ServiceRepository::Register");
+
+            lock (_lock)
+            {
+                _dictionary[type] = service;
+            }
+
+            return this;
+        }
+
+        /// <summary>
+        /// Unregister service of given type.
+        /// </summary>
+        [NotNull]
+        public ServiceRepository Unregister
+            (
+                [NotNull] Type type
+            )
+        {
+            Code.NotNull(type, "type");
+
+            Log.Trace("ServiceRepository::Unregister");
+
+            lock (_lock)
+            {
+                _dictionary.Remove(type);
+            }
+
+            return this;
+        }
+
+        #endregion
+
+        #region IDisposable members
+
+        /// <inheritdoc cref="IDisposable.Dispose"/>
+        public void Dispose()
+        {
+            Log.Trace("ServiceRepository::Dispose");
+
+            lock (_lock)
+            {
+                foreach (var pair in _dictionary)
+                {
+                    IDisposable service 
+                        = pair.Value as IDisposable;
+                    if (!ReferenceEquals(service, null))
+                    {
+                        service.Dispose();
+                    }
+                }
+            }
+        }
+
+        #endregion
+    }
+}
