be1be96e2a7e95714989728591feb6951f702a68
Alexey Mironov <amironov73@gmail.com>
2017-04-27 09:56:38 +0800

    Improve IrbisUpprtCaseTable

--- Source/Classic/Libs/ManagedIrbis/Source/IrbisAlphabetTable.cs
+++ Source/Classic/Libs/ManagedIrbis/Source/IrbisAlphabetTable.cs
@@ -29,6 +29,7 @@ using MoonSharp.Interpreter;
 
 namespace ManagedIrbis
 {
+    //
     // Таблица алфавитных символов используется системой ИРБИС
     // при разбиении текста на слова и представляет собой список
     // кодов символов, которые считаются алфавитными.
@@ -40,13 +41,14 @@ namespace ManagedIrbis
     // ИРБИС и может быть изменено.
     //
     // Стандартное содержимое
+    //
     // 038 064 065 066 067 068 069 070 071 072 073 074 075 076 077 078 079 080 081 082 083 084 085 086 087 088 089 090 097 098 099 100
     // 101 102 103 104 105 106 107 108 109 110 111 112 113 114 115 116 117 118 119 120 121 122 128 129 130 131 132 133 134 135 136 137
     // 138 139 140 141 142 143 144 145 146 147 148 149 150 151 152 153 154 155 156 157 158 159 160 161 162 163 164 165 166 167 168 169
     // 170 171 172 173 174 175 176 177 178 179 180 181 182 183 184 185 186 187 188 189 190 191 192 193 194 195 196 197 198 199 200 201
     // 202 203 204 205 206 207 208 209 210 211 212 213 214 215 216 217 218 219 220 221 222 223 224 225 226 227 228 229 230 231 232 233
     // 234 235 236 237 238 239 240 241 242 243 244 245 246 247 248 249 250 251 252 253 254 255
-
+    //
 
     /// <summary>
     /// ISISAC.TAB
@@ -55,7 +57,8 @@ namespace ManagedIrbis
     [MoonSharpUserData]
     public sealed class IrbisAlphabetTable
 #if !SILVERLIGHT && !WIN81 && !PORTABLE
-        : IHandmadeSerializable
+        : IHandmadeSerializable,
+        IVerifiable
 #endif
     {
         #region Constants
@@ -433,9 +436,11 @@ namespace ManagedIrbis
         {
             Code.NotNull(text, "text");
 
-            if (text.Length == 0
-                || (IsAlpha(text[0])
-                            && IsAlpha(text[text.Length - 1])))
+            if (
+                text.Length == 0
+                || IsAlpha(text[0])
+                    && IsAlpha(text[text.Length - 1])
+               )
             {
                 return text;
             }
@@ -521,7 +526,7 @@ namespace ManagedIrbis
 
 #if !SILVERLIGHT && !WIN81 && !PORTABLE
 
-        /// <inheritdoc />
+        /// <inheritdoc cref="IHandmadeSerializable.RestoreFromStream" />
         public void RestoreFromStream
             (
                 BinaryReader reader
@@ -534,7 +539,7 @@ namespace ManagedIrbis
             _BuildCharacters();
         }
 
-        /// <inheritdoc />
+        /// <inheritdoc cref="IHandmadeSerializable.SaveToStream" />
         public void SaveToStream
             (
                 BinaryWriter writer
@@ -547,6 +552,41 @@ namespace ManagedIrbis
         }
 
 #endif
+
+        #endregion
+
+        #region IVerifiable members
+
+        /// <inheritdoc cref="IVerifiable.Verify"/>
+        public bool Verify
+            (
+                bool throwOnError
+            )
+        {
+            Verifier<IrbisAlphabetTable> verifier
+                = new Verifier<IrbisAlphabetTable>(this, throwOnError);
+
+            verifier
+                .Assert(_table.Length != 0, "table.Length");
+
+            for (int i = 0; i < _table.Length; i++)
+            {
+                byte value = _table[i];
+                for (int j = i + 1; j < _table.Length; j++)
+                {
+                    if (_table[j] == value)
+                    {
+                        verifier.Assert
+                            (
+                                false,
+                                "repeat byte: " + value
+                            );
+                    }
+                }
+            }
+
+            return verifier.Result;
+        }
 
         #endregion
     }
--- Source/Classic/Libs/ManagedIrbis/Source/IrbisUpperCaseTable.cs
+++ Source/Classic/Libs/ManagedIrbis/Source/IrbisUpperCaseTable.cs
@@ -14,6 +14,10 @@ using System.IO;
 using System.Text;
 using System.Text.RegularExpressions;
 
+using AM;
+using AM.IO;
+using AM.Runtime;
+
 using CodeJam;
 
 using JetBrains.Annotations;
@@ -26,12 +30,38 @@ using MoonSharp.Interpreter;
 
 namespace ManagedIrbis
 {
+    //
+    // Системная таблица ISISUCW.TAB для перевода
+    // текста в верхний регистр.
+    // Таблица реализована в виде текстового файла.
+    // Местонахождение и имя файла по умолчанию:
+    // <IRBIS_SERVER_ROOT>\ISISUCW.TAB.
+    // Местонахождение и имя файла определяется значением
+    // параметра UCTABPATH в конфигурационном файле
+    // ИРБИС и может быть изменено.
+    //
+    // Стандартное содержимое
+    //
+    // 000 001 002 003 004 005 006 007 008 009 010 011 012 013 014 015 016 017 018 019 020 021 022 023 024 025 026 027 028 028 030 031
+    // 032 033 034 035 036 037 038 039 040 041 042 043 044 045 046 047 048 049 050 051 052 053 054 055 056 057 058 059 060 061 062 063
+    // 064 065 066 067 068 069 070 071 072 073 074 075 076 077 078 079 080 081 082 083 084 085 086 087 088 089 090 091 092 093 094 095
+    // 096 065 066 067 068 069 070 071 072 073 074 075 076 077 078 079 080 081 082 083 084 085 086 087 088 089 090 123 124 125 126 127
+    // 128 129 130 131 132 133 134 135 136 137 138 139 140 141 142 143 144 145 146 147 148 149 150 151 152 153 154 155 156 157 158 159
+    // 160 161 161 163 164 165 166 167 168 169 170 171 172 173 174 175 176 177 178 178 165 181 182 183 168 185 170 187 163 189 189 175
+    // 192 193 194 195 196 197 198 199 200 201 202 203 204 205 206 207 208 209 210 211 212 213 214 215 216 217 218 219 220 221 222 223
+    // 192 193 194 195 196 197 198 199 200 201 202 203 204 205 206 207 208 209 210 211 212 213 214 215 216 217 218 219 220 221 222 223
+    //
+
     /// <summary>
     /// Table for uppercase character conversion.
     /// </summary>
     [PublicAPI]
     [MoonSharpUserData]
     public sealed class IrbisUpperCaseTable
+#if !SILVERLIGHT && !WIN81 && !PORTABLE
+        : IHandmadeSerializable,
+        IVerifiable
+#endif
     {
         #region Constants
 
@@ -43,9 +73,62 @@ namespace ManagedIrbis
         #endregion
 
         #region Properties
+
+        /// <summary>
+        /// Собственно таблица.
+        /// </summary>
+        public Dictionary<char, char> Mapping
+        {
+            get { return _mapping; }
+        }
+
         #endregion
 
         #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public IrbisUpperCaseTable()
+        {
+            _encoding = IrbisEncoding.Ansi;
+            _table = new byte[]
+                {
+                    000, 001, 002, 003, 004, 005, 006, 007,
+                    008, 009, 010, 011, 012, 013, 014, 015,
+                    016, 017, 018, 019, 020, 021, 022, 023,
+                    024, 025, 026, 027, 028, 028, 030, 031,
+                    032, 033, 034, 035, 036, 037, 038, 039,
+                    040, 041, 042, 043, 044, 045, 046, 047,
+                    048, 049, 050, 051, 052, 053, 054, 055,
+                    056, 057, 058, 059, 060, 061, 062, 063,
+                    064, 065, 066, 067, 068, 069, 070, 071,
+                    072, 073, 074, 075, 076, 077, 078, 079,
+                    080, 081, 082, 083, 084, 085, 086, 087,
+                    088, 089, 090, 091, 092, 093, 094, 095,
+                    096, 065, 066, 067, 068, 069, 070, 071,
+                    072, 073, 074, 075, 076, 077, 078, 079,
+                    080, 081, 082, 083, 084, 085, 086, 087,
+                    088, 089, 090, 123, 124, 125, 126, 127,
+                    128, 129, 130, 131, 132, 133, 134, 135,
+                    136, 137, 138, 139, 140, 141, 142, 143,
+                    144, 145, 146, 147, 148, 149, 150, 151,
+                    152, 153, 154, 155, 156, 157, 158, 159,
+                    160, 161, 161, 163, 164, 165, 166, 167,
+                    168, 169, 170, 171, 172, 173, 174, 175,
+                    176, 177, 178, 178, 165, 181, 182, 183,
+                    168, 185, 170, 187, 163, 189, 189, 175,
+                    192, 193, 194, 195, 196, 197, 198, 199,
+                    200, 201, 202, 203, 204, 205, 206, 207,
+                    208, 209, 210, 211, 212, 213, 214, 215,
+                    216, 217, 218, 219, 220, 221, 222, 223,
+                    192, 193, 194, 195, 196, 197, 198, 199,
+                    200, 201, 202, 203, 204, 205, 206, 207,
+                    208, 209, 210, 211, 212, 213, 214, 215,
+                    216, 217, 218, 219, 220, 221, 222, 223
+                };
+            _BuildMapping();
+        }
 
         /// <summary>
         /// Constructor.
@@ -81,19 +164,72 @@ namespace ManagedIrbis
 
             _encoding = encoding;
             _table = table;
+            _BuildMapping();
         }
 
         #endregion
 
         #region Private members
 
-        private readonly Encoding _encoding;
+        private static object _lock = new object();
 
-        private readonly byte[] _table;
+        private static IrbisUpperCaseTable _instance;
+
+        private Encoding _encoding;
+
+        private byte[] _table;
+
+        private Dictionary<char, char> _mapping;
+
+        private void _BuildMapping()
+        {
+            byte[] bytes = new byte[256];
+            for (int i = 0; i < 256; i++)
+            {
+                bytes[i] = (byte)i;
+            }
+            char[] fromMapping = _encoding.GetChars(bytes);
+            char[] toMapping = _encoding.GetChars(_table);
+            _mapping = new Dictionary<char, char>(256);
+            for (int i = 0; i < 256; i++)
+            {
+                _mapping[fromMapping[i]] = toMapping[i];
+            }
+        }
 
         #endregion
 
         #region Public methods
+
+        /// <summary>
+        /// Get global instance of the
+        /// <see cref="IrbisUpperCaseTable"/>.
+        /// </summary>
+        [NotNull]
+        public static IrbisUpperCaseTable GetInstance
+            (
+                [NotNull] IrbisConnection connection
+            )
+        {
+            Code.NotNull(connection, "connection");
+
+            if (ReferenceEquals(_instance, null))
+            {
+                lock (_lock)
+                {
+                    if (ReferenceEquals(_instance, null))
+                    {
+                        _instance = FromServer
+                            (
+                                connection,
+                                FileName
+                            );
+                    }
+                }
+            }
+
+            return _instance;
+        }
 
         /// <summary>
         /// Load the table from specified server file.
@@ -201,6 +337,18 @@ namespace ManagedIrbis
         }
 
         /// <summary>
+        /// Free global instance of
+        /// <see cref="IrbisUpperCaseTable"/>.
+        /// </summary>
+        public static void ResetInstance()
+        {
+            lock (_lock)
+            {
+                _instance = null;
+            }
+        }
+
+        /// <summary>
         /// Формируем исходный код с определением таблицы.
         /// </summary>
         public void ToSourceCode
@@ -241,9 +389,13 @@ namespace ManagedIrbis
                 char c
             )
         {
-            string text = new string(c, 1);
+            char result;
+            if (!Mapping.TryGetValue(c, out result))
+            {
+                result = c;
+            }
 
-            return ToUpper(text)[0];
+            return result;
         }
 
         /// <summary>
@@ -257,20 +409,18 @@ namespace ManagedIrbis
         {
             Code.NotNull(text, "text");
 
-            byte[] bytes = _encoding.GetBytes(text);
-            for (int i = 0; i < bytes.Length; i++)
+            StringBuilder result = new StringBuilder(text.Length);
+            foreach (char c1 in text)
             {
-                bytes[i] = _table[bytes[i]];
+                char c2;
+                if (!Mapping.TryGetValue(c1, out c2))
+                {
+                    c2 = c1;
+                }
+                result.Append(c2);
             }
 
-            string result = _encoding.GetString
-                (
-                    bytes,
-                    0,
-                    bytes.Length
-                );
-
-            return result;
+            return result.ToString();
         }
 
 #if !WIN81 && !PORTABLE
@@ -327,6 +477,58 @@ namespace ManagedIrbis
                     count = 0;
                 }
             }
+        }
+
+        #endregion
+
+        #region IHandmadeSerializable members
+
+#if !SILVERLIGHT && !WIN81 && !PORTABLE
+
+        /// <inheritdoc cref="IHandmadeSerializable.RestoreFromStream" />
+        public void RestoreFromStream
+            (
+                BinaryReader reader
+            )
+        {
+            Code.NotNull(reader, "reader");
+
+            _encoding = Encoding.GetEncoding(reader.ReadInt32());
+            _table = reader.ReadByteArray();
+            _BuildMapping();
+        }
+
+        /// <inheritdoc cref="IHandmadeSerializable.SaveToStream" />
+        public void SaveToStream
+            (
+                BinaryWriter writer
+            )
+        {
+            Code.NotNull(writer, "writer");
+
+            writer.Write(_encoding.CodePage);
+            writer.WriteArray(_table);
+        }
+
+#endif
+
+        #endregion
+
+        #region IVerifiable members
+
+        /// <inheritdoc cref="IVerifiable.Verify"/>
+        public bool Verify
+            (
+                bool throwOnError
+            )
+        {
+            Verifier<IrbisUpperCaseTable> verifier
+                = new Verifier<IrbisUpperCaseTable>(this, throwOnError);
+
+            verifier
+                .Assert(_table.Length != 0, "table.Length");
+
+            return verifier.Result;
         }
 
         #endregion
