fdfe24f063b1c78b07a4997e60941ad30d62c591
Alexey Mironov <amironov73@gmail.com>
2017-05-01 15:45:52 +0800

    Fix warnings

--- Source/Classic/Libs/ManagedIrbis.Office/Properties/AssemblyInfo.cs
+++ Source/Classic/Libs/ManagedIrbis.Office/Properties/AssemblyInfo.cs
@@ -15,9 +15,6 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 #endregion
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
