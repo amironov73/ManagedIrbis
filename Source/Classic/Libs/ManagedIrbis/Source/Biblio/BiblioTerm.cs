33d8be94ef6c965b950151d685b47578e650865a
Alexey Mironov <amironov73@gmail.com>
2017-05-30 20:30:02 +0800

    Working on BiblioDocument

---
+++ Source/Classic/Libs/ManagedIrbis/Source/Biblio/BiblioTerm.cs
@@ -0,0 +1,61 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+/* BiblioTerm.cs -- 
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AM;
+using AM.Collections;
+using AM.IO;
+using AM.Runtime;
+using AM.Text;
+
+using CodeJam;
+
+using JetBrains.Annotations;
+
+using ManagedIrbis.Client;
+
+using MoonSharp.Interpreter;
+
+#endregion
+
+namespace ManagedIrbis.Biblio
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [PublicAPI]
+    [MoonSharpUserData]
+    public sealed class BiblioTerm
+    {
+        #region Properties
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [CanBeNull]
+        public string Prefix { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [CanBeNull]
+        public string Data { get; set; }
+
+        #endregion
+    }
+}
