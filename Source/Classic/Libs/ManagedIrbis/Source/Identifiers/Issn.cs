a998546bc5a94ff41034ceb4a75c7fa60c2be6d5
Alexey Mironov <amironov73@gmail.com>
2017-06-09 16:51:45 +0800

    Working on Issn class

--- Source/Classic/Libs/ManagedIrbis/Source/Identifiers/Issn.cs
+++ Source/Classic/Libs/ManagedIrbis/Source/Identifiers/Issn.cs
@@ -63,8 +63,13 @@ namespace ManagedIrbis.Identifiers
     // См. https://ru.wikipedia.org/wiki/%D0%9C%D0%B5%D0%B6%D0%B4%D1%83%D0%BD%D0%B0%D1%80%D0%BE%D0%B4%D0%BD%D1%8B%D0%B9_%D1%81%D1%82%D0%B0%D0%BD%D0%B4%D0%B0%D1%80%D1%82%D0%BD%D1%8B%D0%B9_%D1%81%D0%B5%D1%80%D0%B8%D0%B9%D0%BD%D1%8B%D0%B9_%D0%BD%D0%BE%D0%BC%D0%B5%D1%80
     // https://en.wikipedia.org/wiki/International_Standard_Serial_Number
     //
-
-
+    // Пример расчёта контрольной суммы
+    // ISSN 0033-765X (журнал — «Радио», 2-2006.)
+    // 0x8 + 0x7 + 3x6 + 3x5 + 7x4 + 6x3 + 5x2 + 10x1 =
+    // 0   + 0   + 18  + 15  + 28  + 18  + 10  + 10   = 99 = 9 * 11 + 0(остаток).
+    //
+    // Контрольная сумма(остаток) = 0 — номер правильный.
+    //
 
     /// <summary>
     /// Всё, связанное с ISSN.
@@ -78,11 +83,87 @@ namespace ManagedIrbis.Identifiers
 
         #endregion
 
+        #region Public data
+
+        /// <summary>
+        /// Coefficients for control digit calculation.
+        /// </summary>
+        public static int[] Coefficients
+            = { 8, 7, 6, 5, 4, 3, 2, 1 };
+
+        #endregion
+
         #region Private members
+
+        private static int ConvertDigit
+            (
+                char c
+            )
+        {
+            int result = c == 'X' || c == 'x'
+                ? 10
+                : c - '0';
+
+            return result;
+        }
+
+        private static char ConvertDigit
+            (
+                int n
+            )
+        {
+            char result = n == 10
+                ? 'X'
+                : (char)('0' + n);
+
+            return result;
+        }
 
         #endregion
 
         #region Public methods
+
+        /// <summary>
+        /// Compute check digit.
+        /// </summary>
+        public static char ComputeCheckDigit
+            (
+                [NotNull] char[] digits
+            )
+        {
+            Code.NotNull(digits, "digits");
+
+            int sum = 0;
+            for (int i = 0; i < 7; i++)
+            {
+                sum = sum + ConvertDigit(digits[i]) * Coefficients[i];
+            }
+            char result = ConvertDigit(11 - sum % 11);
+
+            return result;
+        }
+
+
+        /// <summary>
+        /// Check control digit.
+        /// </summary>
+        public static bool CheckControlDigit
+            (
+                [NotNull] char[] digits
+            )
+        {
+            Code.NotNull(digits, "digits");
+
+            int sum = 0;
+            for (int i = 0; i < 8; i++)
+            {
+                int n = ConvertDigit(digits[i]);
+                sum = sum + n * Coefficients[i];
+            }
+            bool result = sum % 11 == 0;
+
+            return result;
+        }
 
         #endregion
     }
---
+++ Source/UnitTests/ManagedIrbis/Identifiers/IssnTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using ManagedIrbis.Identifiers;
+
+namespace UnitTests.ManagedIrbis.Identifiers
+{
+    [TestClass]
+    public class IssnTest
+    {
+        [TestMethod]
+        public void Ean13_ComputeCheckDigit_1()
+        {
+            Assert.AreEqual
+                (
+                    'X',
+                    Issn.ComputeCheckDigit
+                    (
+                        "0033765X".ToCharArray()
+                    )
+                );
+        }
+
+        [TestMethod]
+        public void Issn_CheckControlDigit_1()
+        {
+            Assert.IsTrue
+                (
+                    Issn.CheckControlDigit
+                        (
+                            "0033765X".ToCharArray()
+                        )
+                );
+
+            Assert.IsFalse
+                (
+                    Issn.CheckControlDigit
+                        (
+                            "00337651".ToCharArray()
+                        )
+                );
+        }
+    }
+}
