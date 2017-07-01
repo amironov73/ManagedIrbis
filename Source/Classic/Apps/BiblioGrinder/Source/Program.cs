cf278aee09d5dec6368bdf61a0c1ee4cd9fb2d45
Alexey Mironov <amironov73@gmail.com>
2017-07-01 09:43:39 +0800

    Add BiblioGrinder, AM.Web, ManagedIrbis.Web projects

---
+++ Source/Classic/Apps/BiblioGrinder/Source/Program.cs
@@ -0,0 +1,37 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+/* Program.cs -- 
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#endregion
+
+namespace BiblioGrinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+
+            }
+        }
+    }
+}
