f4dedec1ed97482d53f0ac497981f53ce7f97bcd
Alexey Mironov <amironov73@gmail.com>
2017-07-01 14:12:26 +0800

    Add HeadToHead project

---
+++ Source/Classic/Apps/HeadToHead/Source/MainForm.cs
@@ -0,0 +1,108 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+/* MainForm.cs -- 
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using AM;
+using AM.Collections;
+using AM.Windows.Forms;
+
+using CodeJam;
+
+using IrbisUI;
+using IrbisUI.Universal;
+
+using JetBrains.Annotations;
+
+using ManagedIrbis;
+using ManagedIrbis.Batch;
+using ManagedIrbis.Infrastructure.Commands;
+using ManagedIrbis.Readers;
+
+using Newtonsoft.Json;
+
+using CM = System.Configuration.ConfigurationManager;
+
+#endregion
+
+namespace HeadToHead
+{
+    public partial class MainForm
+        : UniversalForm
+    {
+        #region Properties
+
+
+
+        #endregion
+
+        #region Construction
+
+        public MainForm()
+        {
+            Initialize += _Initialize;
+
+            InitializeComponent();
+
+            HideMainMenu();
+            HideToolStrip();
+            HideStatusStrip();
+        }
+
+        #endregion
+
+        #region Private members
+
+        private void _Initialize
+            (
+                object sender,
+                EventArgs e
+            )
+        {
+            Icon = Properties.Resources.HeadToHead;
+
+            if (TestProviderConnection())
+            {
+                WriteLine("Connection OK");
+                Active = true;
+                Controller.EnableControls();
+
+                UniversalCentralControl universal = CentralControl
+                    as UniversalCentralControl;
+                if (!ReferenceEquals(universal, null))
+                {
+                    universal.SetDefaultFocus();
+                }
+            }
+            else
+            {
+                Controller.DisableControls();
+                return;
+            }
+
+            WriteLine("Application ready");
+        }
+
+        #endregion
+    }
+}
