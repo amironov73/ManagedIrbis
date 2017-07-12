267def87feb88ba3ed5a29ae8cfc6184e3664127
Alexey Mironov <amironov73@gmail.com>
2017-07-12 17:28:49 +0800

    Add MstExplorer project

---
+++ Source/Classic/Apps/MstExplorer/Source/MainForm.cs
@@ -0,0 +1,92 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+/* MainForm.cs -- 
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using AM;
+using AM.Collections;
+using AM.Windows.Forms;
+
+using CodeJam;
+
+using IrbisUI;
+using IrbisUI.Universal;
+
+using JetBrains.Annotations;
+
+using ManagedIrbis;
+using ManagedIrbis.Batch;
+using ManagedIrbis.Infrastructure.Commands;
+using ManagedIrbis.Readers;
+
+using Newtonsoft.Json;
+
+using CM = System.Configuration.ConfigurationManager;
+
+#endregion
+
+namespace MstExplorer
+{
+    public partial class MainForm
+        : UniversalForm
+    {
+        #region Properties
+
+
+
+        #endregion
+
+        #region Construction
+
+        public MainForm()
+        {
+            Initialize += _Initialize;
+
+            InitializeComponent();
+
+            HideMainMenu();
+            //HideToolStrip();
+            //HideStatusStrip();
+
+            MstPanel panel = new MstPanel(this);
+            SetupCentralControl(panel);
+        }
+
+        #endregion
+
+        #region Private members
+
+        private void _Initialize
+        (
+            object sender,
+            EventArgs e
+        )
+        {
+            Icon = Properties.Resources.Explorer;
+
+            WriteLine("Application ready");
+        }
+
+        #endregion
+    }
+}
---
+++ Source/Classic/Apps/MstExplorer/Source/MstPanel.cs
@@ -0,0 +1,100 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+/* ListPanel.cs -- 
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using AM;
+using AM.Configuration;
+using AM.Data;
+using AM.IO;
+using AM.Json;
+using AM.Logging;
+using AM.Reflection;
+using AM.Runtime;
+using AM.Text;
+using AM.Text.Output;
+using AM.UI;
+using AM.Windows.Forms;
+
+using CodeJam;
+
+using IrbisUI;
+using IrbisUI.Universal;
+
+using JetBrains.Annotations;
+
+using ManagedIrbis;
+using ManagedIrbis.Client;
+using ManagedIrbis.Fields;
+
+using MoonSharp.Interpreter;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+#endregion
+
+namespace MstExplorer
+{
+    public partial class MstPanel 
+        : UniversalCentralControl
+    {
+        #region Properties
+
+        
+
+        #endregion
+
+        #region Construction
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public MstPanel()
+            : base(null)
+        {
+            InitializeComponent();
+        }
+
+        public MstPanel
+            (
+                [NotNull] MainForm mainForm
+            )
+            : base(mainForm)
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Private members
+
+
+
+        #endregion
+
+
+        #region Public methods
+
+        
+
+        #endregion
+    }
+}
