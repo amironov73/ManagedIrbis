481e38b17c92bce3c0a583caa14ab991288584f4
Alexey Mironov <amironov73@gmail.com>
2017-08-09 08:52:26 +0800

    Add IBF classes

---
+++ Source/Classic/Libs/ManagedIrbis/Source/Ibf/Infrastructure/IbfContext.cs
@@ -0,0 +1,87 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+/* IbfContext.cs -- 
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AM;
+using AM.Collections;
+using AM.IO;
+using AM.Logging;
+using AM.Runtime;
+using AM.Text;
+
+using CodeJam;
+
+using JetBrains.Annotations;
+
+using ManagedIrbis.Client;
+
+using MoonSharp.Interpreter;
+
+using Newtonsoft.Json;
+
+#endregion
+
+namespace ManagedIrbis.Ibf.Infrastructure
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [PublicAPI]
+    [MoonSharpUserData]
+    public sealed class IbfContext
+    {
+        #region Properties
+
+        /// <summary>
+        /// Provider.
+        /// </summary>
+        [NotNull]
+        public IrbisProvider Provider { get; private set; }
+
+        #endregion
+
+        #region Construction
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public IbfContext
+            (
+                [NotNull] IrbisProvider provider
+            )
+        {
+            Code.NotNull(provider, "provider");
+
+            Provider = provider;
+        }
+
+        #endregion
+
+        #region Private members
+
+        #endregion
+
+        #region Public methods
+
+        #endregion
+
+        #region Object members
+
+        #endregion
+    }
+}
---
+++ Source/Classic/Libs/ManagedIrbis/Source/Ibf/Infrastructure/IbfNode.cs
@@ -0,0 +1,110 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+/* IbfNode.cs -- 
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AM;
+using AM.Collections;
+using AM.IO;
+using AM.Logging;
+using AM.Runtime;
+using AM.Text;
+
+using CodeJam;
+
+using JetBrains.Annotations;
+
+using MoonSharp.Interpreter;
+
+using Newtonsoft.Json;
+
+#endregion
+
+namespace ManagedIrbis.Ibf.Infrastructure
+{
+    //
+    // http://sntnarciss.ru/irbis/spravka/wa0101104100.htm
+    // http://sntnarciss.ru/irbis/spravka/wa0203050100.htm
+    //
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [PublicAPI]
+    [MoonSharpUserData]
+    public class IbfNode
+    {
+        #region Properties
+
+        #endregion
+
+        #region Construction
+
+        #endregion
+
+        #region Private members
+
+        /// <summary>
+        /// Called after node execution.
+        /// </summary>
+        protected virtual void OnAfterExecution
+            (
+                [NotNull] IbfContext context
+            )
+        {
+            // Nothing to do here yet
+        }
+
+        /// <summary>
+        /// Called before node execution.
+        /// </summary>
+        protected virtual void OnBeforeExecution
+            (
+                [NotNull] IbfContext context
+            )
+        {
+            // Nothing to do here yet
+        }
+
+        #endregion
+
+        #region Public methods
+
+        /// <summary>
+        /// Execute the command.
+        /// </summary>
+        public virtual void Execute
+            (
+                [NotNull] IbfContext context
+            )
+        {
+            Code.NotNull(context, "context");
+
+            OnBeforeExecution(context);
+
+            // Nothing to do here yet
+
+            OnAfterExecution(context);
+        }
+
+        #endregion
+
+        #region Object members
+
+        #endregion
+    }
+}
