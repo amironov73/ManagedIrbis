dd59723ea3caa4845a9708f97684a13fa4ecfd5f
Alexey Mironov <amironov73@gmail.com>
2017-09-18 16:20:41 +0800

    Add tests

--- Source/Classic/Libs/ManagedIrbis/Source/Quality/RuleReport.cs
+++ Source/Classic/Libs/ManagedIrbis/Source/Quality/RuleReport.cs
@@ -9,8 +9,22 @@
 
 #region Using directives
 
-using System;
-using System.Collections.Generic;
+using System.IO;
+using System.ComponentModel;
+using System.Xml.Serialization;
+
+using AM;
+using AM.Collections;
+using AM.IO;
+using AM.Runtime;
+
+using CodeJam;
+
+using JetBrains.Annotations;
+
+using MoonSharp.Interpreter;
+
+using Newtonsoft.Json;
 
 #endregion
 
@@ -19,24 +33,48 @@ namespace ManagedIrbis.Quality
     /// <summary>
     /// Отчёт о работе правила.
     /// </summary>
+    [PublicAPI]
+    [XmlRoot("report")]
+    [MoonSharpUserData]
     public sealed class RuleReport
+        : IHandmadeSerializable,
+        IVerifiable
     {
         #region Properties
 
         /// <summary>
         /// Дефекты, обнаруженные правилом.
         /// </summary>
-        public List<FieldDefect> Defects { get; set; }
+        [NotNull]
+        [ItemNotNull]
+        [XmlElement("defect")]
+        [JsonProperty("defects")]
+        [DisplayName("Дефекты")]
+        public NonNullCollection<FieldDefect> Defects { get; private set; }
 
         /// <summary>
         /// Общий урон.
         /// </summary>
+        [XmlAttribute("damage")]
+        [JsonProperty("damage")]
+        [DisplayName("Штраф")]
         public int Damage { get; set; }
 
         /// <summary>
         /// Начисленный бонус.
         /// </summary>
+        [XmlAttribute("bonus")]
+        [JsonProperty("bonus")]
+        [DisplayName("Бонус")]
         public int Bonus { get; set; }
+
+        /// <summary>
+        /// Arbitrary user data.
+        /// </summary>
+        [CanBeNull]
+        [XmlIgnore]
+        [JsonIgnore]
+        public object UserData { get; set; }
 
         #endregion
 
@@ -47,7 +85,107 @@ namespace ManagedIrbis.Quality
         /// </summary>
         public RuleReport()
         {
-            Defects = new List<FieldDefect>();
+            Defects = new NonNullCollection<FieldDefect>();
+        }
+
+        #endregion
+
+        #region Public methods
+
+        /// <summary>
+        /// Should serialize the <see cref="Bonus"/> property?
+        /// </summary>
+        public bool ShouldSerializeBonus()
+        {
+            return Bonus != 0;
+        }
+
+        /// <summary>
+        /// Should serialize the <see cref="Damage"/> property?
+        /// </summary>
+        public bool ShouldSerializeDamage()
+        {
+            return Damage != 0;
+        }
+
+        /// <summary>
+        /// Should serialize the <see cref="Defects"/> property?
+        /// </summary>
+        public bool ShouldSerializeDefects()
+        {
+            return Defects.Count != 0;
+        }
+
+        #endregion
+
+        #region IHandmadeSerializable members
+
+        /// <inheritdoc cref="IHandmadeSerializable.RestoreFromStream" />
+        public void RestoreFromStream
+            (
+                BinaryReader reader
+            )
+        {
+            Code.NotNull(reader, "reader");
+
+            Defects = reader.ReadNonNullCollection<FieldDefect>();
+            Damage = reader.ReadPackedInt32();
+            Bonus = reader.ReadPackedInt32();
+        }
+
+        /// <inheritdoc cref="IHandmadeSerializable.SaveToStream" />
+        public void SaveToStream
+            (
+                BinaryWriter writer
+            )
+        {
+            Code.NotNull(writer, "writer");
+
+            writer
+                .WriteCollection(Defects)
+                .WritePackedInt32(Damage)
+                .WritePackedInt32(Bonus);
+        }
+
+        #endregion
+
+        #region IVerifiable members
+
+        /// <inheritdoc cref="IVerifiable.Verify" />
+        public bool Verify
+            (
+                bool throwOnError
+            )
+        {
+            Verifier<RuleReport> verifier
+                = new Verifier<RuleReport>(this, throwOnError);
+
+            verifier
+                .Assert(Damage >= 0, "Damage >= 0")
+                .Assert(Bonus >= 0, "Bonus >= 0");
+
+            foreach (FieldDefect defect in Defects)
+            {
+                verifier.VerifySubObject(defect, "defect");
+            }
+
+            return verifier.Result;
+        }
+
+        #endregion
+
+        #region Object members
+
+        /// <inheritdoc cref="object.ToString" />
+        public override string ToString()
+        {
+            return string.Format
+                (
+                    "Damage={0}, Bonus={1}, Defects={2}",
+                    Damage,
+                    Bonus,
+                    Defects.Count
+                );
         }
 
         #endregion
---
+++ Source/UnitTests/ManagedIrbis/Quality/RuleReportTest.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+using AM.IO;
+using AM.Json;
+using AM.Runtime;
+using AM.Xml;
+using ManagedIrbis;
+using ManagedIrbis.Quality;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests.ManagedIrbis.Quality
+{
+    [TestClass]
+    public class RuleReportTest
+    {
+        [TestMethod]
+        public void RuleReport_Construction_1()
+        {
+            RuleReport report = new RuleReport();
+            Assert.IsNotNull(report.Defects);
+            Assert.AreEqual(0, report.Defects.Count);
+            Assert.AreEqual(0, report.Damage);
+            Assert.AreEqual(0, report.Bonus);
+        }
+
+        private void _TestSerialization
+            (
+                RuleReport first
+            )
+        {
+            byte[] bytes = first.SaveToMemory();
+            RuleReport second = bytes.RestoreObjectFromMemory<RuleReport>();
+            Assert.AreEqual(first.Defects.Count, second.Defects.Count);
+            for (int i = 0; i < first.Defects.Count; i++)
+            {
+                Assert.AreEqual(first.Defects[i].Damage, second.Defects[i].Damage);
+                Assert.AreEqual(first.Defects[i].Field, second.Defects[i].Field);
+                Assert.AreEqual(first.Defects[i].Repeat, second.Defects[i].Repeat);
+                Assert.AreEqual(first.Defects[i].Message, second.Defects[i].Message);
+                Assert.AreEqual(first.Defects[i].Subfield, second.Defects[i].Subfield);
+                Assert.AreEqual(first.Defects[i].Value, second.Defects[i].Value);
+            }
+            Assert.AreEqual(first.Damage, second.Damage);
+            Assert.AreEqual(first.Bonus, second.Bonus);
+            Assert.IsNull(second.UserData);
+        }
+
+        [TestMethod]
+        public void RuleReport_Serialization_1()
+        {
+            RuleReport report = new RuleReport();
+            _TestSerialization(report);
+
+
+            report.UserData = "User data";
+            _TestSerialization(report);
+
+            report.Defects.Add(new FieldDefect{Damage = 10, Field = 100, Message = "Сообщение"});
+            report.Damage = 10;
+            _TestSerialization(report);
+        }
+
+        [TestMethod]
+        public void RuleReport_Verify_1()
+        {
+            RuleReport report = new RuleReport();
+            Assert.IsTrue(report.Verify(false));
+
+            report.Damage = -10;
+            Assert.IsFalse(report.Verify(false));
+
+            report.Damage = 10;
+            report.Defects.Add(new FieldDefect{Damage = 10, Field = 100, Message = "Сообщение"});
+            Assert.IsTrue(report.Verify(false));
+        }
+
+        [TestMethod]
+        public void RuleReport_ToXml_1()
+        {
+            RuleReport report = new RuleReport();
+            Assert.AreEqual("<report />", XmlUtility.SerializeShort(report));
+
+            report.Damage = 100;
+            report.Bonus = 10;
+            report.Defects.Add(new FieldDefect{Damage = 100, Field = 200, Message = "Сообщение"});
+            Assert.AreEqual("<report damage=\"100\" bonus=\"10\"><defect field=\"200\" message=\"Сообщение\" damage=\"100\" /></report>", XmlUtility.SerializeShort(report));
+        }
+
+        [TestMethod]
+        public void RuleReport_ToJson_1()
+        {
+            RuleReport report = new RuleReport();
+            Assert.AreEqual("{}", JsonUtility.SerializeShort(report));
+
+            report.Damage = 100;
+            report.Bonus = 10;
+            report.Defects.Add(new FieldDefect { Damage = 100, Field = 200, Message = "Сообщение" });
+            Assert.AreEqual("{'defects':[{'field':200,'message':'Сообщение','damage':100}],'damage':100,'bonus':10}", JsonUtility.SerializeShort(report));
+        }
+
+        [TestMethod]
+        public void RuleReport_ToString_1()
+        {
+            RuleReport report = new RuleReport();
+            Assert.AreEqual("Damage=0, Bonus=0, Defects=0", report.ToString());
+
+            report.Damage = 100;
+            report.Bonus = 5;
+            report.Defects.Add(new FieldDefect());
+            Assert.AreEqual("Damage=100, Bonus=5, Defects=1", report.ToString());
+        }
+    }
+}
