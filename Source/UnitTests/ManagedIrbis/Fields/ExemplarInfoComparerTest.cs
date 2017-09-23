e9a9c749c171243f3f9bcfdda0fc66eab5449a93
Alexey Mironov <amironov73@gmail.com>
2017-09-23 13:53:26 +0800

    Add tests

---
+++ Source/UnitTests/ManagedIrbis/Fields/ExemplarInfoComparerTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+
+using ManagedIrbis.Fields;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests.ManagedIrbis.Fields
+{
+    [TestClass]
+    public class ExemplarInfoComparerTest
+    {
+        [NotNull]
+        private ExemplarInfo _GetFirst()
+        {
+            return new ExemplarInfo
+            {
+                Number = "1234",
+                Description = "First description"
+            };
+        }
+
+        [NotNull]
+        private ExemplarInfo _GetSecond()
+        {
+            return new ExemplarInfo
+            {
+                Number = "234",
+                Description = "Second description"
+            };
+        }
+
+        [TestMethod]
+        public void ExemplarInfoComparer_ByNumber_1()
+        {
+            ExemplarInfo first = _GetFirst();
+            ExemplarInfo second = _GetSecond();
+            IComparer<ExemplarInfo> comparer = ExemplarInfoComparer.ByNumber();
+
+            Assert.IsTrue
+                (
+                    comparer.Compare(first, second) > 0
+                );
+        }
+
+        [TestMethod]
+        public void ExemplarInfoComparer_ByDescription_1()
+        {
+            ExemplarInfo first = _GetFirst();
+            ExemplarInfo second = _GetSecond();
+            IComparer<ExemplarInfo> comparer = ExemplarInfoComparer.ByDescription();
+
+            Assert.IsTrue
+            (
+                comparer.Compare(first, second) < 0
+            );
+        }
+    }
+}
