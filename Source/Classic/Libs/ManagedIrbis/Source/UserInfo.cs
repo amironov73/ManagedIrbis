78c8fc3158f9fdcb0e5d9c7858f49cee70646f4e
Alexey Mironov <amironov73@gmail.com>
2017-10-01 10:36:38 +0800

    Add tests

--- Source/Classic/Libs/ManagedIrbis/Source/UserInfo.cs
+++ Source/Classic/Libs/ManagedIrbis/Source/UserInfo.cs
@@ -10,7 +10,9 @@
 #region Using directives
 
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Xml.Serialization;
 
@@ -49,77 +51,94 @@ namespace ManagedIrbis
         /// <summary>
         /// Номер по порядку.
         /// </summary>
+        [CanBeNull]
         [XmlIgnore]
         [JsonIgnore]
+        [Browsable(false)]
         public string Number { get; set; }
 
         /// <summary>
         /// Логин.
         /// </summary>
-        [XmlAttribute("name")]
-        [JsonProperty("name")]
+        [CanBeNull]
+        [XmlElement("name")]
+        [JsonProperty("name", NullValueHandling = NullValueHandling.Ignore)]
         public string Name { get; set; }
 
         /// <summary>
         /// Пароль.
         /// </summary>
-        [XmlAttribute("password")]
-        [JsonProperty("password")]
+        [CanBeNull]
+        [XmlElement("password")]
+        [JsonProperty("password", NullValueHandling = NullValueHandling.Ignore)]
         public string Password { get; set; }
 
         /// <summary>
         /// Доступность АРМ Каталогизатор.
         /// </summary>
-        [XmlAttribute("cataloguer")]
-        [JsonProperty("cataloguer")]
+        [CanBeNull]
+        [XmlElement("cataloguer")]
+        [JsonProperty("cataloguer", NullValueHandling = NullValueHandling.Ignore)]
         public string Cataloger { get; set; }
 
         /// <summary>
         /// АРМ Читатель.
         /// </summary>
-        [XmlAttribute("reader")]
-        [JsonProperty("reader")]
+        [CanBeNull]
+        [XmlElement("reader")]
+        [JsonProperty("reader", NullValueHandling = NullValueHandling.Ignore)]
         public string Reader { get; set; }
 
         /// <summary>
         /// АРМ Книговыдача.
         /// </summary>
-        [XmlAttribute("circulation")]
-        [JsonProperty("circulation")]
+        [CanBeNull]
+        [XmlElement("circulation")]
+        [JsonProperty("circulation", NullValueHandling = NullValueHandling.Ignore)]
         public string Circulation { get; set; }
 
         /// <summary>
         /// АРМ Комплектатор.
         /// </summary>
-        [XmlAttribute("acquisitions")]
-        [JsonProperty("acquisitions")]
+        [CanBeNull]
+        [XmlElement("acquisitions")]
+        [JsonProperty("acquisitions", NullValueHandling = NullValueHandling.Ignore)]
         public string Acquisitions { get; set; }
 
         /// <summary>
         /// АРМ Книгообеспеченность.
         /// </summary>
+        [CanBeNull]
+        [XmlElement("provision")]
+        [JsonProperty("provision", NullValueHandling = NullValueHandling.Ignore)]
         public string Provision { get; set; }
 
         /// <summary>
         /// АРМ Администратор.
         /// </summary>
-        [XmlAttribute("administrator")]
-        [JsonProperty("administrator")]
+        [XmlElement("administrator")]
+        [JsonProperty("administrator", NullValueHandling = NullValueHandling.Ignore)]
         public string Administrator { get; set; }
 
-        #endregion
-
-        #region Construction
+        /// <summary>
+        /// Arbitrary user data.
+        /// </summary>
+        [CanBeNull]
+        [XmlIgnore]
+        [JsonIgnore]
+        [Browsable(false)]
+        public object UserData { get; set; }
 
         #endregion
 
         #region Private members
 
+        [NotNull]
         private string _FormatPair
             (
-                string prefix,
-                string value,
-                string defaultValue
+                [NotNull] string prefix,
+                [CanBeNull] string value,
+                [NotNull] string defaultValue
             )
         {
             if (value.SameString(defaultValue))
@@ -138,6 +157,26 @@ namespace ManagedIrbis
         #endregion
 
         #region Public methods
+
+        /// <summary>
+        /// Encode.
+        /// </summary>
+        [NotNull]
+        public string Encode()
+        {
+            return string.Format
+                (
+                    "{0}\r\n{1}\r\n{2}{3}{4}{5}{6}{7}",
+                    Name,
+                    Password,
+                    _FormatPair("C", Cataloger, "irbisc.ini"),
+                    _FormatPair("R", Reader, "irbisr.ini"),
+                    _FormatPair("B", Circulation, "irbisb.ini"),
+                    _FormatPair("M", Acquisitions, "irbisp.ini"),
+                    _FormatPair("K", Provision, "irbisk.ini"),
+                    _FormatPair("A", Administrator, "irbisa.ini")
+                );
+        }
 
         /// <summary>
         /// Разбор ответа сервера.
@@ -164,15 +203,15 @@ namespace ManagedIrbis
 
                 UserInfo user = new UserInfo
                 {
-                    Number = lines[0],
-                    Name = lines[1],
-                    Password = lines[2],
-                    Cataloger = lines[3],
-                    Reader = lines[4],
-                    Circulation = lines[5],
-                    Acquisitions = lines[6],
-                    Provision = lines[7],
-                    Administrator = lines[8]
+                    Number = lines[0].EmptyToNull(),
+                    Name = lines[1].EmptyToNull(),
+                    Password = lines[2].EmptyToNull(),
+                    Cataloger = lines[3].EmptyToNull(),
+                    Reader = lines[4].EmptyToNull(),
+                    Circulation = lines[5].EmptyToNull(),
+                    Acquisitions = lines[6].EmptyToNull(),
+                    Provision = lines[7].EmptyToNull(),
+                    Administrator = lines[8].EmptyToNull()
                 };
                 result.Add(user);
             }
@@ -181,37 +220,77 @@ namespace ManagedIrbis
         }
 
         /// <summary>
-        /// Encode.
+        /// Should serialize the <see cref="Cataloger"/> field?
         /// </summary>
-        [NotNull]
-        public string Encode()
+        [ExcludeFromCodeCoverage]
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public bool ShouldSerializeCataloger()
         {
-            return string.Format
-                (
-                    "{0}\r\n{1}\r\n{2}{3}{4}{5}{6}{7}",
-                    Name,
-                    Password,
-                    _FormatPair("C", Cataloger, "irbisc.ini"),
-                    _FormatPair("R", Reader, "irbisr.ini"),
-                    _FormatPair("B", Circulation, "irbisb.ini"),
-                    _FormatPair("M", Acquisitions, "irbisp.ini"),
-                    _FormatPair("K", Provision, "irbisk.ini"),
-                    _FormatPair("A", Administrator, "irbisa.ini")
-                );
+            return !string.IsNullOrEmpty(Cataloger);
+        }
+
+        /// <summary>
+        /// Should serialize the <see cref="Reader"/> field?
+        /// </summary>
+        [ExcludeFromCodeCoverage]
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public bool ShouldSerializeReader()
+        {
+            return !string.IsNullOrEmpty(Reader);
+        }
+
+        /// <summary>
+        /// Should serialize the <see cref="Circulation"/> field?
+        /// </summary>
+        [ExcludeFromCodeCoverage]
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public bool ShouldSerializeCirculation()
+        {
+            return !string.IsNullOrEmpty(Circulation);
+        }
+
+        /// <summary>
+        /// Should serialize the <see cref="Acquisitions"/> field?
+        /// </summary>
+        [ExcludeFromCodeCoverage]
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public bool ShouldSerializeAcquisitions()
+        {
+            return !string.IsNullOrEmpty(Acquisitions);
+        }
+
+        /// <summary>
+        /// Should serialize the <see cref="Provision"/> field?
+        /// </summary>
+        [ExcludeFromCodeCoverage]
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public bool ShouldSerializeProvision()
+        {
+            return !string.IsNullOrEmpty(Provision);
+        }
+
+        /// <summary>
+        /// Should serialize the <see cref="Administrator"/> field?
+        /// </summary>
+        [ExcludeFromCodeCoverage]
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public bool ShouldSerializeAdministrator()
+        {
+            return !string.IsNullOrEmpty(Administrator);
         }
 
         #endregion
 
         #region IHandmadeSerializable members
 
-        /// <summary>
-        /// Restore object state from the specified stream.
-        /// </summary>
+        /// <inheritdoc cref="IHandmadeSerializable.RestoreFromStream" />
         public void RestoreFromStream
             (
                 BinaryReader reader
             )
         {
+            Code.NotNull(reader, "reader");
+
             Number = reader.ReadNullableString();
             Name = reader.ReadNullableString();
             Password = reader.ReadNullableString();
@@ -222,9 +301,7 @@ namespace ManagedIrbis
             Administrator = reader.ReadNullableString();
         }
 
-        /// <summary>
-        /// Save object stat to the specified stream.
-        /// </summary>
+        /// <inheritdoc cref="IHandmadeSerializable.SaveToStream" />
         public void SaveToStream
             (
                 BinaryWriter writer
@@ -248,9 +325,7 @@ namespace ManagedIrbis
 
         #region IVerifiable members
 
-        /// <summary>
-        /// Verify object state.
-        /// </summary>
+        /// <inheritdoc cref="IVerifiable.Verify" />
         public bool Verify
             (
                 bool throwOnError
@@ -272,12 +347,7 @@ namespace ManagedIrbis
 
         #region Object members
 
-        /// <summary>
-        /// Returns a <see cref="System.String" />
-        /// that represents this instance.
-        /// </summary>
-        /// <returns>A <see cref="System.String" />
-        /// that represents this instance.</returns>
+        /// <inheritdoc cref="object.ToString" />
         public override string ToString()
         {
             return string.Format
@@ -286,15 +356,15 @@ namespace ManagedIrbis
                     + "Cataloger: {3}, Reader: {4}, Circulation: {5}, "
                     + "Acquisitions: {6}, Provision: {7}, " 
                     + "Administrator: {8}",
-                    Number,
-                    Name,
-                    Password,
-                    Cataloger,
-                    Reader,
-                    Circulation,
-                    Acquisitions,
-                    Provision,
-                    Administrator
+                    Number.ToVisibleString(),
+                    Name.ToVisibleString(),
+                    Password.ToVisibleString(),
+                    Cataloger.ToVisibleString(),
+                    Reader.ToVisibleString(),
+                    Circulation.ToVisibleString(),
+                    Acquisitions.ToVisibleString(),
+                    Provision.ToVisibleString(),
+                    Administrator.ToVisibleString()
                 );
         }
 
---
+++ Source/UnitTests/ManagedIrbis/UserInfoTest.cs
@@ -0,0 +1,193 @@
+﻿using AM.Json;
+using AM.Runtime;
+using AM.Text;
+using AM.Xml;
+
+using JetBrains.Annotations;
+
+using ManagedIrbis;
+using ManagedIrbis.Infrastructure;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests.ManagedIrbis
+{
+    [TestClass]
+    public class UserInfoTest
+    {
+        [NotNull]
+        private UserInfo _GetUserInfo()
+        {
+            return new UserInfo
+            {
+                Name = "TylerDurden",
+                Password = "FightClub",
+                Cataloger = "Tyler.ini"
+            };
+        }
+
+        [TestMethod]
+        public void UserInfo_Constructor_1()
+        {
+            UserInfo user = new UserInfo();
+            Assert.IsNull(user.Name);
+            Assert.IsNull(user.Password);
+            Assert.IsNull(user.Cataloger);
+            Assert.IsNull(user.Reader);
+            Assert.IsNull(user.Circulation);
+            Assert.IsNull(user.Acquisitions);
+            Assert.IsNull(user.Provision);
+            Assert.IsNull(user.Administrator);
+            Assert.IsNull(user.UserData);
+        }
+
+        private void _TestSerialization
+            (
+                [NotNull] UserInfo first
+            )
+        {
+            byte[] bytes = first.SaveToMemory();
+            UserInfo second = bytes.RestoreObjectFromMemory<UserInfo>();
+            Assert.AreEqual(first.Number, second.Number);
+            Assert.AreEqual(first.Name, second.Name);
+            Assert.AreEqual(first.Password, second.Password);
+            Assert.AreEqual(first.Cataloger, second.Cataloger);
+            Assert.AreEqual(first.Circulation, second.Circulation);
+            Assert.AreEqual(first.Acquisitions, second.Acquisitions);
+            Assert.AreEqual(first.Provision, second.Provision);
+            Assert.AreEqual(first.Administrator, second.Administrator);
+            Assert.IsNull(second.UserData);
+        }
+
+        [TestMethod]
+        public void UserInfo_Serialization_1()
+        {
+            UserInfo user = new UserInfo();
+            _TestSerialization(user);
+
+            user.UserData = "User data";
+            _TestSerialization(user);
+
+            user = _GetUserInfo();
+            _TestSerialization(user);
+        }
+
+        [TestMethod]
+        public void UserInfo_Encode_1()
+        {
+            const string reader = "irbisr.ini";
+
+            UserInfo user = new UserInfo();
+            Assert.AreEqual
+                (
+                    "\n\nC=;R=;B=;M=;K=;A=;",
+                    user.Encode().DosToUnix()
+                );
+
+            user = _GetUserInfo();
+            Assert.AreEqual
+                (
+                    "TylerDurden\nFightClub\nC=Tyler.ini;R=;B=;M=;K=;A=;",
+                    user.Encode().DosToUnix()
+                );
+
+            user.Reader = reader;
+            Assert.AreEqual
+                (
+                    "TylerDurden\nFightClub\nC=Tyler.ini;B=;M=;K=;A=;",
+                    user.Encode().DosToUnix()
+                );
+        }
+
+        [TestMethod]
+        public void UserInfo_Parse_1()
+        {
+            const string name = "TylerDurden";
+            const string password = "FightClub";
+            const string cataloger = "Tyler.ini";
+
+            ResponseBuilder builder = new ResponseBuilder()
+                .NewLine()
+                .NewLine()
+                .Append(1).NewLine()
+                .AppendAnsi(name).NewLine()
+                .AppendAnsi(password).NewLine()
+                .AppendAnsi(cataloger).NewLine() // Cataloger
+                .NewLine() // Reader
+                .NewLine() // Circulation
+                .NewLine() // Acquisitions
+                .NewLine() // Provision
+                .NewLine(); // Administrator
+
+            IrbisConnection connection = new IrbisConnection();
+            byte[] answer = builder.Encode();
+            byte[] request = new byte[0];
+            ServerResponse response = new ServerResponse
+                (
+                    connection,
+                    answer,
+                    request,
+                    true
+                );
+            UserInfo[] users = UserInfo.Parse(response);
+            Assert.AreEqual(1, users.Length);
+            Assert.AreEqual(name, users[0].Name);
+            Assert.AreEqual(password, users[0].Password);
+            Assert.AreEqual(cataloger, users[0].Cataloger);
+            Assert.IsNull(users[0].Reader);
+            Assert.IsNull(users[0].Circulation);
+            Assert.IsNull(users[0].Acquisitions);
+            Assert.IsNull(users[0].Provision);
+            Assert.IsNull(users[0].Administrator);
+            Assert.IsNull(users[0].UserData);
+        }
+
+        [TestMethod]
+        public void UserInfo_ToXml_1()
+        {
+            UserInfo user = new UserInfo();
+            Assert.AreEqual("<user />", XmlUtility.SerializeShort(user));
+
+            user = _GetUserInfo();
+            Assert.AreEqual("<user><name>TylerDurden</name><password>FightClub</password><cataloguer>Tyler.ini</cataloguer></user>", XmlUtility.SerializeShort(user));
+        }
+
+        [TestMethod]
+        public void UserInfo_ToJson_1()
+        {
+            UserInfo user = new UserInfo();
+            Assert.AreEqual("{}", JsonUtility.SerializeShort(user));
+
+            user = _GetUserInfo();
+            Assert.AreEqual("{'name':'TylerDurden','password':'FightClub','cataloguer':'Tyler.ini'}", JsonUtility.SerializeShort(user));
+        }
+
+        [TestMethod]
+        public void UserInfo_Verify_1()
+        {
+            UserInfo user = new UserInfo();
+            Assert.IsFalse(user.Verify(false));
+
+            user = _GetUserInfo();
+            Assert.IsTrue(user.Verify(false));
+        }
+
+        [TestMethod]
+        public void UserInfo_ToString_1()
+        {
+            UserInfo user = new UserInfo();
+            Assert.AreEqual
+                (
+                    "Number: (null), Name: (null), Password: (null), Cataloger: (null), Reader: (null), Circulation: (null), Acquisitions: (null), Provision: (null), Administrator: (null)",
+                    user.ToString().DosToUnix()
+                );
+
+            user = _GetUserInfo();
+            Assert.AreEqual
+                (
+                    "Number: (null), Name: TylerDurden, Password: FightClub, Cataloger: Tyler.ini, Reader: (null), Circulation: (null), Acquisitions: (null), Provision: (null), Administrator: (null)",
+                    user.ToString().DosToUnix()
+                );
+        }
+    }
+}
