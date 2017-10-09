6077cd46f4b01b42bda5468094706ea4564a82c0
Alexey Mironov <amironov73@gmail.com>
2017-10-09 16:24:47 +0800

    Add PingPlotter control

---
+++ Source/Classic/Libs/IrbisUI/Source/Statistics/PingPlotter.cs
@@ -0,0 +1,174 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+/* PingPlotter.cs -- 
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using JetBrains.Annotations;
+
+using ManagedIrbis.Statistics;
+
+using MoonSharp.Interpreter;
+
+#endregion
+
+namespace IrbisUI.Source.Statistics
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [PublicAPI]
+    [MoonSharpUserData]
+
+    public sealed partial class PingPlotter
+        : Control
+    {
+        #region Properties
+
+        /// <summary>
+        /// Statistics.
+        /// </summary>
+        [CanBeNull]
+        public PingStatistics Statistics { get; set; }
+
+        #endregion
+
+        #region Construction
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public PingPlotter()
+        {
+            BackColor = Color.White;
+            DoubleBuffered = true;
+            TabStop = false;
+            SetStyle
+                (
+                    ControlStyles.DoubleBuffer
+                    | ControlStyles.OptimizedDoubleBuffer
+                    | ControlStyles.ResizeRedraw,
+                    true
+                );
+            SetStyle
+                (
+                    ControlStyles.Selectable,
+                    false
+                );
+
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Control members
+
+        /// <inheritdoc cref="Control.OnPaint" />
+        protected override void OnPaint
+            (
+                PaintEventArgs e
+            )
+        {
+            base.OnPaint(e);
+
+            if (ReferenceEquals(Statistics, null))
+            {
+                return;
+            }
+
+            PingData[] data = Statistics.Data.ToArray();
+            if (data.Length == 0)
+            {
+                return;
+            }
+
+            int maxTime = Statistics.MaxTime;
+            if (maxTime == 0)
+            {
+                maxTime = 1;
+            }
+            Rectangle workArea = ClientRectangle;
+            int height = workArea.Height;
+            int top = workArea.Top;
+            int bottom = workArea.Bottom;
+            double scale = (double)height / maxTime;
+
+            Graphics graphics = e.Graphics;
+
+            using (Pen linePen = new Pen(Color.LightBlue))
+            {
+                int time = 50;
+                while (time < maxTime)
+                {
+                    int y = (int)(bottom - time * scale);
+                    graphics.DrawLine
+                        (
+                            linePen,
+                            workArea.Left,
+                            y,
+                            workArea.Right,
+                            y
+                        );
+
+                    time += 50;
+                }
+            }
+
+            using (Pen linePen = new Pen(Color.Blue))
+            using (Pen errorPen = new Pen(Color.Red))
+            using (Pen endPen = new Pen(Color.LightGreen))
+            {
+                int i = 0;
+                int x1 = workArea.Left;
+                int x2 = x1 + 1;
+                int y1 = (int)(bottom - data[i].RoundTripTime * scale);
+                int right = workArea.Right;
+
+                while (x2 < right && i < data.Length)
+                {
+                    if (data[i].Success)
+                    {
+                        int y2 = (int)(bottom - data[i].RoundTripTime * scale);
+                        graphics.DrawLine(linePen, x1, y1, x2, y2);
+                        y1 = y2;
+                    }
+                    else
+                    {
+                        graphics.DrawLine
+                            (
+                                errorPen,
+                                x1, y1, x1, bottom
+                            );
+                    }
+
+                    x1++;
+                    x2++;
+                    i++;
+                }
+
+                graphics.DrawLine
+                    (
+                        endPen,
+                        x1, top, x1, bottom
+                    );
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Source/UITests/Sources/Tests/PingPlotterTest.cs
@@ -0,0 +1,114 @@
+﻿/* PingPlotterTest.cs -- 
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using AM;
+using AM.Windows.Forms;
+
+using CodeJam;
+
+using IrbisUI;
+using IrbisUI.Source.Statistics;
+
+using JetBrains.Annotations;
+
+using ManagedIrbis;
+using ManagedIrbis.Statistics;
+
+using MoonSharp.Interpreter;
+
+using Newtonsoft.Json;
+
+#endregion
+
+namespace UITests
+{
+    public sealed class PingPlotterTest
+        : IUITest
+    {
+        #region Private members
+
+        private void _FillStatistics
+            (
+                [NotNull] PingStatistics statistics
+            )
+        {
+            statistics.Clear();
+            Random random = new Random();
+            const int min = 10, max = 1000;
+            int current = random.Next(min, max);
+            int howMany = random.Next(10, 1000);
+            for (int i = 0; i < howMany; i++)
+            {
+                PingData item = new PingData
+                {
+                    Moment = DateTime.Now,
+                    RoundTripTime = current,
+                    Success = random.Next(1000) % 17 != 0
+                };
+                statistics.Add(item);
+                current = Math.Max(min, Math.Min
+                    (
+                        max,
+                        current + random.Next(11)) - 5
+                    );
+            }
+        }
+
+        #endregion
+
+        #region IUITest members
+
+        public void Run
+            (
+                IWin32Window ownerWindow
+            )
+        {
+            IrbisConnection connection = new IrbisConnection();
+            IrbisPing pinger = new IrbisPing(connection);
+            _FillStatistics(pinger.Statistics);
+
+            using (Form form = new Form())
+            {
+                form.Size = new Size(800, 600);
+
+                PingPlotter plotter = new PingPlotter
+                {
+                    Location = new Point(10, 10),
+                    Size = new Size(600, 300),
+                    Statistics = pinger.Statistics
+                };
+                form.Controls.Add(plotter);
+
+                Button button = new Button
+                {
+                    Location = new Point(620, 10),
+                    Width = 75,
+                    Text = "Обновить"
+                };
+                form.Controls.Add(button);
+                button.Click += (sender, args) =>
+                {
+                    _FillStatistics(pinger.Statistics);
+                    plotter.Invalidate();
+                };
+
+                form.ShowDialog(ownerWindow);
+            }
+        }
+
+        #endregion
+    }
+}
