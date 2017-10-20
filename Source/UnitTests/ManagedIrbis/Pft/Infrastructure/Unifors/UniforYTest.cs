d21f901e30c8104bff7cd651789f3ecd16e0c3fe
Alexey Mironov <amironov73@gmail.com>
2017-10-20 08:23:03 +0800

    Add test for &uf('y')

---
+++ Source/UnitTests/ManagedIrbis/Pft/Infrastructure/Unifors/UniforYTest.cs
@@ -0,0 +1,40 @@
+﻿using ManagedIrbis;
+using ManagedIrbis.Pft.Infrastructure;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests.ManagedIrbis.Pft.Infrastructure.Unifors
+{
+    [TestClass]
+    public class UniforYTest
+    {
+        private MarcRecord _GetRecord()
+        {
+            MarcRecord record = new MarcRecord();
+            record.Fields.Add(RecordField.Parse(910, "^A0^B32^C20070104^DБИНТ^E7.50^H107206G^=2^U2004/7^S20070104^!ХР"));
+            record.Fields.Add(RecordField.Parse(910, "^A0^B33^C20070104^DБИНТ^E60.00^H107216G^U2004/7^S20070104^!ХР"));
+            record.Fields.Add(RecordField.Parse(910, "^A0^B557^C19990924^DЧЗ^H107236G^=2^U2004/7"));
+            record.Fields.Add(RecordField.Parse(910, "^A0^B558^C19990924^DЧЗ^H107246G^=2^U2004/7"));
+            record.Fields.Add(RecordField.Parse(910, "^A0^B559^C19990924^DЧЗ^H107256G^=2^U2004/7"));
+            record.Fields.Add(RecordField.Parse(910, "^AU^B556^C19990924^DХР^E2400^H107226G^112^U1996/28^Y60"));
+            record.Fields.Add(RecordField.Parse(910, "^AU^BЗИ-1^C20071226^DЖГ^S20140604^125^!КДИ^01^TЗИ"));
+
+            return record;
+        }
+
+        [TestMethod]
+        public void UniforY_FreeExemplars_1()
+        {
+            PftContext context = new PftContext(null)
+            {
+                Record = _GetRecord()
+            };
+            Unifor unifor = new Unifor();
+            string expression = "Y";
+            unifor.Execute(context, null, expression);
+            string actual = context.Text;
+            Assert.AreEqual("БИНТ(2), ЧЗ(3), ХР(12), ЖГ(25)", actual);
+
+        }
+    }
+}
