1e2427979e83092ea7982912b9096ccd24777bd5
Alexey Mironov <amironov73@gmail.com>
2017-10-27 08:05:54 +0800

    Add test for PftFalse node

--- Source/UnitTests/ManagedIrbis/Pft/Infrastructure/Ast/PftFalseTest.cs
+++ Source/UnitTests/ManagedIrbis/Pft/Infrastructure/Ast/PftFalseTest.cs
@@ -10,22 +10,22 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace UnitTests.ManagedIrbis.Pft.Infrastructure.Ast
 {
     [TestClass]
-    public class PftTrueTest
+    public class PftFalseTest
     {
         [TestMethod]
-        public void PftTrue_Construction_1()
+        public void PftFalse_Construction_1()
         {
-            PftTrue node = new PftTrue();
+            PftFalse node = new PftFalse();
             Assert.IsTrue(node.ConstantExpression);
             Assert.IsTrue(node.ExtendedSyntax);
             Assert.IsFalse(node.RequiresConnection);
         }
 
         [TestMethod]
-        public void PftTrue_Construction_2()
+        public void PftFalse_Construction_2()
         {
-            PftToken token = new PftToken(PftTokenKind.True, 1, 1, "True");
-            PftTrue node = new PftTrue(token);
+            PftToken token = new PftToken(PftTokenKind.False, 1, 1, "False");
+            PftFalse node = new PftFalse(token);
             Assert.IsTrue(node.ConstantExpression);
             Assert.IsTrue(node.ExtendedSyntax);
             Assert.IsFalse(node.RequiresConnection);
@@ -35,55 +35,55 @@ namespace UnitTests.ManagedIrbis.Pft.Infrastructure.Ast
         }
 
         [TestMethod]
-        public void PftTrue_Compile_1()
+        public void PftFalse_Compile_1()
         {
             PftCompiler compiler = new PftCompiler();
             PftProgram program = new PftProgram();
-            program.Children.Add(new PftTrue());
+            program.Children.Add(new PftFalse());
             compiler.CompileProgram(program);
         }
 
         [TestMethod]
-        public void PftTrue_Execute_1()
+        public void PftFalse_Execute_1()
         {
             PftContext context = new PftContext(null);
-            PftTrue node = new PftTrue();
+            PftFalse node = new PftFalse();
             node.Execute(context);
             string actual = context.Text.DosToUnix();
-            Assert.IsTrue(node.Value);
+            Assert.IsFalse(node.Value);
             Assert.AreEqual("", actual);
         }
 
         [TestMethod]
-        public void PftTrue_PrettyPrint_1()
+        public void PftFalse_PrettyPrint_1()
         {
             PftPrettyPrinter printer = new PftPrettyPrinter();
             PftProgram program = new PftProgram();
-            program.Children.Add(new PftTrue());
+            program.Children.Add(new PftFalse());
             program.PrettyPrint(printer);
-            Assert.AreEqual("true ", printer.ToString().DosToUnix());
+            Assert.AreEqual("false ", printer.ToString().DosToUnix());
         }
 
         [TestMethod]
-        public void PftTrue_ToString_1()
+        public void PftFalse_ToString_1()
         {
-            PftTrue node = new PftTrue();
-            Assert.AreEqual("true", node.ToString());
+            PftFalse node = new PftFalse();
+            Assert.AreEqual("false", node.ToString());
         }
 
         [TestMethod]
-        public void PftTrue_Value_1()
+        public void PftFalse_Value_1()
         {
-            PftTrue node = new PftTrue();
-            Assert.IsTrue(node.Value);
+            PftFalse node = new PftFalse();
+            Assert.IsFalse(node.Value);
         }
 
         [TestMethod]
-        public void PftTrue_Value_2()
+        public void PftFalse_Value_2()
         {
-            PftTrue node = new PftTrue();
-            node.Value = false;
-            Assert.IsTrue(node.Value);
+            PftFalse node = new PftFalse();
+            node.Value = true;
+            Assert.IsFalse(node.Value);
         }
     }
 }
