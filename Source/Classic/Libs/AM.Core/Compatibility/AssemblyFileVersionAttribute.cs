9c4911807ea83190d1977e92fed5b4935fac5323
Alexey Mironov <amironov73@gmail.com>
2017-10-28 12:58:06 +0800

    Add AssemblyFileVersionAttribute for WinMobile compatibility

---
+++ Source/Classic/Libs/AM.Core/Compatibility/AssemblyFileVersionAttribute.cs
@@ -0,0 +1,36 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+/* AssemblyFileVersionAttribute.cs -- for WinMobile compatibility
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#if WINMOBILE
+
+namespace System.Reflection
+{
+    /// <summary>
+    /// For WinMobile compatibility.
+    /// </summary>
+    public sealed class AssemblyFileVersionAttribute
+        : Attribute
+    {
+        #region Construction
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public AssemblyFileVersionAttribute
+            (
+                string version
+            )
+        {
+        }
+
+        #endregion
+    }
+}
+
+#endif
