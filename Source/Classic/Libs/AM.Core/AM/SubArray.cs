0862d6c0dad3671cb7ee1d13cffd43a407ad1b8c
Alexey Mironov <amironov73@gmail.com>
2017-11-10 17:07:19 +0800

    Add SubArray class with tests

---
+++ Source/Classic/Libs/AM.Core/AM/SubArray.cs
@@ -0,0 +1,310 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+/* SubArray.cs -- portion (segment) of an array
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+using AM.Logging;
+
+using CodeJam;
+
+using JetBrains.Annotations;
+
+using MoonSharp.Interpreter;
+
+#endregion
+
+namespace AM
+{
+    /// <summary>
+    /// Segment of an array.
+    /// </summary>
+    [PublicAPI]
+    [MoonSharpUserData]
+    public struct SubArray<T>
+        : IList<T>,
+        IReadOnlyList<T>,
+        IEquatable<SubArray<T>>
+    {
+        #region Properties
+
+        /// <summary>
+        /// Array.
+        /// </summary>
+        [NotNull]
+        public T[] Array { get; private set; }
+
+        /// <summary>
+        /// Offset.
+        /// </summary>
+        public int Offset { get; private set; }
+
+        /// <summary>
+        /// Length.
+        /// </summary>
+        public int Length { get; private set; }
+
+        /// <summary>
+        /// Indexer.
+        /// </summary>
+        public T this[int index]
+        {
+            get { return Array[Offset + index]; }
+            set { Array[Offset + index] = value; }
+        }
+
+        #endregion
+
+        #region Construction
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public SubArray
+            (
+                [NotNull] T[] array,
+                int offset,
+                int length
+            )
+            : this()
+        {
+            Code.NotNull(array, "array");
+            Code.Nonnegative(offset, "offset");
+            Code.Nonnegative(length, "length");
+
+            if (offset > array.Length)
+            {
+                Log.Error
+                    (
+                        "SubArray::Constructor: "
+                        + "offset="
+                        + offset
+                    );
+
+                throw new ArgumentOutOfRangeException("offset");
+            }
+
+            Array = array;
+            Offset = offset;
+            Length = Math.Min(length, array.Length - offset);
+        }
+
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public SubArray
+            (
+                [NotNull] T[] array,
+                int offset
+            )
+            : this()
+        {
+            Code.NotNull(array, "array");
+            Code.Nonnegative(offset, "offset");
+
+            if (offset > array.Length)
+            {
+                Log.Error
+                    (
+                        "SubArray::Constructor: "
+                        + "offset="
+                        + offset
+                    );
+
+                throw new ArgumentOutOfRangeException("offset");
+            }
+
+            Array = array;
+            Offset = offset;
+            Length = array.Length - offset;
+        }
+        #endregion
+
+        #region Public methods
+
+        /// <summary>
+        /// Creates an array from this instance.
+        /// </summary>
+        [NotNull]
+        public T[] ToArray()
+        {
+            T[] result = new T[Length];
+            System.Array.Copy(Array, Offset, result, 0, Length);
+
+            return result;
+        }
+
+        #endregion
+
+        #region IList<T> members
+
+        [ExcludeFromCodeCoverage]
+        void ICollection<T>.Add(T item)
+        {
+            throw new NotSupportedException();
+        }
+
+        [ExcludeFromCodeCoverage]
+        void ICollection<T>.Clear()
+        {
+            throw new NotSupportedException();
+        }
+
+        /// <inheritdoc cref="ICollection{T}.Contains" />
+        public bool Contains(T item)
+        {
+            int index = System.Array.IndexOf(Array, item, Offset, Length);
+
+            return index >= 0;
+        }
+
+        /// <inheritdoc cref="ICollection{T}.CopyTo" />
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            System.Array.Copy(Array, Offset, array, arrayIndex, Length);
+        }
+
+        [ExcludeFromCodeCoverage]
+        bool ICollection<T>.Remove(T item)
+        {
+            throw new NotSupportedException();
+        }
+
+        [ExcludeFromCodeCoverage]
+        int ICollection<T>.Count { get { return Length; } }
+
+        [ExcludeFromCodeCoverage]
+        bool ICollection<T>.IsReadOnly
+        {
+            get { return true; }
+        }
+
+        /// <inheritdoc cref="IList{T}.IndexOf" />
+        public int IndexOf(T item)
+        {
+            int result = System.Array.IndexOf(Array, item, Offset, Length);
+
+            return result >= 0
+                ? result - Offset
+                : -1;
+        }
+
+        [ExcludeFromCodeCoverage]
+        void IList<T>.Insert(int index, T item)
+        {
+            throw new NotSupportedException();
+        }
+
+        [ExcludeFromCodeCoverage]
+        void IList<T>.RemoveAt(int index)
+        {
+            throw new NotSupportedException();
+        }
+
+        #endregion
+
+        #region IEnumerable<T> members
+
+        [ExcludeFromCodeCoverage]
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        /// <inheritdoc cref="IEnumerable{T}.GetEnumerator" />
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < Length; i++)
+            {
+                yield return this[i];
+            }
+        }
+
+        #endregion
+
+        #region IEquatable<SubArray<T>> members
+
+        /// <inheritdoc cref="IEquatable{T}.Equals(T)" />
+        public bool Equals
+            (
+                SubArray<T> other
+            )
+        {
+            return ReferenceEquals(Array, other.Array)
+                   && Offset == other.Offset
+                   && Length == other.Length;
+        }
+
+        #endregion
+
+        #region IReadOnlyList<T> members
+
+        [ExcludeFromCodeCoverage]
+        int IReadOnlyCollection<T>.Count
+        {
+            get { return Length; }
+        }
+
+        #endregion
+
+        #region Object members
+
+        /// <inheritdoc cref="ValueType.Equals(object)" />
+        public override bool Equals(object obj)
+        {
+            if (obj is SubArray<T>)
+            {
+                return Equals((SubArray<T>)obj);
+            }
+
+            return false;
+        }
+
+        /// <inheritdoc cref="ValueType.GetHashCode" />
+        [ExcludeFromCodeCoverage]
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (Array.GetHashCode() * 397 + Offset) * 397 + Length;
+            }
+        }
+
+        /// <inheritdoc cref="ValueType.ToString" />
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+            using (IEnumerator<T> enumerator = GetEnumerator())
+            {
+                if (enumerator.MoveNext())
+                {
+                    T o = enumerator.Current;
+                    result.Append(o.ToVisibleString());
+
+                    while (enumerator.MoveNext())
+                    {
+                        o = enumerator.Current;
+                        result.Append(", ");
+                        result.Append(o.ToVisibleString());
+                    }
+                }
+            }
+
+            return result.ToString();
+        }
+
+        #endregion
+    }
+}
---
+++ Source/UnitTests/AM/SubArrayTest.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using AM;
+
+using JetBrains.Annotations;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests.AM
+{
+    [TestClass]
+    public class SubArrayTest
+    {
+        [NotNull]
+        private int[] _GetArray()
+        {
+            return new[] {1, 2, 3, 4, 5, 6, 7};
+        }
+
+        [TestMethod]
+        public void SubArray_Construction_1()
+        {
+            int[] array = _GetArray();
+            SubArray<int> subArray = new SubArray<int>(array, 2, 3);
+            Assert.AreSame(array, subArray.Array);
+            Assert.AreEqual(2, subArray.Offset);
+            Assert.AreEqual(3, subArray.Length);
+
+            subArray = new SubArray<int>(array, 5, 3);
+            Assert.AreEqual(2, subArray.Length);
+
+            subArray = new SubArray<int>(array, 7, 3);
+            Assert.AreEqual(0, subArray.Length);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void SubArray_Construction_1a()
+        {
+            int[] array = _GetArray();
+            SubArray<int> subArray = new SubArray<int>(array, -2, 3);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void SubArray_Construction_1b()
+        {
+            int[] array = _GetArray();
+            SubArray<int> subArray = new SubArray<int>(array, 20, 3);
+        }
+
+        [TestMethod]
+        public void SubArray_Construction_2()
+        {
+            int[] array = _GetArray();
+            SubArray<int> subArray = new SubArray<int>(array, 2);
+            Assert.AreSame(array, subArray.Array);
+            Assert.AreEqual(2, subArray.Offset);
+            Assert.AreEqual(5, subArray.Length);
+
+            subArray = new SubArray<int>(array, 5);
+            Assert.AreEqual(2, subArray.Length);
+
+            subArray = new SubArray<int>(array, 7);
+            Assert.AreEqual(0, subArray.Length);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void SubArray_Construction_2a()
+        {
+            int[] array = _GetArray();
+            SubArray<int> subArray = new SubArray<int>(array, -2);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void SubArray_Construction_2b()
+        {
+            int[] array = _GetArray();
+            SubArray<int> subArray = new SubArray<int>(array, 20);
+        }
+
+        [TestMethod]
+        public void SubArray_Indexer_1()
+        {
+            int[] array = _GetArray();
+            SubArray<int> subArray = new SubArray<int>(array, 3);
+
+            for (int i = 0; i < subArray.Length; i++)
+            {
+                Assert.AreEqual(array[subArray.Offset + i], subArray[i]);
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void SubArray_Indexer_1a()
+        {
+            int[] array = _GetArray();
+            SubArray<int> subArray = new SubArray<int>(array, 3);
+            int value = subArray[-100];
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void SubArray_Indexer_1b()
+        {
+            int[] array = _GetArray();
+            SubArray<int> subArray = new SubArray<int>(array, 3);
+            int value = subArray[100];
+        }
+
+        [TestMethod]
+        public void SubArray_Indexer_2()
+        {
+            int[] array = _GetArray();
+            SubArray<int> subArray = new SubArray<int>(array, 3);
+
+            for (int i = 0; i < subArray.Length; i++)
+            {
+                int x = i * i;
+                subArray[i] = x;
+                Assert.AreEqual(x, subArray[i]);
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void SubArray_Indexer_2a()
+        {
+            int[] array = _GetArray();
+            SubArray<int> subArray = new SubArray<int>(array, 3);
+            subArray[-100] = -100;
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void SubArray_Indexer_2b()
+        {
+            int[] array = _GetArray();
+            SubArray<int> subArray = new SubArray<int>(array, 3);
+            subArray[100] = 100;
+        }
+
+        [TestMethod]
+        public void SubArray_ToArray_1()
+        {
+            int[] source = _GetArray();
+            SubArray<int> subArray = new SubArray<int>(source, 3);
+            int[] target = subArray.ToArray();
+            Assert.AreEqual(subArray.Length, target.Length);
+            for (int i = 0; i < subArray.Length; i++)
+            {
+                Assert.AreEqual(subArray[i], target[i]);
+            }
+        }
+
+        [TestMethod]
+        public void SubArray_Contains_1()
+        {
+            int[] source = _GetArray();
+            SubArray<int> subArray = new SubArray<int>(source, 2, 2);
+            Assert.IsTrue(subArray.Contains(3));
+            Assert.IsFalse(subArray.Contains(33));
+            Assert.IsFalse(subArray.Contains(1));
+            Assert.IsFalse(subArray.Contains(7));
+        }
+
+        [TestMethod]
+        public void SubArray_CopyTo_1()
+        {
+            int[] source = _GetArray();
+            SubArray<int> subArray = new SubArray<int>(source, 2, 2);
+            int[] target = new int[2];
+            subArray.CopyTo(target, 0);
+            Assert.AreEqual(3, target[0]);
+            Assert.AreEqual(4, target[1]);
+        }
+
+        [TestMethod]
+        public void SubArray_IndexOf_1()
+        {
+            int[] source = _GetArray();
+            SubArray<int> subArray = new SubArray<int>(source, 2, 2);
+            Assert.AreEqual(0, subArray.IndexOf(3));
+            Assert.AreEqual(1, subArray.IndexOf(4));
+            Assert.AreEqual(-1, subArray.IndexOf(1));
+            Assert.AreEqual(-1, subArray.IndexOf(0));
+            Assert.AreEqual(-1, subArray.IndexOf(9));
+        }
+
+        [TestMethod]
+        public void SubArray_GetEnumerator_1()
+        {
+            int[] source = _GetArray();
+            SubArray<int> subArray = new SubArray<int>(source, 2, 2);
+            IEnumerator<int> enumerator = subArray.GetEnumerator();
+            Assert.IsTrue(enumerator.MoveNext());
+            Assert.AreEqual(3, enumerator.Current);
+            Assert.IsTrue(enumerator.MoveNext());
+            Assert.AreEqual(4, enumerator.Current);
+            Assert.IsFalse(enumerator.MoveNext());
+            enumerator.Dispose();
+        }
+
+        [TestMethod]
+        public void SubArray_Equals_1()
+        {
+            int[] source = _GetArray();
+            SubArray<int> sub1 = new SubArray<int>(source, 2, 2);
+            SubArray<int> sub2 = new SubArray<int>(source, 2, 2);
+            Assert.IsTrue(sub1.Equals(sub2));
+
+            sub2 = new SubArray<int>(source, 2, 3);
+            Assert.IsFalse(sub1.Equals(sub2));
+        }
+
+        [TestMethod]
+        public void SubArray_Equals_2()
+        {
+            int[] source = _GetArray();
+            SubArray<int> sub1 = new SubArray<int>(source, 2, 2);
+            SubArray<int> sub2 = new SubArray<int>(source, 2, 2);
+            Assert.IsTrue(sub1.Equals((object)sub2));
+
+            sub2 = new SubArray<int>(source, 2, 3);
+            Assert.IsFalse(sub1.Equals((object)sub2));
+
+            // ReSharper disable once RedundantCast
+            // ReSharper disable once SuspiciousTypeConversion.Global
+            Assert.IsFalse(sub1.Equals((object)"Hello"));
+        }
+
+        [TestMethod]
+        public void SubArray_ToString_1()
+        {
+            int[] source = _GetArray();
+            SubArray<int> subArray = new SubArray<int>(source, 2, 2);
+            Assert.AreEqual("3, 4", subArray.ToString());
+
+            subArray = new SubArray<int>(source, 2, 1);
+            Assert.AreEqual("3", subArray.ToString());
+
+            subArray = new SubArray<int>(source, 2, 0);
+            Assert.AreEqual("", subArray.ToString());
+        }
+    }
+}
