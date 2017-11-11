6e41d86945af2d0141cdf38a812fe700dc3ef398
Alexey Mironov <amironov73@gmail.com>
2017-11-11 14:36:38 +0800

    Add PredicateBuilder class with tests

---
+++ Source/Classic/Libs/AM.Core/AM/PredicateBuilder.cs
@@ -0,0 +1,172 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+/* PredicateBuilder.cs -- tool for predicate creation
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using System;
+using System.Linq.Expressions;
+
+using CodeJam;
+
+using JetBrains.Annotations;
+
+using MoonSharp.Interpreter;
+
+#endregion
+
+namespace AM
+{
+    /// <summary>
+    /// Tool for predicate creation.
+    /// </summary>
+    /// <remarks>
+    /// Borrowed from
+    /// <see href="http://www.albahari.com/nutshell/predicatebuilder.aspx"/>
+    /// </remarks>
+    [PublicAPI]
+    [MoonSharpUserData]
+    public static class PredicateBuilder
+    {
+        #region Public methods
+
+        /// <summary>
+        /// Creates a predicate expression from the given lambda expression.
+        /// </summary>
+        [NotNull]
+        public static Expression<Func<T, bool>> Create<T>
+            (
+                [NotNull] Expression<Func<T, bool>> predicate
+            )
+        {
+            Code.NotNull(predicate, "predicate");
+
+            return predicate;
+        }
+
+        /// <summary>
+        /// Creates a predicate that evaluates to <c>True</c>.
+        /// </summary>
+        [NotNull]
+        public static Expression<Func<bool>> True()
+        {
+            return () => true;
+        }
+
+        /// <summary>
+        /// Creates a predicate that evaluates to <c>True</c>.
+        /// </summary>
+        [NotNull]
+        public static Expression<Func<T, bool>> True<T>()
+        {
+            return param => true;
+        }
+
+        /// <summary>
+        /// Creates a predicate that evaluates to <c>False</c>.
+        /// </summary>
+        [NotNull]
+        public static Expression<Func<bool>> False()
+        {
+            return () => false;
+        }
+
+        /// <summary>
+        /// Creates a predicate that evaluates to <c>False</c>.
+        /// </summary>
+        [NotNull]
+        public static Expression<Func<T, bool>> False<T>()
+        {
+            return param => false;
+        }
+
+        /// <summary>
+        /// Combines the first predicate with the second
+        /// using the logical <c>OR</c>.
+        /// </summary>
+        [NotNull]
+        public static Expression<Func<T, bool>> Or<T>
+            (
+                [NotNull] this Expression<Func<T, bool>> expr1,
+                [NotNull] Expression<Func<T, bool>> expr2
+            )
+        {
+            Code.NotNull(expr1, "expr1");
+            Code.NotNull(expr2, "expr2");
+
+            InvocationExpression invokedExpr = Expression.Invoke
+                (
+                    expr2,
+                    expr1.Parameters
+                );
+
+            return Expression.Lambda<Func<T, bool>>
+                (
+                    Expression.OrElse
+                        (
+                            expr1.Body,
+                            invokedExpr
+                        ),
+                    expr1.Parameters
+                );
+        }
+
+        /// <summary>
+        /// Combines the first predicate with the second
+        /// using the logical <c>AND</c>.
+        /// </summary>
+        [NotNull]
+        public static Expression<Func<T, bool>> And<T>
+            (
+                [NotNull] this Expression<Func<T, bool>> expr1,
+                [NotNull] Expression<Func<T, bool>> expr2
+            )
+        {
+            Code.NotNull(expr1, "expr1");
+            Code.NotNull(expr2, "expr2");
+
+            InvocationExpression invokedExpr = Expression.Invoke
+                (
+                    expr2,
+                    expr1.Parameters
+                );
+
+            return Expression.Lambda<Func<T, bool>>
+                (
+                    Expression.AndAlso
+                        (
+                            expr1.Body,
+                            invokedExpr
+                        ),
+                    expr1.Parameters
+                );
+        }
+
+        /// <summary>
+        /// Negates a given predicate.
+        /// </summary>
+        [NotNull]
+        public static Expression<Func<T, bool>> Not<T>
+            (
+                [NotNull] this Expression<Func<T, bool>> expression
+            )
+        {
+            Code.NotNull(expression, "expression");
+
+            UnaryExpression negated = Expression.Not(expression.Body);
+
+            return Expression.Lambda<Func<T, bool>>
+                (
+                    negated,
+                    expression.Parameters
+                );
+        }
+
+        #endregion
+    }
+}
---
+++ Source/UnitTests/AM/PredicateBuilderTest.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Linq.Expressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using AM;
+
+namespace UnitTests.AM
+{
+    [TestClass]
+    public class PredicateBuilderTest
+    {
+        [TestMethod]
+        public void PredicateBuilder_True_1()
+        {
+            var trueFunc = PredicateBuilder.True<int>().Compile();
+            Assert.IsTrue(trueFunc(0));
+        }
+
+        [TestMethod]
+        public void PredicateBuilder_True_2()
+        {
+            var trueFunc = PredicateBuilder.True().Compile();
+            Assert.IsTrue(trueFunc());
+        }
+
+        [TestMethod]
+        public void PredicateBuilder_False_1()
+        {
+            var falseFunc = PredicateBuilder.False<int>().Compile();
+            Assert.IsFalse(falseFunc(0));
+        }
+
+        [TestMethod]
+        public void PredicateBuilder_False_2()
+        {
+            var falseFunc = PredicateBuilder.False().Compile();
+            Assert.IsFalse(falseFunc());
+        }
+
+        [TestMethod]
+        public void PredicateBuilder_Create_1()
+        {
+            var func = PredicateBuilder.Create<int>(x => x > 0).Compile();
+            Assert.IsTrue(func(10));
+            Assert.IsFalse(func(-10));
+        }
+
+        [TestMethod]
+        public void PredicateBuilder_Or_1()
+        {
+            var func1 = PredicateBuilder.Create<int>(x => x > 0);
+            var func2 = PredicateBuilder.Create<int>(x => x < -100);
+            var orFunc = func1.Or(func2).Compile();
+            Assert.IsTrue(orFunc(10));
+            Assert.IsTrue(orFunc(-110));
+            Assert.IsFalse(orFunc(-10));
+        }
+
+        [TestMethod]
+        public void PredicateBuilder_And_1()
+        {
+            var func1 = PredicateBuilder.Create<int>(x => x > 0);
+            var func2 = PredicateBuilder.Create<int>(x => x < 100);
+            var andFunc = func1.And(func2).Compile();
+            Assert.IsTrue(andFunc(10));
+            Assert.IsFalse(andFunc(-10));
+            Assert.IsFalse(andFunc(110));
+        }
+
+        [TestMethod]
+        public void PredicateBuilder_Not_1()
+        {
+            var func = PredicateBuilder.Create<int>(x => x > 0);
+            var notFunc = func.Not().Compile();
+            Assert.IsTrue(notFunc(-10));
+            Assert.IsFalse(notFunc(10));
+        }
+    }
+}
