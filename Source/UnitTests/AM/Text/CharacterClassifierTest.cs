b25b59a8e71e0a54a556f08b2bd079a39d795e5b
Alexey Mironov <amironov73@gmail.com>
2017-11-18 15:20:20 +0800

    Setup codecov

--- Source/UnitTests/AM/Text/CharacterClassifierTest.cs
+++ Source/UnitTests/AM/Text/CharacterClassifierTest.cs
@@ -9,114 +9,84 @@ namespace UnitTests.AM.Text
     public class CharacterClassifierTest
     {
         [TestMethod]
-        public void CharacterClassifier_DetectCharacterClasses1()
+        public void CharacterClassifier_DetectCharacterClasses_1()
         {
             string text = string.Empty;
             CharacterClass classes
-                = CharacterClassifier.DetectCharacterClasses
-                (
-                    text
-                );
+                = CharacterClassifier.DetectCharacterClasses(text);
             Assert.AreEqual(CharacterClass.None, classes);
         }
 
         [TestMethod]
-        public void CharacterClassifier_DetectCharacterClasses2()
+        public void CharacterClassifier_DetectCharacterClasses_2()
         {
             string text = "Hello";
             CharacterClass classes
-                = CharacterClassifier.DetectCharacterClasses
-                (
-                    text
-                );
+                = CharacterClassifier.DetectCharacterClasses(text);
             Assert.AreEqual(CharacterClass.BasicLatin, classes);
         }
 
         [TestMethod]
-        public void CharacterClassifier_DetectCharacterClasses3()
+        public void CharacterClassifier_DetectCharacterClasses_3()
         {
             string text = "Привет";
             CharacterClass classes
-                = CharacterClassifier.DetectCharacterClasses
-                (
-                    text
-                );
+                = CharacterClassifier.DetectCharacterClasses(text);
             Assert.AreEqual(CharacterClass.Cyrillic, classes);
         }
 
         [TestMethod]
-        public void CharacterClassifier_DetectCharacterClasses4()
+        public void CharacterClassifier_DetectCharacterClasses_4()
         {
             string text = "2128506";
             CharacterClass classes
-                = CharacterClassifier.DetectCharacterClasses
-                (
-                    text
-                );
+                = CharacterClassifier.DetectCharacterClasses(text);
             Assert.AreEqual(CharacterClass.Digit, classes);
         }
 
         [TestMethod]
-        public void CharacterClassifier_DetectCharacterClasses5()
+        public void CharacterClassifier_DetectCharacterClasses_5()
         {
             string text = "\r\n";
             CharacterClass classes
-                = CharacterClassifier.DetectCharacterClasses
-                (
-                    text
-                );
+                = CharacterClassifier.DetectCharacterClasses(text);
             Assert.AreEqual(CharacterClass.ControlCharacter, classes);
         }
 
         [TestMethod]
-        public void CharacterClassifier_DetectCharacterClasses6()
+        public void CharacterClassifier_DetectCharacterClasses_6()
         {
             string text = "Hello, Привет, 2128506\r\n";
             CharacterClass classes
-                = CharacterClassifier.DetectCharacterClasses
-                (
-                    text
-                );
+                = CharacterClassifier.DetectCharacterClasses(text);
             Assert.AreEqual
                 (
                     CharacterClass.BasicLatin
-                    |CharacterClass.ControlCharacter
-                    |CharacterClass.Cyrillic
-                    |CharacterClass.Digit,
+                    | CharacterClass.ControlCharacter
+                    | CharacterClass.Cyrillic
+                    | CharacterClass.Digit,
                     classes
                 );
         }
 
         [TestMethod]
-        public void CharacterClassifier_IsBothCyrillicAndLatin()
+        public void CharacterClassifier_IsBothCyrillicAndLatin_1()
         {
             string text = "Hello, 2128506\r\n";
             CharacterClass classes
-                = CharacterClassifier.DetectCharacterClasses
-                (
-                    text
-                );
+                = CharacterClassifier.DetectCharacterClasses(text);
             Assert.IsFalse(CharacterClassifier.IsBothCyrillicAndLatin(classes));
 
             text = "Привет, 2128506\r\n";
-            classes = CharacterClassifier.DetectCharacterClasses
-                (
-                    text
-                );
+            classes = CharacterClassifier.DetectCharacterClasses(text);
             Assert.IsFalse(CharacterClassifier.IsBothCyrillicAndLatin(classes));
 
             text = "Hello, Привет, 2128506\r\n";
-            classes = CharacterClassifier.DetectCharacterClasses
-                (
-                    text
-                );
+            classes = CharacterClassifier.DetectCharacterClasses(text);
             Assert.IsTrue(CharacterClassifier.IsBothCyrillicAndLatin(classes));
 
             text = "2128506\r\n";
-            classes = CharacterClassifier.DetectCharacterClasses
-                (
-                    text
-                );
+            classes = CharacterClassifier.DetectCharacterClasses(text);
             Assert.IsFalse(CharacterClassifier.IsBothCyrillicAndLatin(classes));
         }
     }
--- runcodecov.cmd
+++ runcodecov.cmd
@@ -1,7 +1,9 @@
 @echo off
 
 cd Source\UnitTests\bin\Debug
-C:\ProgramData\chocolatey\bin\OpenCover.Console.exe  -register:user -target:"C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\MSTest.exe" -targetargs:"/testcontainer:UnitTests.dll" -filter:"+[AM.Core.*]* +[ManagedIrbis]*" -output:coverage.xml
+C:\ProgramData\chocolatey\bin\OpenCover.Console.exe  -register:user -target:"C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\MSTest.exe" -targetargs:"/testcontainer:UnitTests.dll /runconfig:run.testsettings" -filter:"+[AM.Core.*]* +[ManagedIrbis]*" -output:coverage.xml
 C:\ProgramData\chocolatey\bin\codecov.exe -f coverage.xml
+
+cd ..\..\..\..
 
 pause
