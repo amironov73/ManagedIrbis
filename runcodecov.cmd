e597ed0e4010f2a3b893a1b1b2cb3f54a137a7d4
Alexey Mironov <amironov73@gmail.com>
2017-11-18 14:20:02 +0800

    Fix bad characters in the file names

--- runcodecov.cmd
+++ runcodecov.cmd
@@ -1,7 +1,7 @@
 @echo off
 
 cd Source\UnitTests\bin\Debug
-C:\ProgramData\chocolatey\bin\OpenCover.Console.exe  -register:user -target:"C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\MSTest.exe" -targetargs:"/testcontainer:UnitTests.dll" -filter:"+[AM.Core.*]*" -output:coverage.xml
+C:\ProgramData\chocolatey\bin\OpenCover.Console.exe  -register:user -target:"C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\MSTest.exe" -targetargs:"/testcontainer:UnitTests.dll" -filter:"+[AM.Core.*]* +[ManagedIrbis]*" -output:coverage.xml
 C:\ProgramData\chocolatey\bin\codecov.exe -f coverage.xml
 
 pause
