9a462491d77ce7eab22ce827a22009a660717448
Alexey Mironov <amironov73@gmail.com>
2017-11-27 18:11:08 +0800

    Add BlockedRecord class

---
+++ Source/Classic/Libs/ManagedIrbis/Source/Monitoring/BlockedRecord.cs
@@ -0,0 +1,166 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+/* BlockedRecord.cs -- 
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+using AM;
+using AM.Collections;
+using AM.IO;
+using AM.Logging;
+using AM.Runtime;
+using AM.Text;
+
+using CodeJam;
+
+using JetBrains.Annotations;
+
+using MoonSharp.Interpreter;
+
+using Newtonsoft.Json;
+
+#endregion
+
+namespace ManagedIrbis.Monitoring
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [PublicAPI]
+    [XmlRoot("blocked")]
+    [MoonSharpUserData]
+    public sealed class BlockedRecord
+        : IHandmadeSerializable,
+        IVerifiable,
+        IEquatable<BlockedRecord>
+    {
+        #region Properties
+
+        /// <summary>
+        /// Database.
+        /// </summary>
+        [CanBeNull]
+        [XmlAttribute("database")]
+        [JsonProperty("database", NullValueHandling = NullValueHandling.Ignore)]
+        public string Database { get; set; }
+
+        /// <summary>
+        /// MFN.
+        /// </summary>
+        [XmlAttribute("mfn")]
+        [JsonProperty("mfn", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public int Mfn { get; set; }
+
+        /// <summary>
+        /// Count.
+        /// </summary>
+        [XmlAttribute("count")]
+        [JsonProperty("count", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public int Count { get; set; }
+
+        /// <summary>
+        /// Since the moment.
+        /// </summary>
+        [XmlAttribute("since")]
+        [JsonProperty("since", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public DateTime Since { get; set; }
+
+        #endregion
+
+        #region Construction
+
+        #endregion
+
+        #region Private members
+
+        #endregion
+
+        #region Public methods
+
+        #endregion
+
+        #region IHandmadeSerializable members
+
+        /// <inheritdoc cref="IHandmadeSerializable.RestoreFromStream" />
+        public void RestoreFromStream
+            (
+                BinaryReader reader
+            )
+        {
+            Code.NotNull(reader, "reader");
+        }
+
+        /// <inheritdoc cref="IHandmadeSerializable.SaveToStream" />
+        public void SaveToStream
+            (
+                BinaryWriter writer
+            )
+        {
+            Code.NotNull(writer, "writer");
+        }
+
+        #endregion
+
+        #region IVerifiable members
+
+        /// <inheritdoc cref="IVerifiable.Verify" />
+        public bool Verify
+            (
+                bool throwOnError
+            )
+        {
+            Verifier<BlockedRecord> verifier
+                = new Verifier<BlockedRecord>(this, throwOnError);
+
+            verifier
+                .Assert(Mfn != 0, "Mfn != 0");
+
+            return verifier.Result;
+        }
+
+        #endregion
+
+        #region IEquatable<T> members
+
+        /// <inheritdoc cref="IEquatable{T}.Equals(T)" />
+        public bool Equals
+            (
+                BlockedRecord other
+            )
+        {
+            if (ReferenceEquals(other, null))
+            {
+                return false;
+            }
+
+            return Database == other.Database
+                && Mfn == other.Mfn;
+        }
+
+        #endregion
+
+        #region Object members
+
+        /// <inheritdoc cref="object.ToString" />
+        public override string ToString()
+        {
+            return Database.ToVisibleString() + ":" + Mfn.ToInvariantString();
+        }
+
+        #endregion
+    }
+}
