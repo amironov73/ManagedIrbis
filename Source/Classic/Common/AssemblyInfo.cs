87e36bf164d20ce0a9858df3f84729f59f941074
Alexey Mironov <amironov73@gmail.com>
2017-11-30 08:37:39 +0800

    Jump to version 1.8.3

--- Source/Classic/Common/AssemblyInfo.cs
+++ Source/Classic/Common/AssemblyInfo.cs
@@ -45,5 +45,5 @@ using System.Security;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.8.2.2150")]
-[assembly: AssemblyFileVersion("1.8.2.2150")]
+[assembly: AssemblyVersion("1.8.3.2300")]
+[assembly: AssemblyFileVersion("1.8.3.2300")]
