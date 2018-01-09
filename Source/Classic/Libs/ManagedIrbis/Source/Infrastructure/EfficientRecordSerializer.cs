237184c211e4f018bebde566bde73d09243ea41e
Alexey Mironov <amironov73@gmail.com>
2018-01-09 09:35:49 +0800

    Add EfficientRecordSerializer class

---
+++ Source/Classic/Libs/ManagedIrbis/Source/Infrastructure/EfficientRecordSerializer.cs
@@ -0,0 +1,111 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+/* EfficientRecordSerializer.cs -- 
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using System.IO;
+
+using AM.IO;
+
+using CodeJam;
+
+using JetBrains.Annotations;
+
+using MoonSharp.Interpreter;
+
+#endregion
+
+namespace ManagedIrbis.Infrastructure
+{
+    /// <summary>
+    /// Fast and efficient serializer for <see cref="MarcRecord"/>.
+    /// </summary>
+    [PublicAPI]
+    [MoonSharpUserData]
+    public static class EfficientRecordSerializer
+    {
+        #region Public methods
+
+        /// <summary>
+        /// Deserialize the <see cref="MarcRecord"/>.
+        /// </summary>
+        [NotNull]
+        public static MarcRecord Deserialize
+            (
+                [NotNull] BinaryReader reader
+            )
+        {
+            Code.NotNull(reader, "reader");
+
+            MarcRecord result = new MarcRecord();
+            result.Fields.BeginUpdate();
+            int fieldCount = reader.ReadInt16();
+            result.Fields.EnsureCapacity(fieldCount);
+            for (int fieldIndex = 0; fieldIndex < fieldCount; fieldIndex++)
+            {
+                int tag = reader.ReadInt16();
+                RecordField field = new RecordField(tag)
+                {
+                    Value = reader.ReadNullableString()
+                };
+                result.Fields.Add(field);
+                int subfieldCount = reader.ReadInt16();
+                for (int subCount = 0; subCount < subfieldCount; subCount++)
+                {
+                    char code = (char) reader.ReadByte();
+                    SubField subField = new SubField(code)
+                    {
+                        Value = reader.ReadNullableString()
+                    };
+                    field.SubFields.Add(subField);
+                }
+            }
+
+            result.EndUpdate();
+            return result;
+        }
+
+        /// <summary>
+        /// Serialize the <see cref="MarcRecord"/>.
+        /// </summary>
+        public static void Serialize
+            (
+                [NotNull] BinaryWriter writer,
+                [NotNull] MarcRecord record
+            )
+        {
+            Code.NotNull(writer, "writer");
+            Code.NotNull(record, "record");
+
+            unchecked
+            {
+                short fieldCount = (short) record.Fields.Count;
+                writer.Write(fieldCount);
+                for (short fieldIndex = 0; fieldIndex < fieldCount; fieldIndex++)
+                {
+                    RecordField field = record.Fields[fieldIndex];
+                    short tag = (short) field.Tag;
+                    writer.Write(tag);
+                    writer.WriteNullable(field.Value);
+                    short subfieldCount = (short) field.SubFields.Count;
+                    writer.Write(subfieldCount);
+                    for (short subIndex = 0; subIndex < subfieldCount; subIndex++)
+                    {
+                        SubField subField = field.SubFields[subIndex];
+                        byte code = (byte) subField.Code;
+                        writer.Write(code);
+                        writer.WriteNullable(subField.Value);
+                    }
+                }
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Source/UnitTests/ManagedIrbis/Infrastructure/EfficientRecordSerializerTest.cs
@@ -0,0 +1,67 @@
+﻿using System.IO;
+
+using AM;
+
+using JetBrains.Annotations;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using ManagedIrbis;
+using ManagedIrbis.Client;
+using ManagedIrbis.Infrastructure;
+
+namespace UnitTests.ManagedIrbis.Infrastructure
+{
+    [TestClass]
+    public class EfficientRecordSerializerTest
+        : Common.CommonUnitTest
+    {
+        private void _TestSerialization
+            (
+                [NotNull] MarcRecord firstRecord
+            )
+        {
+            MemoryStream stream = new MemoryStream();
+            BinaryWriter writer = new BinaryWriter(stream);
+            EfficientRecordSerializer.Serialize(writer, firstRecord);
+            writer.Close();
+
+            byte[] bytes = stream.ToArray();
+            stream = new MemoryStream(bytes);
+            BinaryReader reader = new BinaryReader(stream);
+            MarcRecord secondRecord = EfficientRecordSerializer.Deserialize(reader);
+            Assert.AreEqual(firstRecord.Fields.Count, secondRecord.Fields.Count);
+            for (int fieldIndex = 0; fieldIndex < firstRecord.Fields.Count; fieldIndex++)
+            {
+                RecordField firstField = firstRecord.Fields[fieldIndex];
+                RecordField secondField = secondRecord.Fields[fieldIndex];
+                Assert.AreEqual(firstField.Tag, secondField.Tag);
+                Assert.AreEqual(firstField.Value, secondField.Value);
+                Assert.AreEqual(firstField.SubFields.Count, secondField.SubFields.Count);
+                for (int subIndex = 0; subIndex < firstField.SubFields.Count; subIndex++)
+                {
+                    SubField firstSubField = firstField.SubFields[subIndex];
+                    SubField secondSubField = secondField.SubFields[subIndex];
+                    Assert.AreEqual(firstSubField.Code, secondSubField.Code);
+                    Assert.AreEqual(firstSubField.Value, secondSubField.Value);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void EfficientRecordSerializer_Serialization_1()
+        {
+            MarcRecord record = new MarcRecord();
+            _TestSerialization(record);
+
+            using (IrbisProvider provider = GetProvider())
+            {
+                for (int mfn = 1; mfn < 10; mfn++)
+                {
+                    record = provider.ReadRecord(mfn).ThrowIfNull();
+                    _TestSerialization(record);
+                }
+            }
+        }
+    }
+}
