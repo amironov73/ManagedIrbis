fa4d1e4a36c8127e26ccdf6b2a2482a54802f437
Alexey Mironov <amironov73@gmail.com>
2018-01-10 14:46:44 +0800

    Add &uf('+B') support

---
+++ Source/Classic/Libs/ManagedIrbis/Source/Pft/Infrastructure/Unifors/UniforPlusB.cs
@@ -0,0 +1,63 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+/* UniforPlusB.cs -- 
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using AM;
+
+using JetBrains.Annotations;
+
+#endregion
+
+// ReSharper disable ForCanBeConvertedToForeach
+
+namespace ManagedIrbis.Pft.Infrastructure.Unifors
+{
+    //
+    // ibatrak
+    //
+    // Неописанная функция unifor('+B').
+    // Суммирует байты входной строки.
+    //
+
+    static class UniforPlusB
+    {
+        #region Public methods
+
+        /// <summary>
+        /// Sum of input string bytes.
+        /// </summary>
+        public static void ByteSum
+            (
+                [NotNull] PftContext context,
+                [CanBeNull] PftNode node,
+                [CanBeNull] string expression
+            )
+        {
+            if (!ReferenceEquals(expression, null))
+            {
+                byte[] bytes = IrbisEncoding.Utf8.GetBytes(expression);
+                long sum = 0;
+                unchecked
+                {
+                    for (int i = 0; i < bytes.Length; i++)
+                    {
+                        sum += bytes[i];
+                    }
+                }
+
+                string output = sum.ToInvariantString();
+                context.Write(node, output);
+                context.OutputFlag = true;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Source/UnitTests/ManagedIrbis/Pft/Infrastructure/Unifors/UniforPlusBTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests.ManagedIrbis.Pft.Infrastructure.Unifors
+{
+    [TestClass]
+    public class UniforPlusBTest
+        : CommonUniforTest
+    {
+        [TestMethod]
+        public void UniforPlusB_ByteSum_1()
+        {
+            Execute("+B", "0");
+            Execute("+B1", "49");
+            Execute("+B12", "99");
+            Execute("+B123", "150");
+            Execute("+B1234", "202");
+            Execute("+BПривет", "2210");
+        }
+    }
+}
