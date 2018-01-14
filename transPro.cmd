61142a6f9959c1049709b1e2969bee9b482b9a6d
Alexey Mironov <amironov73@gmail.com>
2018-01-14 15:22:05 +0800

    Add solutions for FW 3.5/4.6/4.7

--- transPro.cmd
+++ transPro.cmd
@@ -3,13 +3,15 @@
 Utils\TransProject.exe "Source\Classic\Libs\AM.Core\AM.Core.csproj"           "Source\Classic\Libs\AM.Core\AM.Core.3.5.csproj"
 Utils\TransProject.exe "Source\Classic\Libs\AM.Core\AM.Core.csproj"           "Source\Classic\Libs\AM.Core\AM.Core.4.0.csproj"
 Utils\TransProject.exe "Source\Classic\Libs\AM.Core\AM.Core.csproj"           "Source\Classic\Libs\AM.Core\AM.Core.4.6.csproj"
+Utils\TransProject.exe "Source\Classic\Libs\AM.Core\AM.Core.csproj"           "Source\Classic\Libs\AM.Core\AM.Core.4.7.csproj"
 Utils\TransProject.exe "Source\Classic\Libs\AM.Core\AM.Core.csproj"           "Source\Droid\Libs\AM.Core\AM.Core.csproj"                 ..\..\..\Classic\Libs\AM.Core\
 Utils\TransProject.exe "Source\Classic\Libs\AM.Core\AM.Core.csproj"           "Source\Universal\Libs\AM.Core\AM.Core.csproj"             ..\..\..\Classic\Libs\AM.Core\
 Utils\TransProject.exe "Source\Classic\Libs\AM.Core\AM.Core.csproj"           "Source\WinMobile\AM.Core\AM.Core.csproj"                  ..\..\Classic\Libs\AM.Core\
 
-Utils\TransProject.exe "Source\Classic\Libs\ManagedIrbis\ManagedIrbis.csproj" "Source\Classic\Libs\AM.Core\AM.Core.3.5.csproj"
-Utils\TransProject.exe "Source\Classic\Libs\ManagedIrbis\ManagedIrbis.csproj" "Source\Classic\Libs\AM.Core\AM.Core.4.0.csproj"
-Utils\TransProject.exe "Source\Classic\Libs\ManagedIrbis\ManagedIrbis.csproj" "Source\Classic\Libs\AM.Core\AM.Core.4.6.csproj"
+Utils\TransProject.exe "Source\Classic\Libs\ManagedIrbis\ManagedIrbis.csproj" "Source\Classic\Libs\ManagedIrbis\ManagedIrbis.3.5.csproj"
+Utils\TransProject.exe "Source\Classic\Libs\ManagedIrbis\ManagedIrbis.csproj" "Source\Classic\Libs\ManagedIrbis\ManagedIrbis.4.0.csproj"
+Utils\TransProject.exe "Source\Classic\Libs\ManagedIrbis\ManagedIrbis.csproj" "Source\Classic\Libs\ManagedIrbis\ManagedIrbis.4.6.csproj"
+Utils\TransProject.exe "Source\Classic\Libs\ManagedIrbis\ManagedIrbis.csproj" "Source\Classic\Libs\ManagedIrbis\ManagedIrbis.4.7.csproj"
 Utils\TransProject.exe "Source\Classic\Libs\ManagedIrbis\ManagedIrbis.csproj" "Source\Droid\Libs\ManagedIrbis\ManagedIrbis.csproj"       ..\..\..\Classic\Libs\ManagedIrbis\
 Utils\TransProject.exe "Source\Classic\Libs\ManagedIrbis\ManagedIrbis.csproj" "Source\Universal\Libs\ManagedIrbis\ManagedIrbis.csproj"   ..\..\..\Classic\Libs\ManagedIrbis\
 Utils\TransProject.exe "Source\Classic\Libs\ManagedIrbis\ManagedIrbis.csproj" "Source\WinMobile\ManagedIrbis\ManagedIrbis.csproj"        ..\..\Classic\Libs\ManagedIrbis\
@@ -17,6 +19,7 @@ Utils\TransProject.exe "Source\Classic\Libs\ManagedIrbis\ManagedIrbis.csproj" "S
 Utils\TransProject.exe "Source\Classic\Libs\AM.AOT\AM.AOT.csproj"             "Source\Classic\Libs\AM.AOT\AM.AOT.3.5.csproj"
 Utils\TransProject.exe "Source\Classic\Libs\AM.AOT\AM.AOT.csproj"             "Source\Classic\Libs\AM.AOT\AM.AOT.4.0.csproj"
 Utils\TransProject.exe "Source\Classic\Libs\AM.AOT\AM.AOT.csproj"             "Source\Classic\Libs\AM.AOT\AM.AOT.4.6.csproj"
+Utils\TransProject.exe "Source\Classic\Libs\AM.AOT\AM.AOT.csproj"             "Source\Classic\Libs\AM.AOT\AM.AOT.4.7.csproj"
 Utils\TransProject.exe "Source\Classic\Libs\AM.AOT\AM.AOT.csproj"             "Source\Droid\Libs\AM.AOT\AM.AOT.csproj"                   ..\..\..\Classic\Libs\AM.AOT\
 Utils\TransProject.exe "Source\Classic\Libs\AM.AOT\AM.AOT.csproj"             "Source\Universal\Libs\AM.AOT\AM.AOT.csproj"               ..\..\..\Classic\Libs\AM.AOT\
 rem Utils\TransProject.exe "Source\Classic\Libs\AM.AOT\AM.AOT.csproj"             "Source\WinMobile\AM.AOT\AM.AOT.csproj"                   ..\..\Classic\Libs\AM.AOT\
@@ -24,5 +27,17 @@ rem Utils\TransProject.exe "Source\Classic\Libs\AM.AOT\AM.AOT.csproj"           
 Utils\TransProject.exe "Source\Classic\Libs\RestfulIrbis\RestfulIrbis.csproj" "Source\Classic\Libs\RestfulIrbis\RestfulIrbis.3.5.csproj"
 Utils\TransProject.exe "Source\Classic\Libs\RestfulIrbis\RestfulIrbis.csproj" "Source\Classic\Libs\RestfulIrbis\RestfulIrbis.4.0.csproj"
 Utils\TransProject.exe "Source\Classic\Libs\RestfulIrbis\RestfulIrbis.csproj" "Source\Classic\Libs\RestfulIrbis\RestfulIrbis.4.6.csproj"
+Utils\TransProject.exe "Source\Classic\Libs\RestfulIrbis\RestfulIrbis.csproj" "Source\Classic\Libs\RestfulIrbis\RestfulIrbis.4.7.csproj"
 Utils\TransProject.exe "Source\Classic\Libs\RestfulIrbis\RestfulIrbis.csproj" "Source\Droid\Libs\RestfulIrbis\RestfulIrbis.csproj"       ..\..\..\Classic\Libs\RestfulIrbis\
 Utils\TransProject.exe "Source\Classic\Libs\RestfulIrbis\RestfulIrbis.csproj" "Source\Universal\Libs\RestfulIrbis\RestfulIrbis.csproj"   ..\..\..\Classic\Libs\RestfulIrbis\
+
+Utils\TransProject.exe "Source\Classic\Libs\AM.Drawing\AM.Drawing.csproj" "Source\Classic\Libs\AM.Drawing\AM.Drawing.3.5.csproj"
+Utils\TransProject.exe "Source\Classic\Libs\AM.Drawing\AM.Drawing.csproj" "Source\Classic\Libs\AM.Drawing\AM.Drawing.4.0.csproj"
+Utils\TransProject.exe "Source\Classic\Libs\AM.Drawing\AM.Drawing.csproj" "Source\Classic\Libs\AM.Drawing\AM.Drawing.4.6.csproj"
+Utils\TransProject.exe "Source\Classic\Libs\AM.Drawing\AM.Drawing.csproj" "Source\Classic\Libs\AM.Drawing\AM.Drawing.4.7.csproj"
+
+Utils\TransProject.exe "Source\Classic\Libs\AM.Windows.Forms\AM.Windows.Forms.csproj" "Source\Classic\Libs\AM.Windows.Forms\AM.Windows.Forms.3.5.csproj"
+Utils\TransProject.exe "Source\Classic\Libs\AM.Windows.Forms\AM.Windows.Forms.csproj" "Source\Classic\Libs\AM.Windows.Forms\AM.Windows.Forms.4.0.csproj"
+Utils\TransProject.exe "Source\Classic\Libs\AM.Windows.Forms\AM.Windows.Forms.csproj" "Source\Classic\Libs\AM.Windows.Forms\AM.Windows.Forms.4.6.csproj"
+Utils\TransProject.exe "Source\Classic\Libs\AM.Windows.Forms\AM.Windows.Forms.csproj" "Source\Classic\Libs\AM.Windows.Forms\AM.Windows.Forms.4.7.csproj"
+
