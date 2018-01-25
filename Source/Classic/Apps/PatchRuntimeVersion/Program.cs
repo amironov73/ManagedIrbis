c0be458f86e3a53ed08deb900516bb88d8dd118c
Alexey Mironov <amironov73@gmail.com>
2018-01-25 10:08:25 +0800

    Add PatchRuntimeVersion.exe utility

---
+++ Source/Classic/Apps/PatchRuntimeVersion/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Xml.XPath;
+
+namespace PatchRuntimeVersion
+{
+    class Program
+    {
+        private static readonly string[] runtimeVersions =
+        {
+            "1.0",   "v1.0.3705",
+            "1.1",   "v1.1.4322",
+            "2.0",   "v2.0.50727",
+            "3.0",   "v2.0.50727",
+            "3.5",   "v2.0.50727",
+            "4.0",   "v4.0",
+            "4.0.1", "v4.0",
+            "4.0.2", "v4.0",
+            "4.0.3", "v4.0",
+            "4.5",   "v4.0",
+            "4.5.1", "v4.0",
+            "4.5.2", "v4.0",
+            "4.6",   "v4.0",
+            "4.6.1", "v4.0",
+            "4.7",   "v4.0",
+            "4.7.1", "v4.0"
+        };
+
+        private static readonly string[] skuIds =
+        {
+            "4.0",   ".NETFramework,Version=v4.0",
+            "4.0.1", ".NETFramework,Version=v4.0.1",
+            "4.0.2", ".NETFramework,Version=v4.0.2",
+            "4.0.3", ".NETFramework,Version=v4.0.3",
+            "4.5",   ".NETFramework,Version=v4.5",
+            "4.5.1", ".NETFramework,Version=v4.5.1",
+            "4.5.2", ".NETFramework,Version=v4.5.2",
+            "4.6",   ".NETFramework,Version=v4.6",
+            "4.6.1", ".NETFramework,Version=v4.6.1",
+            "4.7",   ".NETFramework,Version=v4.7",
+            "4.7.1", ".NETFramework,Version=v4.7.1"
+        };
+
+        static void ProcessFile
+            (
+                string fileName,
+                string runtimeValue,
+                string skuValue
+            )
+        {
+            Console.WriteLine(fileName);
+            XDocument document = XDocument.Load(fileName);
+            XElement element = document.XPathSelectElement("/configuration/startup/supportedRuntime");
+            if (ReferenceEquals(element, null))
+            {
+                return;
+            }
+
+            XAttribute version = element.Attribute("version")
+                ?? new XAttribute("version", runtimeValue);
+            version.Value = runtimeValue;
+
+            XAttribute sku = element.Attribute("sku");
+            if (string.IsNullOrEmpty(skuValue))
+            {
+                if (!ReferenceEquals(sku, null))
+                {
+                    sku.Remove();
+                }
+            }
+            else
+            {
+                if (ReferenceEquals(sku, null))
+                {
+                    sku = new XAttribute("sku", skuValue);
+                }
+                sku.Value = skuValue;
+            }
+
+            document.Save(fileName);
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("PatchRuntimeVersion <file.config> <version>");
+
+                return;
+            }
+
+            string ver = args[1];
+            string foundRuntime = null;
+            for (int i = 0; i < runtimeVersions.Length; i += 2)
+            {
+                if (runtimeVersions[i] == ver)
+                {
+                    foundRuntime = runtimeVersions[i + 1];
+                    break;
+                }
+            }
+
+            if (string.IsNullOrEmpty(foundRuntime))
+            {
+                Console.WriteLine("Unknown version: {0}", ver);
+                return;
+            }
+
+            string foundSku = null;
+            if (foundRuntime == "v4.0")
+            {
+                for (int i = 0; i < skuIds.Length; i += 2)
+                {
+                    if (skuIds[i] == ver)
+                    {
+                        foundSku = skuIds[i + 1];
+                        break;
+                    }
+                }
+            }
+
+            string directory = Path.GetDirectoryName(args[0]) ?? ".";
+            string pattern = Path.GetFileName(args[0]) ?? "*.exe.config";
+            string[] files = Directory.GetFiles(directory, pattern);
+            foreach (string file in files)
+            {
+                ProcessFile(file, foundRuntime, foundSku);
+            }
+        }
+    }
+}
--- compile.cmd
+++ compile.cmd
@@ -2,9 +2,11 @@
 
 rem SET MSBUILD=C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe
 SET MSBUILD="%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe"
+SET BINARIES=Binaries
 SET OUTPUT=..\..\..\..\Binaries
 SET PARAMS=/consoleloggerparameters:ErrorsOnly /m
 SET NUGET=Nuget\nuget.exe
+SET PATCH=Utils\PatchRuntimeVersion.exe
 
 echo v3.5 restore
 echo.
@@ -13,11 +15,16 @@ echo.
 echo v3.5 DEBUG
 echo.
 %MSBUILD% Source\ManagedIrbis.3.5.sln /p:Configuration=Debug    /t:Rebuild %PARAMS%
-
 echo.
 echo v3.5 RELEASE
 echo.
 %MSBUILD% Source\ManagedIrbis.3.5.sln /p:Configuration=Release  /t:Rebuild %PARAMS%
+echo.
+echo v3.5 patch
+echo.
+%PATCH% %BINARIES%\Debug35\*.exe.config   3.5
+%PATCH% %BINARIES%\Release35\*.exe.config 3.5
+echo.
 
 echo v4.0 restore
 echo.
@@ -26,11 +33,16 @@ echo.
 echo v4.0 DEBUG
 echo.
 %MSBUILD% Source\ManagedIrbis.4.0.sln /p:Configuration=Debug    /t:Rebuild %PARAMS%
-
 echo.
 echo v4.0 RELEASE
 echo.
 %MSBUILD% Source\ManagedIrbis.4.0.sln /p:Configuration=Release  /t:Rebuild %PARAMS%
+echo.
+echo v4.0 patch
+echo.
+%PATCH% %BINARIES%\Debug40\*.exe.config   4.0
+%PATCH% %BINARIES%\Release40\*.exe.config 4.0
+echo.
 
 echo v4.5 restore
 echo.
@@ -39,11 +51,16 @@ echo.
 echo v4.5 DEBUG
 echo.
 %MSBUILD% Source\ManagedIrbis.4.5.sln /p:Configuration=Debug    /t:Rebuild /p:OutputPath=%OUTPUT%\Debug45 %PARAMS%
-
 echo.
 echo v4.5 RELEASE
 echo.
 %MSBUILD% Source\ManagedIrbis.4.5.sln /p:Configuration=Release  /t:Rebuild /p:OutputPath=%OUTPUT%\Release45 %PARAMS%
+echo.
+echo v4.5 patch
+echo.
+%PATCH% %BINARIES%\Debug45\*.exe.config   4.5
+%PATCH% %BINARIES%\Release45\*.exe.config 4.5
+echo.
 
 echo v4.6 restore
 echo.
@@ -52,11 +69,16 @@ echo.
 echo v4.6 DEBUG
 echo.
 %MSBUILD% Source\ManagedIrbis.4.6.sln /p:Configuration=Debug    /t:Rebuild %PARAMS%
-
 echo.
 echo v4.6 RELEASE
 echo.
 %MSBUILD% Source\ManagedIrbis.4.6.sln /p:Configuration=Release  /t:Rebuild %PARAMS%
+echo.
+echo v4.6 patch
+echo.
+%PATCH% %BINARIES%\Debug46\*.exe.config   4.6
+%PATCH% %BINARIES%\Release46\*.exe.config 4.6
+echo.
 
 echo v4.7 restore
 echo.
@@ -65,8 +87,13 @@ echo.
 echo v4.7 DEBUG
 echo.
 %MSBUILD% Source\ManagedIrbis.4.7.sln /p:Configuration=Debug    /t:Rebuild %PARAMS%
-
 echo.
 echo v4.7 RELEASE
 echo.
 %MSBUILD% Source\ManagedIrbis.4.7.sln /p:Configuration=Release  /t:Rebuild %PARAMS%
+echo.
+echo v4.7 patch
+echo.
+%PATCH% %BINARIES%\Debug47\*.exe.config   4.7
+%PATCH% %BINARIES%\Release47\*.exe.config 4.7
+echo.
