8987accc642b28e254de871567b7f1edc66053b1
Alexey Mironov <amironov73@gmail.com>
2018-02-27 19:45:21 +0800

    Add tests for NonCloseableTextReader and ScopedLockFactory classes

---
+++ Source/UnitTests/AM/IO/NonCloseableTextReaderTest.cs
@@ -0,0 +1,124 @@
+﻿using System.IO;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using AM.IO;
+
+using JetBrains.Annotations;
+
+using Moq;
+
+namespace UnitTests.AM.IO
+{
+    [TestClass]
+    public class NonCloseableTextReaderTest
+        : Common.CommonUnitTest
+    {
+        [NotNull]
+        private Mock<TextReader> _GetReader()
+        {
+            Mock<TextReader> mock = new Mock<TextReader>();
+
+            mock.Setup(r => r.Close());
+            mock.Setup(r => r.Peek()).Returns(-1);
+            mock.Setup(r => r.Read()).Returns(-1);
+            mock.Setup(r => r.Read(It.IsAny<char[]>(), It.IsAny<int>(), It.IsAny<int>())).Returns(0);
+            mock.Setup(r => r.ReadBlock(It.IsAny<char[]>(), It.IsAny<int>(), It.IsAny<int>())).Returns(0);
+            mock.Setup(r => r.ReadLine()).Returns(string.Empty);
+            mock.Setup(r => r.ReadToEnd()).Returns(string.Empty);
+
+            return mock;
+        }
+
+        [TestMethod]
+        public void NonCloseableTextReader_Construction_1()
+        {
+            TextReader textReader = new StringReader("hello");
+            NonCloseableTextReader reader = new NonCloseableTextReader(textReader);
+            reader.Dispose();
+        }
+
+        //[TestMethod]
+        //public void NonCloseableTextReader_ReallyClose_1()
+        //{
+        //    Mock<TextReader> mock = _GetReader();
+        //    NonCloseableTextReader reader = new NonCloseableTextReader(mock.Object);
+        //    reader.ReallyClose();
+        //    mock.Verify(r => r.Close(), Times.Once);
+        //}
+
+        [TestMethod]
+        public void NonCloseableTextReader_Close_1()
+        {
+            Mock<TextReader> mock = _GetReader();
+            NonCloseableTextReader reader = new NonCloseableTextReader(mock.Object);
+            reader.Close();
+            mock.Verify(r => r.Close(), Times.Never);
+        }
+
+        [TestMethod]
+        public void NonCloseableTextReader_Dispose_1()
+        {
+            Mock<TextReader> mock = _GetReader();
+            NonCloseableTextReader reader = new NonCloseableTextReader(mock.Object);
+            reader.Dispose();
+            mock.Verify(r => r.Close(), Times.Never);
+        }
+
+        [TestMethod]
+        public void NonCloseableTextReader_Peek_1()
+        {
+            Mock<TextReader> mock = _GetReader();
+            NonCloseableTextReader reader = new NonCloseableTextReader(mock.Object);
+            Assert.AreEqual(-1, reader.Peek());
+            mock.Verify(r => r.Peek(), Times.Once);
+        }
+
+        [TestMethod]
+        public void NonCloseableTextReader_Read_1()
+        {
+            Mock<TextReader> mock = _GetReader();
+            NonCloseableTextReader reader = new NonCloseableTextReader(mock.Object);
+            Assert.AreEqual(-1, reader.Read());
+            mock.Verify(r => r.Read(), Times.Once);
+        }
+
+        [TestMethod]
+        public void NonCloseableTextReader_Read_2()
+        {
+            Mock<TextReader> mock = _GetReader();
+            NonCloseableTextReader reader = new NonCloseableTextReader(mock.Object);
+            char[] buffer = new char[10];
+            Assert.AreEqual(0, reader.Read(buffer, 0, buffer.Length));
+            mock.Verify(r => r.Read(It.IsAny<char[]>(), It.IsAny<int>(), It.IsAny<int>()), Times.Once);
+        }
+
+        [TestMethod]
+        public void NonCloseableTextReader_ReadBlock_1()
+        {
+            Mock<TextReader> mock = _GetReader();
+            NonCloseableTextReader reader = new NonCloseableTextReader(mock.Object);
+            char[] buffer = new char[10];
+            Assert.AreEqual(0, reader.ReadBlock(buffer, 0, buffer.Length));
+            mock.Verify(r => r.ReadBlock(It.IsAny<char[]>(), It.IsAny<int>(), It.IsAny<int>()), Times.Once);
+        }
+
+        [TestMethod]
+        public void NonCloseableTextReader_ReadLine_1()
+        {
+            Mock<TextReader> mock = _GetReader();
+            NonCloseableTextReader reader = new NonCloseableTextReader(mock.Object);
+            Assert.AreEqual(string.Empty, reader.ReadLine());
+            mock.Verify(r => r.ReadLine(), Times.Once);
+        }
+
+        [TestMethod]
+        public void NonCloseableTextReader_ReadToEnd_1()
+        {
+            Mock<TextReader> mock = _GetReader();
+            NonCloseableTextReader reader = new NonCloseableTextReader(mock.Object);
+            Assert.AreEqual(string.Empty, reader.ReadToEnd());
+            mock.Verify(r => r.ReadToEnd(), Times.Once);
+        }
+    }
+}
---
+++ Source/UnitTests/AM/Threading/ScopedLockFactoryTest.cs
@@ -0,0 +1,27 @@
+﻿using AM.Threading;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests.AM.Text
+{
+    [TestClass]
+    public class ScopedLockFactoryTest
+    {
+        [TestMethod]
+        public void ScopedLockFactory_Construction_1()
+        {
+            using (ScopedLockFactory factory = new ScopedLockFactory())
+            {
+                using (var lock1 = factory.CreateLock())
+                {
+                    Assert.IsNotNull(lock1);
+                }
+
+                using (var lock2 = factory.CreateLock())
+                {
+                    Assert.IsNotNull(lock2);
+                }
+            }
+        }
+    }
+}
