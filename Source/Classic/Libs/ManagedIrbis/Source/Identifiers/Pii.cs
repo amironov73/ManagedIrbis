da13bd2fd83ba9d560e0bd8e0da2801ab40eae20
Alexey Mironov <amironov73@gmail.com>
2018-05-26 14:11:32 +0800

    Add classes for ISRC, PII and PMID

---
+++ Source/Classic/Libs/ManagedIrbis/Source/Identifiers/Pii.cs
@@ -0,0 +1,67 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+/* Pii.cs -- Publisher Item Identifier.
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using JetBrains.Annotations;
+
+using MoonSharp.Interpreter;
+
+#endregion
+
+namespace ManagedIrbis.Identifiers
+{
+    //
+    // См. https://ru.wikipedia.org/wiki/Publisher_Item_Identifier
+    //
+    // Publisher Item Identifier (PII) — уникальный идентификатор,
+    // применяемый некоторыми научными журналами для идентификации
+    // научных работ. Он основан на более ранних идентификаторах
+    // ISSN и ISBN, к которым добавлены символ для уточнения типа
+    // публикации, номер сущности и контрольная цифра.
+    //
+    // Системой PII пользуются с 1996 года издатели American Chemical
+    // Society, American Institute of Physics, American Physical
+    // Society, Elsevier и IEEE.
+    //
+    // Формат
+    //
+    // Идентификатор PII представляет собой строку из 17 символов, состоящую из:
+    //
+    // Символ типа публикации: «S» означает периодическое издание
+    // и код ISSN, «B» — книги и код ISBN.
+    // ISSN (8 цифр) или ISBN (10 цифр)
+    // для периодики добавлено 2 цифры, чтобы выровнять длину кода.
+    // Часто используется 2 последние цифры года, в который произошло
+    // присвоение номера PII.
+    // пятизначный код, присвоенный данной работе издателем.
+    // Должен быть уникален в рамках данного журнала или книги
+    // контрольная цифра (0-9 или X)
+    // При печати код PII может быть дополнен знаками пунктуации для
+    // упрощения чтения, например, Sxxxx-xxxx(yy)iiiii-d
+    // или Bx-xxx-xxxxx-x/iiiii-d.
+    //
+
+    /// <summary>
+    /// Publisher Item Identifier.
+    /// </summary>
+    [PublicAPI]
+    [MoonSharpUserData]
+    public class Pii
+    {
+        #region Properties
+
+        /// <summary>
+        /// Identifier.
+        /// </summary>
+        public string Identifier { get; set; }
+
+        #endregion
+    }
+}
---
+++ Source/Classic/Libs/ManagedIrbis/Source/Identifiers/Pmid.cs
@@ -0,0 +1,57 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+/* Pmid.cs -- PubMed Identifier.
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using JetBrains.Annotations;
+
+using MoonSharp.Interpreter;
+
+#endregion
+
+namespace ManagedIrbis.Identifiers
+{
+    //
+    // См. https://ru.wikipedia.org/wiki/PMID
+    //
+    // PMID (от англ. PubMed Identifier, PubMed ID) — уникальный
+    // идентификационный номер, присваиваемый каждой публикации,
+    // описание, аннотация или полный текст которой хранятся
+    // в базе данных PubMed.
+    //
+    // Допускается одновременный поиск нескольких публикаций
+    // по их PMID (с уточняющим тегом [pmid] или без него),
+    // введённым в поисковую строку PubMed через пробел — например,
+    // 7170002 16381840.
+    //
+    // При одновременном поиске названий публикаций и других терминов
+    // использование стандартных поисковых тегов PubMed обязательно:
+    // lipman[au] 16381840[pmid].
+    //
+    // Идентификаторы PMID, присвоенные публикациям в PubMed,
+    // впоследствии никогда не меняются и не используются повторно.
+    //
+
+    /// <summary>
+    /// PubMed Identifier.
+    /// </summary>
+    [PublicAPI]
+    [MoonSharpUserData]
+    public class Pmid
+    {
+        #region Properties
+
+        /// <summary>
+        /// Identifier.
+        /// </summary>
+        public string Identifier { get; set; }
+
+        #endregion
+    }
+}
