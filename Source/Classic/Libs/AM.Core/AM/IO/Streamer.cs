c00d11878ce87a586c3ced3cd48351cbfcd60a7a
Alexey Mironov <amironov73@gmail.com>
2018-08-21 07:11:26 +0800

    Add Streamer class with tests

---
+++ Source/Classic/Libs/AM.Core/AM/IO/Streamer.cs
@@ -0,0 +1,278 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+/* Streamer.cs --
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using System;
+using System.IO;
+using System.Text;
+
+using CodeJam;
+
+using JetBrains.Annotations;
+
+using MoonSharp.Interpreter;
+
+#endregion
+
+namespace AM.IO
+{
+    /// <summary>
+    ///
+    /// </summary>
+    [PublicAPI]
+    [MoonSharpUserData]
+    public sealed class Streamer
+        : IDisposable
+    {
+        #region Properties
+
+        /// <summary>
+        /// End of the stream?
+        /// </summary>
+        public bool Eof
+        {
+            get
+            {
+                return _position >= _limit && !_Advance();
+            }
+        }
+
+        /// <summary>
+        /// Inner stream.
+        /// </summary>
+        [NotNull]
+        public Stream InnerStream
+        {
+            get { return _stream; }
+        }
+
+        #endregion
+
+        #region Construction
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public Streamer
+            (
+                [NotNull] Stream stream,
+                int bufferSize
+            )
+        {
+            Code.NotNull(stream, "stream");
+            Code.Positive(bufferSize, "bufferSize");
+
+            if (!stream.CanRead)
+            {
+                throw new ArgumentException();
+            }
+
+            _stream = stream;
+            _bufferSize = bufferSize;
+            _buffer = new byte[_bufferSize];
+            _position = 0;
+            _limit = 0;
+            _Advance();
+        }
+
+        #endregion
+
+        #region Private members
+
+        private readonly Stream _stream;
+        private byte[] _buffer;
+        private readonly int _bufferSize;
+        private int _position, _limit;
+
+        private bool _Advance()
+        {
+            _limit = _stream.Read(_buffer, 0, _bufferSize);
+            _position = 0;
+
+            return _limit > 0;
+        }
+
+        #endregion
+
+        #region Public methods
+
+        /// <summary>
+        /// Peek one byte.
+        /// </summary>
+        public int PeekByte()
+        {
+            if (_position < _limit)
+            {
+                return _buffer[_position];
+            }
+
+            if (!_Advance())
+            {
+                return -1;
+            }
+
+            return _buffer[_position];
+        }
+
+        /// <summary>
+        /// Read some bytes.
+        /// </summary>
+        public int Read
+            (
+                [NotNull] byte[] buffer
+            )
+        {
+            return Read(buffer, 0, buffer.Length);
+        }
+
+        /// <summary>
+        /// Read some bytes.
+        /// </summary>
+        public int Read
+            (
+                [NotNull] byte[] buffer,
+                int offset,
+                int count
+            )
+        {
+            Code.NotNull(buffer, "buffer");
+            Code.Nonnegative(offset, "offset");
+
+            int total = 0;
+            while (count > 0)
+            {
+                int tail = _limit - _position;
+                int portion = Math.Min(tail, count);
+                if (portion <= 0)
+                {
+                    if (!_Advance())
+                    {
+                        break;
+                    }
+                    continue;
+                }
+
+                Array.Copy
+                    (
+                        _buffer,
+                        _position,
+                        buffer,
+                        offset,
+                        portion
+                    );
+
+                count -= portion;
+                offset += portion;
+                _position += portion;
+                total += portion;
+            }
+
+            return total;
+        }
+
+        /// <summary>
+        /// Read one byte.
+        /// </summary>
+        public int ReadByte()
+        {
+            if (_position < _limit)
+            {
+                return _buffer[_position++];
+            }
+
+            if (!_Advance())
+            {
+                return -1;
+            }
+
+            return _buffer[_position++];
+        }
+
+        /// <summary>
+        /// Read one line.
+        /// </summary>
+        [CanBeNull]
+        public string ReadLine
+            (
+                [NotNull] Encoding encoding
+            )
+        {
+            Code.NotNull(encoding, "encoding");
+
+            if (Eof)
+            {
+                return null;
+            }
+
+            MemoryStream result = new MemoryStream();
+            byte found = 0;
+            while (found == 0)
+            {
+                while (_position < _limit)
+                {
+                    byte one = _buffer[_position++];
+                    if (one == '\r' || one == '\n')
+                    {
+                        found = one;
+                        break;
+                    }
+                    result.WriteByte(one);
+                }
+
+                if (found == 0 && !_Advance())
+                {
+                    break;
+                }
+            }
+
+            if (found == '\r')
+            {
+                int peek = PeekByte();
+                if (peek == '\n')
+                {
+                    ReadByte();
+                }
+            }
+
+            return encoding.GetString(result.ToArray());
+        }
+
+        /// <summary>
+        /// Get the rest of the stream.
+        /// </summary>
+        [NotNull]
+        public byte[] ToArray()
+        {
+            MemoryStream result = new MemoryStream();
+
+            if (_position < _limit)
+            {
+                result.Write(_buffer, _position, _limit - _position);
+                _position = _limit;
+            }
+
+            _stream.CopyTo(result);
+
+            return result.ToArray();
+        }
+
+        #endregion
+
+        #region IDisposable members
+
+        /// <inheritdoc cref="IDisposable.Dispose" />
+        public void Dispose()
+        {
+            _stream.Dispose();
+        }
+
+        #endregion
+    }
+}
---
+++ Source/UnitTests/AM/IO/StreamerTest.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using AM.IO;
+
+using JetBrains.Annotations;
+
+using Moq;
+using Moq.Protected;
+
+// ReSharper disable ObjectCreationAsStatement
+
+namespace UnitTests.AM.IO
+{
+    [TestClass]
+    public class StreamerTest
+    {
+        [NotNull]
+        private MemoryStream _GetStream
+            (
+                string text = "Hello,\r\nWorld!"
+            )
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(text);
+            MemoryStream result = new MemoryStream(bytes);
+
+            return result;
+        }
+
+        [TestMethod]
+        public void Streamer_Construction_1()
+        {
+            MemoryStream memory = _GetStream();
+            Streamer streamer = new Streamer(memory, 4);
+            Assert.AreSame(memory, streamer.InnerStream);
+            Assert.IsFalse(streamer.Eof);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Streamer_Construction_2()
+        {
+            Mock<Stream> mock = new Mock<Stream>();
+            mock.SetupGet(s => s.CanRead).Returns(false);
+            Stream stream = mock.Object;
+            new Streamer(stream, 4);
+        }
+
+        [TestMethod]
+        public void Streamer_PeekByte_1()
+        {
+            MemoryStream memory = new MemoryStream(new byte[] { 1, 2, 3 });
+            Streamer streamer = new Streamer(memory, 2);
+            Assert.AreEqual(1, streamer.PeekByte());
+            Assert.AreEqual(1, streamer.PeekByte());
+            Assert.AreEqual(1, streamer.ReadByte());
+            Assert.AreEqual(2, streamer.PeekByte());
+            Assert.AreEqual(2, streamer.PeekByte());
+            Assert.AreEqual(2, streamer.ReadByte());
+            Assert.AreEqual(3, streamer.PeekByte());
+            Assert.AreEqual(3, streamer.PeekByte());
+            Assert.AreEqual(3, streamer.ReadByte());
+            Assert.AreEqual(-1, streamer.PeekByte());
+            Assert.AreEqual(-1, streamer.PeekByte());
+            Assert.AreEqual(-1, streamer.ReadByte());
+        }
+
+        [TestMethod]
+        public void Streamer_Read_1()
+        {
+            MemoryStream memory = _GetStream("\x01\x02\x03\x04\x05\x06");
+            Streamer streamer = new Streamer(memory, 2);
+            byte[] buffer = new byte[4];
+            Assert.AreEqual(4, streamer.Read(buffer));
+            Assert.AreEqual(1, buffer[0]);
+            Assert.AreEqual(2, buffer[1]);
+            Assert.AreEqual(3, buffer[2]);
+            Assert.AreEqual(4, buffer[3]);
+            Assert.AreEqual(2, streamer.Read(buffer));
+            Assert.AreEqual(5, buffer[0]);
+            Assert.AreEqual(6, buffer[1]);
+            Assert.AreEqual(3, buffer[2]);
+            Assert.AreEqual(0, streamer.Read(buffer));
+            Assert.AreEqual(0, streamer.Read(buffer));
+            Assert.AreEqual(5, buffer[0]);
+        }
+
+        [TestMethod]
+        public void Streamer_Read_2()
+        {
+            MemoryStream memory = _GetStream();
+            Streamer streamer = new Streamer(memory, 2);
+            byte[] buffer = new byte[4];
+            Assert.AreEqual(0, streamer.Read(buffer, 0, -1));
+            Assert.AreEqual(0, streamer.Read(buffer, 0, 0));
+        }
+
+        [TestMethod]
+        public void Streamer_ReadByte_1()
+        {
+            MemoryStream stream = _GetStream("\x01\x02\x03");
+            Streamer streamer = new Streamer(stream, 2);
+            Assert.AreEqual(1, streamer.ReadByte());
+            Assert.AreEqual(2, streamer.ReadByte());
+            Assert.AreEqual(3, streamer.ReadByte());
+            Assert.AreEqual(-1, streamer.ReadByte());
+        }
+
+        [TestMethod]
+        public void Streamer_ReadLine_1()
+        {
+            Streamer streamer = new Streamer(_GetStream(), 4);
+            Assert.IsFalse(streamer.Eof);
+            Assert.AreEqual("Hello,", streamer.ReadLine(Encoding.UTF8));
+            Assert.IsFalse(streamer.Eof);
+            Assert.AreEqual("World!", streamer.ReadLine(Encoding.UTF8));
+            Assert.IsTrue(streamer.Eof);
+            Assert.IsNull(streamer.ReadLine(Encoding.UTF8));
+            Assert.IsTrue(streamer.Eof);
+            Assert.IsNull(streamer.ReadLine(Encoding.UTF8));
+        }
+
+        [TestMethod]
+        public void Streamer_Dispose_1()
+        {
+            Mock<Stream> mock = new Mock<Stream>();
+            mock.SetupGet(one => one.CanRead).Returns(true);
+            mock.Setup(one => one.Close());
+            Stream stream = mock.Object;
+            Streamer streamer = new Streamer(stream, 4);
+            streamer.Dispose();
+            mock.Verify(one => one.Close());
+        }
+
+        [TestMethod]
+        public void Streamer_ToArray_1()
+        {
+            MemoryStream stream = _GetStream();
+            byte[] expected = stream.ToArray();
+            stream.Position = 0;
+            Streamer streamer = new Streamer(stream, 4);
+            byte[] actual = streamer.ToArray();
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Streamer_ToArray_2()
+        {
+            MemoryStream stream = new MemoryStream();
+            Streamer streamer = new Streamer(stream, 4);
+            byte[] array = streamer.ToArray();
+            Assert.AreEqual(0, array.Length);
+        }
+    }
+}
