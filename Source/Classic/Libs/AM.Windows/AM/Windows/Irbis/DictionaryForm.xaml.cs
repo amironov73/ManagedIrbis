3f1c2a48cbbdb1e7f63e252f8ad5c7e5da6788d5
Alexey Mironov <amironov73@gmail.com>
2018-09-07 13:24:01 +0800

    Add DictionaryForm class

---
+++ Source/Classic/Libs/AM.Windows/AM/Windows/Irbis/DictionaryForm.xaml.cs
@@ -0,0 +1,118 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+/* DictionaryForm.cs --
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+using ManagedIrbis.Search;
+
+#endregion
+
+namespace AM.Windows.Irbis
+{
+    /// <summary>
+    /// Interaction logic for DictionaryForm.xaml
+    /// </summary>
+    public partial class DictionaryForm
+    {
+        #region Properties
+
+        /// <summary>
+        /// Current item.
+        /// </summary>
+        public TermInfo CurrentItem
+        {
+            get
+            {
+                TermInfo result = listGrid.SelectedValue as TermInfo;
+
+                return result;
+            }
+        }
+
+        /// <summary>
+        /// Terms.
+        /// </summary>
+        public TermInfo[] Terms { get; private set; }
+
+        #endregion
+
+        #region Construction
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public DictionaryForm()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Private members
+
+        private void Window_SizeChanged
+            (
+                object sender,
+                SizeChangedEventArgs e
+            )
+        {
+            double workingWidth = listGrid.ActualWidth
+                - SystemParameters.VerticalScrollBarWidth;
+            gridView.Columns[0].Width = workingWidth * 0.1;
+            gridView.Columns[1].Width = workingWidth - gridView.Columns[0].Width;
+        }
+
+        private void GoButton_OnClick
+            (
+                object sender,
+                RoutedEventArgs e
+            )
+        {
+            DialogResult = true;
+        }
+
+        private void CancelButton_OnClick
+            (
+                object sender,
+                RoutedEventArgs e
+            )
+        {
+            DialogResult = false;
+        }
+
+        #endregion
+
+        #region Public methods
+
+        /// <summary>
+        /// Set terms.
+        /// </summary>
+        public void SetTerms(TermInfo[] terms)
+        {
+            Terms = terms;
+            listGrid.ItemsSource = terms;
+        }
+
+        #endregion
+    }
+}
---
+++ Source/UnitTests/Experiments/DictionaryFormExperiments.cs
@@ -0,0 +1,45 @@
+﻿
+using System.Windows;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using ManagedIrbis;
+
+using AM.Windows.Irbis;
+
+using ManagedIrbis.Search;
+
+namespace UnitTests.Experiments
+{
+    //[TestClass]
+    public class DictionaryFormExperiments
+    {
+        //[TestMethod]
+        public void TestMethod1()
+        {
+            TermInfo[] terms =
+            {
+                new TermInfo{Count = 1, Text = "БЕТОН"},
+                new TermInfo{Count = 1, Text = "БЕТОНИРОВАНИЕ"},
+                new TermInfo{Count = 1, Text = "БЕТОНОМЕШАЛКА"},
+                new TermInfo{Count = 1, Text = "БЕТОНЩИК"},
+                new TermInfo{Count = 1, Text = "ВАТА"},
+                new TermInfo{Count = 1, Text = "ВАТАНИРОВАНИЕ"},
+                new TermInfo{Count = 1, Text = "ВАТАМЕШАЛКА"},
+                new TermInfo{Count = 1, Text = "ВАТОНЩИК"},
+                new TermInfo{Count = 1, Text = "ГРАВИЙ"},
+                new TermInfo{Count = 1, Text = "ГРАВИРОВАНИЕ"},
+                new TermInfo{Count = 1, Text = "ГРАВИМЕШАЛКА"},
+                new TermInfo{Count = 1, Text = "ГРАВИРОВЩИК"},
+                new TermInfo{Count = 1, Text = "ДОСКА"},
+                new TermInfo{Count = 1, Text = "ДОСКИРОВАНИЕ"},
+                new TermInfo{Count = 1, Text = "ДОСКОМЕШАЛКА"},
+                new TermInfo{Count = 1, Text = "ДОСОЧНИК"},
+            };
+
+            DictionaryForm form = new DictionaryForm();
+            form.SetTerms(terms);
+            form.ShowDialog();
+        }
+    }
+}
