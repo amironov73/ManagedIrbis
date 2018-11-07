de5e0b052c1b3b3ea7a9d8740503eed32beebf53
Alexey Mironov <amironov73@gmail.com>
2018-11-07 09:33:33 +0800

    Add Protector project

---
+++ Source/Classic/Apps/Protector/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AM.Configuration;
+
+namespace Protector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 0)
+            {
+                string source = args[0];
+                if (source == "-")
+                {
+                    source = Console.ReadLine();
+                }
+                string encrypted = ConfigurationUtility.Protect(source);
+                Console.WriteLine(encrypted);
+            }
+        }
+    }
+}
---
+++ Source/Classic/Apps/Protector/Properties/LocalAssemblyInfo.cs
@@ -0,0 +1,28 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+/* LocalAssemblyInfo.cs -- general information about assembly
+ * Ars Magna project, http://arsmagna.ru
+ * -------------------------------------------------------
+ * Status: poor
+ */
+
+#region Using directives
+
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+#endregion
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Protector")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("Protector")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e8c44fec-b102-4c50-8da3-f22697c43e4e")]
