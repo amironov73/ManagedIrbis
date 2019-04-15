0ba86ea355f139d12242f22975d080e0e6815695
Alexey Mironov <amironov73@gmail.com>
2019-04-16 07:35:48 +0800

    Add Alligator utility

---
+++ Source/Classic/Apps/Alligator/Source/EffectiveSheet.cs
@@ -0,0 +1,159 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+#region Using directives
+
+using System;
+using System.Windows.Forms;
+
+using AM.Windows.Forms;
+
+using DevExpress.Spreadsheet;
+
+using JetBrains.Annotations;
+
+#endregion
+
+namespace Alligator
+{
+    class EffectiveSheet
+        : IDisposable
+    {
+        public Control Control;
+        public IWorkbook Workbook;
+        private readonly Worksheet _worksheet;
+        private int _currentRow;
+
+        public int CurrentRow => _currentRow;
+
+        public EffectiveSheet()
+        {
+            Workbook = new Workbook();
+            _worksheet = Workbook.Worksheets[0];
+            _currentRow = 0;
+        }
+
+        public EffectiveSheet
+            (
+                [NotNull] IWorkbook workbook
+            )
+        {
+            Workbook = workbook;
+            _worksheet = workbook.Worksheets[0];
+            _currentRow = 0;
+        }
+
+        public void Clear()
+        {
+            _worksheet.Clear(_worksheet.Cells);
+        }
+
+        [NotNull]
+        public Row CurrentLine()
+        {
+            return _worksheet.Rows[_currentRow];
+        }
+
+        public void Dispose()
+        {
+            Workbook.Dispose();
+        }
+
+        [NotNull]
+        public Cell WriteCell(int column, string text)
+        {
+            Cell result = null;
+            Control.InvokeIfRequired(() =>
+            {
+                result = _worksheet.Cells[_currentRow, column];
+                result.Value = text;
+            });
+
+            return result;
+        }
+
+        [NotNull]
+        public Cell WriteCell(int column, int value)
+        {
+            Cell result = null;
+            Control.InvokeIfRequired(() =>
+            {
+                result = _worksheet.Cells[_currentRow, column];
+                result.Value = value;
+            });
+
+            return result;
+        }
+
+        [NotNull]
+        public Cell WriteCell(int column, double value, string format)
+        {
+            Cell result = null;
+            Control.InvokeIfRequired(() =>
+            {
+                result = _worksheet.Cells[_currentRow, column];
+                result.Value = value;
+                result.NumberFormat = format;
+            });
+
+            return result;
+        }
+
+        [NotNull]
+        public Cell WriteCell(int column, decimal value, string format)
+        {
+            Cell result = null;
+            Control.InvokeIfRequired(() =>
+            {
+                result = _worksheet.Cells[_currentRow, column];
+                result.Value = (double)value;
+                result.NumberFormat = format;
+            });
+
+            return result;
+        }
+
+        [NotNull]
+        public Row WriteLine
+            (
+                string format,
+                params object[] args
+            )
+        {
+            WriteCell(0, string.Format(format, args));
+            var result = CurrentLine();
+            NewLine();
+
+            return result;
+        }
+
+        [NotNull]
+        public EffectiveSheet Invoke
+            (
+                [NotNull] MethodInvoker action
+            )
+        {
+            Control.InvokeIfRequired(action);
+
+            return this;
+        }
+
+        public void NewLine()
+        {
+            _currentRow++;
+        }
+
+        public Range GetRange(int column, int topRow, int bottomRow)
+        {
+            return _worksheet.Range.FromLTRB(column, topRow, column, bottomRow);
+        }
+
+        public void SaveDocument
+            (
+                string fileName
+            )
+        {
+            Workbook.SaveDocument(fileName);
+        }
+    }
+}
---
+++ Source/Classic/Apps/Alligator/Source/EffectiveStat.cs
@@ -0,0 +1,144 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+#region Using directives
+
+using System;
+
+using AM;
+
+using CodeJam;
+
+using JetBrains.Annotations;
+
+#endregion
+
+namespace Alligator
+{
+    class EffectiveStat
+    {
+        /// <summary>
+        /// Описание.
+        /// </summary>
+        public string Description;
+
+        /// <summary>
+        /// Количество названий.
+        /// </summary>
+        public int TitleCount;
+
+        /// <summary>
+        /// Количество экземпляров.
+        /// </summary>
+        public int ExemplarCount;
+
+        /// <summary>
+        /// Общая стоимость экземпляров.
+        /// </summary>
+        public decimal TotalCost;
+
+        /// <summary>
+        /// Количество выдач.
+        /// </summary>
+        public int LoanCount;
+
+        /// <summary>
+        /// Дата поступления экземпляров.
+        /// </summary>
+        public DateTime Date;
+
+        /// <summary>
+        /// Сигла (фонд).
+        /// </summary>
+        public string Sigla;
+
+        /// <summary>
+        /// ББК.
+        /// </summary>
+        public string Bbk;
+
+        /// <summary>
+        /// Раздел знаний.
+        /// </summary>
+        public string KnowledgeSection;
+
+        public void Add
+            (
+                [NotNull] EffectiveStat other
+            )
+        {
+            Code.NotNull(other, "other");
+
+            TitleCount += other.TitleCount;
+            ExemplarCount += other.ExemplarCount;
+            TotalCost += other.TotalCost;
+            LoanCount += other.LoanCount;
+            if (Date == DateTime.MinValue)
+            {
+                Date = other.Date;
+            }
+            else if (other.Date != DateTime.MinValue && other.Date < Date)
+            {
+                Date = other.Date;
+            }
+        }
+
+        public void Output
+            (
+                [NotNull] EffectiveEngine engine,
+                bool bold = false
+            )
+        {
+            if (ExemplarCount == 0)
+            {
+                return;
+            }
+
+            decimal loanCost = LoanCount == 0
+                ? TotalCost
+                : TotalCost / LoanCount;
+
+            decimal meanLoan = ExemplarCount == 0
+                ? LoanCount
+                : (decimal)LoanCount / ExemplarCount;
+
+            int days = Date == DateTime.MinValue
+                ? 0
+                : (DateTime.Today - Date).Days + 1;
+            decimal dayLoan = days == 0
+                ? 0
+                : (decimal) LoanCount / days;
+            decimal rdrEff = days == 0
+                ? 0
+                : (decimal) LoanCount / ExemplarCount / days * 1000m;
+            decimal finEff = days == 0
+                ? 0
+                : TotalCost == 0
+                  ? 0
+                  : LoanCount / TotalCost / days * 100000m;
+
+            EffectiveSheet sheet = engine.Sheet;
+            sheet.WriteCell(0, Description);
+            sheet.WriteCell(1, Date.ToShortDateString());
+            sheet.WriteCell(2, Sigla);
+            sheet.WriteCell(3, Bbk);
+            sheet.WriteCell(4, KnowledgeSection);
+            sheet.WriteCell(5, TitleCount);
+            sheet.WriteCell(6, ExemplarCount);
+            sheet.WriteCell(7, TotalCost, "0.00");
+            sheet.WriteCell(8, LoanCount);
+            sheet.WriteCell(9, meanLoan, "0.00");
+            sheet.WriteCell(10, loanCost, "0.00");
+            sheet.WriteCell(11, dayLoan, "0.00");
+            sheet.WriteCell(12, rdrEff, "0.00");
+            sheet.WriteCell(13, finEff, "0.00");
+
+            if (bold)
+            {
+                sheet.Invoke(() => sheet.CurrentLine().Bold());
+            }
+
+            sheet.NewLine();
+        }
+    }
+}
