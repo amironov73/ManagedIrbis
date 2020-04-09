c63319f4f20d0b2e3e2d5dd248a1de38d7e9cf41
Alexey Mironov <amironov73@gmail.com>
2020-04-09 13:35:15 +0800

    Support for connection string encryption

---
+++ Source/UnitTests/AM/Security/SecurityUtilityTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AM.Security;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests.AM.Security
+{
+    [TestClass]
+    class SecurityUtilityTest
+    {
+        [TestMethod]
+        public void SecurityUtility_Encrypt_1()
+        {
+            var secretText = "У попа была собака";
+            var password = "ИРБИС";
+            var encryptedText = SecurityUtility.Encrypt(secretText, password);
+            Assert.AreEqual("", encryptedText);
+        }
+    }
+}
